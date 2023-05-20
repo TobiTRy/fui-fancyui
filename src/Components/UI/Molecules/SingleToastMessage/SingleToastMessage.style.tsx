d7de951924d56dc9fac0a610484104f0e0134fff
MrTRyy <tobiastry@aol.de>
2023-05-20 17:14:14 +0200

    splitOut: X Button to make a Atom of it

--- src/Components/UI/Molecules/SingleToastMessage/SingleToastMessage.style.tsx
+++ src/Components/UI/Molecules/SingleToastMessage/SingleToastMessage.style.tsx
@@ -1,6 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 
-import { borderRadius, colorPalet, fontSize, spacingPx } from '../../Design/design';
+import { borderRadius, colorPalet, fontSize, systemMessages ,spacingPx } from '../../Design/design';
 
 type ToastMessageProps = 'success' | 'warning' | 'error';
 
@@ -13,21 +13,6 @@ interface TimerLineProps {
   time: number;
 }
 
-// colors for different types of messages (success, warning, error)
-const typeColors = {
-  success: {
-    dark: colorPalet.green_dark,
-    light: colorPalet.green_light,
-  },
-  warning: {
-    dark: colorPalet.yellow_dark,
-    light: colorPalet.yellow_light,
-  },
-  error: {
-    dark: colorPalet.red_dark,
-    light: colorPalet.red_light,
-  },
-};
 
 
 // styles for single toast message
@@ -37,11 +22,11 @@ export const Container = styled.div<IToastMessage>`
   position: relative;
   flex-direction: column;
   align-items: left;
-  color: ${({ messageType }) => typeColors[messageType].light};
+  color: ${({ messageType }) => systemMessages[messageType].light};
   border-radius: ${borderRadius.small};
   padding: ${spacingPx.lg};
-  background-color: ${({ messageType }) => typeColors[messageType].dark};
-  border-left: 4px solid ${({ messageType }) => typeColors[messageType].light};
+  background-color: ${({ messageType }) => systemMessages[messageType].dark};
+  border-left: 4px solid ${({ messageType }) => systemMessages[messageType].light};
   box-sizing: border-box;
 `;
 
@@ -72,7 +57,7 @@ export const TimerLine = styled.div<TimerLineProps>`
   left: 0;
   height: 2px;
   width: 100%;
-  background-color: ${({ messageType }) => typeColors[messageType].light};
+  background-color: ${({ messageType }) => systemMessages[messageType].light};
   animation: ${() => timerAnimation} ${({ time }) => time - 300}ms linear forwards;
 `;
 
@@ -90,7 +75,7 @@ export const Message = styled.p`
 
 export const CloseButton = styled.button<IToastMessage>`
   padding: 0  ${spacingPx.sm} 2px;
-  color: ${({ messageType }) => typeColors[messageType].light};
+  color: ${({ messageType }) => systemMessages[messageType].light};
   background: none;
   border: none;
   font-size: ${fontSize.large};
