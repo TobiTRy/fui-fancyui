c7a69e9e0778c7ac1e10fe504cbe4ab83c2f5812
MrTRyy <tobiastry@aol.de>
2023-06-07 18:21:37 +0200

    BuildIn: FancyVR

---
+++ src/Components/UI/Atoms/FancyVR.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from "styled-components";
+
+import { IUiColorsTypes, uiColors } from '../Design/design';
+
+const FancyVR = styled.hr<{design?: IUiColorsTypes }>`
+  position: absolute; // added this line
+  top: 5%; // added this line
+  bottom: 5%; // added this line
+  background-color: ${({ design }) => design ? uiColors[design].main : uiColors.accent.main };
+  display: block;
+  width: 1px;
+  border-radius: 2px;
+  border: 0;
+  padding: 0;
+
+  &::after {
+    content: '';
+    display: block;
+    height: 100%;
+    background-color: ${({ design }) => design ? uiColors[design].main : uiColors.accent.main };
+    width: 1px;
+    border: 0;
+    padding: 0;
+    filter: blur(0.5px);
+  }
+`;
+
+
+
+export default FancyVR
+
