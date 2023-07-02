17ecea496d080d09b9d6364b94e52425a30ccb88
MrTRyy <tobiastry@aol.de>
2023-07-02 18:17:15 +0200

    config

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+
+const packageJson = require('./package.json');
+
+const options = {
+    input: 'src/indexUILibaryFile.ts',
+    output: [
+        {
+            file: packageJson.main,
+            format: 'cjs',
+            sourcemap: true,
+            name: 'react-lib'
+        },
+        {
+            file: packageJson.module,
+            format: 'esm',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        postcss(),
+        terser()
+    ]
+}
+
+export default options;
---
+++ src/indexUILibaryFile.ts
@@ -0,0 +1,3 @@
+import FancyButton from "./Components/UI/Molecules/FancyButton/FancyButton";
+
+export { FancyButton };
