2461c558b7b3f170d6f25c338c84955a11fa253f
MrTRyy <tobiastry@aol.de>
2023-07-14 14:27:25 +0200

    remove: unesasery import

--- src/Routes/ColorPickerRoute/ColorPickerRoute.tsx
+++ src/Routes/ColorPickerRoute/ColorPickerRoute.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ColorPicker from '../../Components/UI/Organisms/FancyColorPicker/FancyColorPicker'
-import TextInput from '../../Components/UI/Molecules/TextInput/TextInput'
-
 
 export default function ColorPickerRoute() {
   return (
