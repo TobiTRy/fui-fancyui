1bf99d4b29cdc9878963a69ae9608f1fbd3d04d2
MrTRyy <tobiastry@aol.de>
2023-07-14 14:35:37 +0200

    update: spacing slider

--- src/Components/UI/Atoms/RawSlider/RawSlider.style.tsx
+++ src/Components/UI/Atoms/RawSlider/RawSlider.style.tsx
@@ -17,6 +17,7 @@ export const StyledRawSlider = styled.input`
   width: 100%;
   margin: 0;
   height: 6px;
+  margin: 10px 0 12px 0;
   background: gray;
   border-radius: 5px;
   background-image: linear-gradient(90deg, ${uiColors.accent.main}, ${uiColors.accent.light});
