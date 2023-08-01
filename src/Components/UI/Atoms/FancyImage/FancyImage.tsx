de9446fa69320f57cc5e930816746533098075d2
MrTRyy <tobiastry@aol.de>
2023-08-01 15:19:30 +0200

    update: FancyImage

--- src/Components/UI/Atoms/FancyImage/FancyImage.tsx
+++ src/Components/UI/Atoms/FancyImage/FancyImage.tsx
@@ -4,36 +4,34 @@ import styled from 'styled-components';
 
 
 
-const StyledImage = styled.img<{ darken?: boolean, aspectRatio: string }>`
+const StyledImage = styled.img<{ darken?: boolean, aspectRatio?: string }>`
   width: 100%;
   object-fit: cover;
-  position: absolute;
-  top: 0;
-  left: 0;
   transition: filter 0.3s;
+  ${({ aspectRatio }) => (aspectRatio ? `aspect-ratio: ${aspectRatio};` : '')}
   aspect-ratio: 1 / 1;
   filter: ${({ darken }) => (darken ? 'brightness(0.5)' : 'none')};
 `;
 
-
 export interface IFancyImage {
   imageUrl: string;
-  aspectRatio: string; // e.g. "16:9"
+  aspectRatio?: string; // e.g. "16/9"
   darken?: boolean;
   link?: string;
+  alt?: string;
 }
 
 export default function FancyImage(props: IFancyImage) {
-  const { imageUrl, aspectRatio, darken, link } = props;
+  const { imageUrl, aspectRatio, darken, link, alt } = props;
 
   return (
     <>
       {link ? (
         <a href={link} target="_blank" rel="noopener noreferrer">
-          <StyledImage src={imageUrl} darken={darken} />
+          <StyledImage src={imageUrl} darken={darken} aspectRatio={aspectRatio} alt={alt}/>
         </a>
       ) : (
-        <StyledImage src={imageUrl} darken={darken} />
+        <StyledImage src={imageUrl} darken={darken} alt={alt}/>
       )}
     </>
 
