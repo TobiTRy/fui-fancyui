bb13758e1c9f6b721d338feeb947e197bf229e35
MrTRyy <tobiastry@aol.de>
2023-08-06 18:02:29 +0200

    update: Some changes

--- src/Components/UI/Atoms/FancyLoadingBar/FancyLoadingBar.tsx
+++ src/Components/UI/Atoms/FancyLoadingBar/FancyLoadingBar.tsx
@@ -4,7 +4,7 @@ import styled, { keyframes } from 'styled-components';
 
 const loadingAnimation = keyframes`
   0% {
-    left: -50%;
+    left: -70%;
   }
   100% {
     left: 100%;
@@ -15,15 +15,14 @@ const LoadingContainer = styled.div`
   position: relative;
   width: 100%;
   height: 2px;
-  background-color: ${uiColors.secondary.darkest};
   overflow: hidden;
 `;
 
 const LoadingBar = styled.div`
   position: absolute;
-  left: -40%;
+  left: -70%;
   height: 100%;
-  width: 50%;
+  width: 70%;
   background-image: linear-gradient(90deg, transparent, ${uiColors.accent.main}, transparent);
   animation: ${loadingAnimation} 3s ease-in-out infinite; // Animation duration set to 4s
 `;
--- src/Components/UI/Atoms/FancyLoadingSpinner/FancyLoadingSpinner.tsx
+++ src/Components/UI/Atoms/FancyLoadingSpinner/FancyLoadingSpinner.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import styled, { keyframes } from 'styled-components';
 import { uiColors } from '../../Design/design';
 
@@ -8,41 +7,68 @@ const spinner = keyframes`
   100% { transform: rotate(360deg); }
 `;
 
+const reverseSpinner = keyframes`
+  0% { transform: rotate(360deg); }
+  100% { transform: rotate(0deg); }
+`;
+
 const sizes = {
   small: {
     width: '12px',
     thickness: '2px',
+    thickness1: '1.5px',
   },
   medium: {
     width: '24px',
     thickness: '2px',
+    thickness1: '1.5px',
   },
   large: {
     width: '48px',
     thickness: '2px',
+    thickness1: '1.5px',
   },
 };
 
-const StyledFancyLoadingSpinner = styled.div<{ $size?: keyof typeof sizes; $thickness?: string }>`
+const SpinnerContainer = styled.div<{ $size?: keyof typeof sizes }>`
   position: relative;
-  border-top: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid ${uiColors.secondary.darkest};
+  width: ${({ $size }) => ($size ? sizes[$size].width : sizes.medium.width)};
+  height: ${({ $size }) => ($size ? sizes[$size].width : sizes.medium.width)};
+  display: flex; // Added Flexbox
+  align-items: center; // Center vertically
+  justify-content: center; // Center horizontally
+`;
+
+const StyledInnerSpinner = styled.div<{ $size?: keyof typeof sizes }>`
+  border-top: ${({ $size }) => ($size ? sizes[$size].thickness1 : sizes.medium.thickness1)} solid transparent;
+  border-right: ${({ $size }) => ($size ? sizes[$size].thickness1 : sizes.medium.thickness1)} solid ${uiColors.accent.main};
+  border-bottom: ${({ $size }) => ($size ? sizes[$size].thickness1 : sizes.medium.thickness1)} solid transparent;
+  border-left: ${({ $size }) => ($size ? sizes[$size].thickness1 : sizes.medium.thickness1)} solid ${uiColors.accent.main};
+  animation: ${reverseSpinner} 1.4s infinite ease-in-out;
+  border-radius: 50%;
+  width: 66%; // Reduced to 66%
+  height: 66%; // Reduced to 66%
+`;
+
+const StyledFancyLoadingSpinner = styled.div<{ $size?: keyof typeof sizes; $thickness?: string }>`
+  position: absolute;
+  border-top: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid transparent;
   border-right: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid ${uiColors.accent.main};
-  border-bottom: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid ${uiColors.secondary.darkest};
+  border-bottom: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid transparent;
   border-left: ${({ $size }) => ($size ? sizes[$size].thickness : sizes.medium.thickness)} solid ${uiColors.accent.main};
   animation: ${spinner} 1.4s infinite ease-in-out;
-
-  &,
-  &:after {
-    border-radius: 50%;
-    width: ${({ $size }) => ($size ? sizes[$size].width : sizes.medium.width)};
-    aspect-ratio: 1/1;
-  }
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
 `;
 
 export default function FancyLoadingSpinner() {
   return (
     <>
-      <StyledFancyLoadingSpinner $size={'large'} />
+      <SpinnerContainer $size={'large'}>
+        <StyledFancyLoadingSpinner $size={'large'} />
+        <StyledInnerSpinner $size={'large'} />
+      </SpinnerContainer>
     </>
   );
 }
