fe27f1493a1438c3d2438b5c2d866321f845c715
MrTRyy <tobiastry@aol.de>
2023-08-06 21:58:32 +0200

    init: Progressbar

--- src/Routes/LoadingSpinnerRoute/LoadingSpinnerRoute.tsx
+++ src/Routes/LoadingSpinnerRoute/LoadingSpinnerRoute.tsx
@@ -4,11 +4,14 @@ import { DesignArea, DesignWrapper } from '../DesignWrapper/Wrapper';
 
 import FancyLoadingSpinner  from '../../Components/UI/Atoms/FancyLoadingSpinner/FancyLoadingSpinner';
 import FancyLoadingBar from '../../Components/UI/Atoms/FancyLoadingBar/FancyLoadingBar';
+import ProzessBar from '../../Components/UI/Atoms/ProgressBar/ProzessBar';
 
 export default function LoadingSpinnerRoute() {
   return (
     <DesignWrapper>
       <DesignArea title="Loading Spinner">
+        <p>Hii</p>
+        <ProzessBar progress={10}/>
         <FancyLoadingSpinner />
         <FancyLoadingBar />
       </DesignArea>
