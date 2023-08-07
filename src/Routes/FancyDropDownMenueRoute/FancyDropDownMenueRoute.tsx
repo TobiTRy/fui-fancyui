ec62d8f55c8feca69fc34dc005b9009454eddcf1
MrTRyy <tobiastry@aol.de>
2023-08-07 17:48:41 +0200

    remove: zustand from dropdown

--- src/Routes/FancyDropDownMenueRoute/FancyDropDownMenueRoute.tsx
+++ src/Routes/FancyDropDownMenueRoute/FancyDropDownMenueRoute.tsx
@@ -11,7 +11,7 @@ export default function FancyDropDownMenueRoute() {
     <DesignWrapper>
       <DesignArea title="FancyDropDownMenue">
         <FancyButton onClick={() => setIsOpen(!isOpen)} label="Open Menue"></FancyButton>
-        <FancyDropDownMenue isOpen={isOpen} alignHorizontal="right" alignVertical="top" width="50%">
+        <FancyDropDownMenue isOpen={isOpen} alignHorizontal="right" alignVertical="top" width="50%" >
           <div style={{display: 'flex', flexDirection: 'column', gap: '24px'}}>
             <FancyButton label="Button" design="primary" />
             <FancyButton label="Button" design="secondary" />
