98de34d5b268acb2c51cf43767599293fef13751
MrTRyy <tobiastry@aol.de>
2023-08-13 23:04:04 +0200

    cleanUp:

--- src/Components/UI/Molecules/InputWrapper/InputWrapper.tsx
+++ src/Components/UI/Molecules/InputWrapper/InputWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { css } from 'styled-components';
 
 import { ErrorMessage, StyledInputWrapper, InputContainer } from './InputWrapper.style';
@@ -8,6 +8,7 @@ import FancySVGAtom from '../../Atoms/FancySVGAtom/FancySVGAtom';
 import { spacing } from '../../Design/design';
 import { AnimatedInputLabel } from '../../Atoms/AnimatedLabel/AnimatedLabel.style';
 
+// Define the styles for the icon
 const iconStyle = css`
   margin-right: ${spacing.xs + 2 + 'px'};
   margin-bottom: 2px;
@@ -15,6 +16,7 @@ const iconStyle = css`
   align-self: flex-end;
 `;
 
+// Define the interface for the color state
 interface IColorState {
   type: 'underline' | 'label';
   isActiv?: boolean;
@@ -23,6 +25,7 @@ interface IColorState {
   placeholder?: string;
 }
 
+// Define a function to calculate the color state based on the input props
 const calcColorState = ({ type, isActiv, errorMessage, value, placeholder }: IColorState) => {
   if (errorMessage) return 'error';
   if (isActiv) return 'active';
@@ -31,6 +34,7 @@ const calcColorState = ({ type, isActiv, errorMessage, value, placeholder }: ICo
   return 'default';
 };
 
+// Define the props for the InputWrapper component
 export type IInputWrapperUserInputProps = Omit<IInputWrapper, 'children' | 'id' | 'underline' | 'autoWidth' | 'value'>;
 export interface IInputWrapper {
   id: string;
@@ -46,12 +50,23 @@ export interface IInputWrapper {
   underline?: boolean;
   placeholder?: string;
 }
+// --------------------------------------------------------------------------- //
+// ------ The Wrapper for the inputs that give him some extra features  ------ //
+// ------------------ like a Label icon errormessage ------------------------- //
 export default function InputWrapper(props: IInputWrapper) {
   const { id, value, isActiv, disabled, children, errorMessage, icon, label, align, underline = true, autoWidth, placeholder } = props;
+  const [isInitial, setIsInitial] = useState(false);
 
+  // Calculate the color state for the label and underline
   const colorStateLabel = calcColorState({ type: 'label', isActiv, errorMessage, value, placeholder });
   const colorStateUnderline = calcColorState({ type: 'underline', isActiv, errorMessage, value, placeholder });
 
+  // Set the initial state of the input field
+  useEffect(() => {
+    if (isActiv) setIsInitial(true);
+  }, [isActiv]);
+
+  // Render the InputWrapper component with the appropriate props
   return (
     <StyledInputWrapper disabled={disabled} $autoWidth={autoWidth}>
       {icon && (
@@ -61,22 +76,22 @@ export default function InputWrapper(props: IInputWrapper) {
       )}
       <InputContainer $givePadding={Boolean(label)}>
         {children}
-        {/* the label for the input field it shows when a label prop exists*/}
+        {/* Render the label for the input field if a label prop exists */}
         {label && (
           <AnimatedInputLabel
             htmlFor={id}
             $align={align}
-            $moveUp={Boolean(value) || isActiv || Boolean(placeholder)}
+            $moveUp={Boolean((isInitial && value === 0 ? undefined : value) || Boolean(isInitial && value === 0)) || isActiv || Boolean(placeholder)}
             $colorState={colorStateLabel}
           >
             {label}
           </AnimatedInputLabel>
         )}
+        {/* Render the underline for the input field if the underline prop is true */}
         {underline && <FancyInputUnderline colorState={colorStateUnderline} />}
       </InputContainer>
-      {/* // ---------if a errorMessage prop exists this message will shown------------- // */}
+      {/* Render the error message if an errorMessage prop exists */}
       {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-      {/* // --------------------------------------------------------------------------- // */}
     </StyledInputWrapper>
   );
 }
