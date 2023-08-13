550d13e6f2948e4ce2ae9aac79b16ce4ae66e068
MrTRyy <tobiastry@aol.de>
2023-08-13 22:53:31 +0200

    cleanUp: make it better

--- src/Components/UI/Molecules/NumberInput/NumberInput.tsx
+++ src/Components/UI/Molecules/NumberInput/NumberInput.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useState } from 'react';
 import { TRawInputAlign } from '../../Atoms/RawInput/RawInput';
 import StyledNumberInput from './NumberInput.styled';
 
@@ -9,25 +9,31 @@ export interface INumberInput extends InputHTMLAttributes<HTMLInputElement> {
   align?: TRawInputAlign;
   activeHandler?: (value: boolean) => void;
 }
-
+// --------------------------------------------------------------------------- //
+// ------------ A simple number input with some extra features ---- ---------- //
+// --------------------------------------------------------------------------- //
 export default function NumberInput(props: INumberInput) {
   const { value, onChange, activeHandler, errorMessage, align, id, autoWidth, max, min, ...moreHTMLProps } = props;
+  const [initialValue, setInitialValue] = useState(true);
 
-  const maxValue = typeof(max) === 'string' ? Number(max) : max;
-  const minValue = typeof(min) === 'string' ? Number(min) : min;
-
+  // This is used to make sure that the value is a number
+  const maxValue = typeof max === 'string' ? Number(max) : max;
+  const minValue = typeof min === 'string' ? Number(min) : min;
 
   // This function is used to make sure that the value is within the min and max value
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInitialValue(false);
     let newValue = Number(e.target.value);
 
-
-    if (maxValue !== undefined && newValue > maxValue) {
-      newValue = maxValue;
-    } else if (minValue !== undefined && newValue < minValue) {
-      newValue = minValue;
+    if (value !== undefined) {
+      if (maxValue !== undefined && newValue > maxValue) {
+        newValue = maxValue;
+      } else if (minValue !== undefined && newValue < minValue) {
+        newValue = minValue;
+      }
     }
 
+    // give back the event to the parent
     if (onChange) {
       e.target.value = newValue.toString();
       onChange(e);
@@ -39,7 +45,7 @@ export default function NumberInput(props: INumberInput) {
       id={id}
       type="number"
       required
-      value={value}
+      value={initialValue ? (value === 0 ? '' : value) : value}
       onChange={handleChange}
       min={minValue}
       max={maxValue}
