fd2f0d51fc2ddcc8f35ce2f72ad953b49433b742
MrTRyy <tobiastry@aol.de>
2023-08-13 16:24:37 +0200

    update: animated Label

--- src/Components/UI/Atoms/AnimatedLabel/AnimatedLabel.style.tsx
+++ src/Components/UI/Atoms/AnimatedLabel/AnimatedLabel.style.tsx
@@ -1,25 +1,62 @@
 import styled, { css } from 'styled-components';
 import { colorPalet, uiColors } from '../../Design/design';
-import { alignHandler } from '../../HelperFunctions/designFunctions/alignmentHandler';
-
 import InputLabel from '../InputLabel/InputLabel';
 
+const activeHandler = (align: string, $moveUp?: boolean) => {
+  if (align !== 'center') {
+    return css`
+      bottom: 0;
+      left: 0;
+      ${$moveUp
+        ? css`
+            transform: translateY(-20px);
+          `
+        : ''}
+    `;
+  } else {
+    return css`
+      bottom: 0;
+      left: 50%;
+      text-align: center;
+      ${$moveUp
+        ? css`
+            transform: translateY(-20px) translate(-50%);
+          `
+        : 'transform: translate(-50%);'};
+    `;
+  }
+};
+
 //the animated label is for the inputs where the Placholder is the label
-export const AnimatedInputLabel = styled(InputLabel)<{ $align?: string; $disabledAndSelected?: boolean }>`
+interface IAnimatedInputLabel {
+  $align?: 'center' | 'left';
+  $moveUp?: boolean;
+  $colorState?: 'error' | 'active' | 'default';
+}
+
+export const AnimatedInputLabel = styled(InputLabel)<IAnimatedInputLabel>`
   position: absolute;
   padding: 12px 0 5px;
-  ${({ $align, $disabledAndSelected }) => alignHandler($align!, 'LabelInput', $disabledAndSelected)};
+  color: ${({ $colorState }) => {
+    switch ($colorState) {
+      case 'error':
+        return colorPalet.red_light;
+      case 'active':
+        return uiColors.accent.main;
+      default:
+        return uiColors.secondary.darkest;
+    }
+  }};
+
+  ${({ $align, $moveUp }) => activeHandler($align!, $moveUp)};
 `;
 
 export const AnimatedLabelFocusStyle = ($align?: 'center' | 'left', $errorMessage?: string) => {
   const isErrorMessage = $errorMessage ? colorPalet.red_dark : uiColors.accent.main;
-  const alignStyle = $align !== 'center' ? 'translateY(-20px)' : 'translateY(-20px) translate(-50%)';
 
   //the focus animation for the Label
   return css`
     &:focus ~ ${AnimatedInputLabel}, &:valid ~ ${AnimatedInputLabel} {
-      color: ${isErrorMessage};
-      transform: ${alignStyle};
       font-weight: 600;
     }
   `;
