75588e3d96b10998db0461d7ebc100ed39cdd0f0
MrTRyy <tobiastry@aol.de>
2023-09-09 12:25:12 +0200

    upddate: make it simpler

--- src/Components/UI/Design/color/lighenColors.ts
+++ src/Components/UI/Design/color/lighenColors.ts
@@ -16,22 +16,21 @@ function generateColorVariations(baseColor: string, steps: number[]): string[] {
   });
 }
 
-
 const degreeSteps = [0, 5, 10, 15, 20, 30, 40, 50, 60, 70];
 const degreeStepsAccent = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45];
 
 function lightenColors(colorType: TColorTypes, color: string): string[] {
   switch (colorType) {
     case 'primary':
-      return generateColorVariations(color, degreeSteps).reverse();
+      return generateColorVariations(color, degreeSteps);
     case 'secondary': {
       const stepsUpdated = degreeSteps.map((step) => -step);
-      return generateColorVariations(color, stepsUpdated).reverse();
+      return generateColorVariations(color, stepsUpdated);
     }
     case 'accent':
-      return generateColorVariations(color, degreeStepsAccent).reverse();
+      return generateColorVariations(color, degreeStepsAccent);
     case 'accentDarken': {
-      const stepsUpdated = degreeStepsAccent.map((step) => -step);
+      const stepsUpdated = degreeStepsAccent.map((step) => -step).reverse();
       return generateColorVariations(color, stepsUpdated);
     }
   }
@@ -49,8 +48,8 @@ export default function generateColorSteps(colorType: TColorTypes, color: string
 
   //make array to object with keys but reversed order
   lightColors.forEach((color, index) => {
-    obj[(lightColors.length - index - 1) as StepKeys] = color;
-  })
+    obj[index  as StepKeys] = color;
+  });
 
   return obj;
 }
