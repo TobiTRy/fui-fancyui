0acd29bc0a7b0ee0694e46e90a92d7ec717f54ea
MrTRyy <tobiastry@aol.de>
2023-09-13 22:03:51 +0200

    update: color

--- src/Components/UI/Atoms/FancyCard/Card.model.ts
+++ src/Components/UI/Atoms/FancyCard/Card.model.ts
@@ -1,14 +1,16 @@
 import { IRoundedEdges } from '../../HelperFunctions/designFunctions/edgeCaluculation';
 import { spacingPx } from '../../Design/design';
-import { uiColors } from '../../Design/color/designColor';
 import { borderRadius } from '../../Design/design';
+import themeStore from '../../Design/color/themeStore';
+
+const theme = themeStore.getState().theme;
 
 type IScaling = '100%' | 'auto' | string;
 // the raw styling props for the card
 export interface StyledCardProps {
   height?: IScaling;
   width?: IScaling;
-  backgroundColor?: keyof typeof uiColors.primary;
+  backgroundColor?: keyof typeof theme.primary;
   radius?: keyof typeof borderRadius;
   roundedEdges?: IRoundedEdges;
   padding?: keyof typeof spacingPx | false;
