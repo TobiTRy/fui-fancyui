6ede880b9b5f76591d30cba601e8f54a88735166
MrTRyy <tobiastry@aol.de>
2023-09-14 11:45:05 +0200

    fix: svgs

--- src/Components/UI/SVGIcons/SVGChevronRight.tsx
+++ src/Components/UI/SVGIcons/SVGChevronRight.tsx
@@ -1,10 +1,8 @@
-import themeStore from '../Design/color/themeStore';
-
 const SVGChevronRight = (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
     <path
-      stroke={themeStore.getState().theme.secondary[0]}
-      strokeWidth="1"
+      strokeWidth={1}
+      stroke="currentColor"
       fillRule="evenodd"
       d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
     />
