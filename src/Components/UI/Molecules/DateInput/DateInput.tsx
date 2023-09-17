ae9048e9f45c090f0afdbd3a751b09cf33076119
MrTRyy <tobiastry@aol.de>
2023-09-17 21:04:30 +0200

    update: color for native icon

--- src/Components/UI/Molecules/DateInput/DateInput.tsx
+++ src/Components/UI/Molecules/DateInput/DateInput.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, InputHTMLAttributes } from 'react';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 
 import RawInput from '../../Atoms/RawInput/RawInput';
 import { TRawInputAlign } from '../../Atoms/RawInput/RawInput';
@@ -8,18 +8,23 @@ import { simpleColorTransition } from '../../Design/simpleTransition';
 import { TUiColorsType } from '../../Design/color/designColor';
 
 type IStyledDatePicker = IStyledPrefixAndPicker<IDateInputProps, 'errorMessage' | 'align'> & { value?: string };
-const StyledDatePicker = styled(RawInput)<IStyledDatePicker & {theme: TUiColorsType}>`
+const StyledDatePicker = styled(RawInput)<IStyledDatePicker & { theme: TUiColorsType }>`
   color: ${({ value }) => (value ? '' : 'transparent')};
 
   &:focus {
-    color: ${({theme}) => theme.secondary[0]};
+    color: ${({ theme }) => theme.secondary[0]};
   }
 
   &::-webkit-calendar-picker-indicator {
-    filter: invert(1);
-  }
+    ${simpleColorTransition}
 
-  ${simpleColorTransition}
+    ${({ theme }) => {
+      const svg = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" viewBox="0 0 24 24"><path fill=${theme.secondary[4]} d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"/></svg>`;
+      return css`
+        background-image: url(${svg});
+      `;
+    }}
+  }
 `;
 
 export interface IDateInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
@@ -29,7 +34,6 @@ export interface IDateInputProps extends Omit<InputHTMLAttributes<HTMLInputEleme
   activeHandler?: (value: boolean) => void;
   align?: TRawInputAlign;
 }
-
 // --------------------------------------------------------------------------- //
 // ---------- Here are the design variants for sizing and alignment ---------- //
 // --------------------------------------------------------------------------- //
--- src/Components/UI/Organisms/FancyDateInput/FancyDateInput.tsx
+++ src/Components/UI/Organisms/FancyDateInput/FancyDateInput.tsx
@@ -3,12 +3,12 @@ import React, { useId, useState } from 'react';
 import DateInput, { IDateInputProps } from '../../Molecules/DateInput/DateInput';
 import InputWrapper, { IInputWrapperUserInputProps } from '../../Molecules/InputWrapper/InputWrapper';
 
-type  IFancyDateInput =  IInputWrapperUserInputProps & IDateInputProps;
+type IFancyDateInput = IInputWrapperUserInputProps & IDateInputProps;
 // --------------------------------------------------------------------------- //
 // ----The TextInput Comonent with surrounding icon, label and underline ----- //
 // --------------------------------------------------------------------------- //
 export default function FancyDateInput(props: IFancyDateInput) {
-  const { id, value, label, icon, errorMessage, align,  disabled,  activeHandler, ...inputProps } = props;
+  const { id, value, label, icon, errorMessage, align, disabled, activeHandler, ...inputProps } = props;
 
   //the states activity of the input
   const [isActiv, setIsActive] = useState(false);
@@ -16,7 +16,7 @@ export default function FancyDateInput(props: IFancyDateInput) {
   // if no id is provided, generate a random one
   const useid = useId();
   const usedId = id ? id : useid;
-  
+
   // handles the focus and blur events and calls the handler from the parent
   const activeFocusHandler = (value: boolean) => {
     setIsActive(value);
@@ -24,14 +24,16 @@ export default function FancyDateInput(props: IFancyDateInput) {
   };
 
   return (
-    <InputWrapper id={usedId} value={value} label={label} disabled={disabled} align={align} isActiv={isActiv} icon={icon} errorMessage={errorMessage}>
-      <DateInput
-        id={usedId}
-        value={value}
-        activeHandler={activeFocusHandler}
-        align={align}
-        {...inputProps}
-      />
-    </InputWrapper>
+    <InputWrapper
+      id={usedId}
+      value={value}
+      label={label}
+      disabled={disabled}
+      align={align}
+      isActiv={isActiv}
+      icon={icon}
+      errorMessage={errorMessage}
+      InputElement={<DateInput id={usedId} value={value} activeHandler={activeFocusHandler} align={align} {...inputProps} />}
+    />
   );
 }
