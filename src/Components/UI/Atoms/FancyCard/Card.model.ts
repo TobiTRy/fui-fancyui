742796ddcfea4cdd66750f08bdb67a3b705bc7db
MrTRyy <tobiastry@aol.de>
2023-09-21 07:11:14 +0200

    update: new color variant

--- src/Components/UI/Atoms/FancyCard/Card.model.ts
+++ src/Components/UI/Atoms/FancyCard/Card.model.ts
@@ -1,18 +1,21 @@
 import { IRoundedEdges } from '../../HelperFunctions/designFunctions/edgeCaluculation';
 import { spacingPx } from '../../Design/design';
 import { borderRadius } from '../../Design/design';
-import { TthemeColorGroup } from '../../Design/color/designColor';
+import { TUiColorsType, TthemeColorGroup } from '../../Design/color/designColor';
 
 
 
 type IScaling = '100%' | 'auto' | string;
 // the raw styling props for the card
 export interface StyledCardProps {
+  themeType?: keyof TUiColorsType;  
+  variant?: number;
+  customColor?: string;
+  customTextColor?: string;
   height?: IScaling;
   width?: IScaling;
   layer?: number;
   shadow?: boolean;
-  backgroundColor?: string | TthemeColorGroup; 
   radius?: keyof typeof borderRadius;
   roundedEdges?: IRoundedEdges;
   padding?: keyof typeof spacingPx | false;
--- src/Components/UI/Atoms/FancyCard/FancyCard.style.tsx
+++ src/Components/UI/Atoms/FancyCard/FancyCard.style.tsx
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 
 import { StyledCardProps } from './Card.model';
 import IStyledPrefixAndOmiter from '../../Interface/IStyledPrefixAndOmiter.model';
@@ -9,16 +9,56 @@ import checkThemeOrColor from '../../Design/color/ckeckThemeOrColor';
 import { TUiColorsType } from '../../Design/color/designColor';
 import { boxShadow } from '../../Design/shadows';
 
+type IGetBackgroundColor = {
+  theme: TUiColorsType;
+  themeType: keyof TUiColorsType;
+  variant?: number;
+  customColor?: string;
+};
+function getBackgroundColor({ theme, themeType, customColor, variant }: IGetBackgroundColor) {
+  if (customColor) return customColor;
+  if (variant) return theme[themeType][variant || 0];
+  return theme[themeType][0];
+}
+
+type IGetTextColor = {
+  theme: TUiColorsType;
+  themeType: keyof TUiColorsType;
+  customTextColor?: string;
+};
+function getTextColor({ theme, themeType, customTextColor }: IGetTextColor) {
+  if (customTextColor) return customTextColor;
+  return theme[themeType][0];
+}
+
+type IGetColorForComponent = {
+  theme: TUiColorsType;
+  themeType: keyof TUiColorsType;
+  customColor?: string;
+  customTextColor?: string;
+  variant?: number;
+};
+
+function getColorForComponent({ theme, themeType, customColor, customTextColor, variant }: IGetColorForComponent) {
+  const backgroundColor = getBackgroundColor({ theme, themeType, customColor, variant });
+  const textColor = getTextColor({ theme, themeType, customTextColor });
+
+  return css`
+    background-color: ${backgroundColor};
+    color: ${textColor};
+  `;
+}
+
 // the converted $ styling props for the card
 type IStyledCard = IStyledPrefixAndOmiter<StyledCardProps>;
 //the main design of the card
 export const StyledCard = styled.div<IStyledCard & { theme: TUiColorsType }>`
+  ${({ theme, $themeType, $customColor, $customTextColor, $variant }) =>
+    getColorForComponent({ theme, themeType: $themeType, customColor: $customColor, customTextColor: $customTextColor, variant: $variant })}
   overflow: hidden;
   width: ${({ $width }) => $width};
   height: ${({ $height }) => $height};
   box-sizing: border-box;
-  background-color: ${({ theme, $backgroundColor }) =>
-    $backgroundColor ? checkThemeOrColor($backgroundColor) : theme.primary[0]};
   padding: ${({ $padding }) => ($padding ? spacingPx[$padding] : '')};
   border-radius: ${({ $roundedEdges, $radius }) => edgeCalculation($roundedEdges, $radius)};
   ${({ $shadow }) => $shadow && boxShadow.sm};
