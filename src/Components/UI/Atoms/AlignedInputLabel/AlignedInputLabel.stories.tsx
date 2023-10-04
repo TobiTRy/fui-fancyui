df583a253d0b443c09527518ad4d341ba139e4f6
MrTRyy <tobiastry@aol.de>
2023-10-04 09:13:02 +0200

    update: working on mac and windos

--- src/Components/UI/Atoms/AlignedInputLabel/AlignedInputLabel.stories.tsx
+++ src/Components/UI/Atoms/AlignedInputLabel/AlignedInputLabel.stories.tsx
@@ -31,8 +31,7 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
-  args: {
-  },
+  args: {},
   render: (args) => (
     <AlignedInputLabel {...args}>
       <Typography type="label">Hello World</Typography>
