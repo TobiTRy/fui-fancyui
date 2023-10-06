ff69060f38d0ea40ce4c34e6cd70bbebb7e1c068
MrTRyy <tobiastry@aol.de>
2023-10-06 23:36:16 +0200

    buildIn: Story

--- src/Components/UI/Molecules/DateOutputFromTo/DateOutputFromTo.stories.tsx
+++ src/Components/UI/Molecules/DateOutputFromTo/DateOutputFromTo.stories.tsx
@@ -10,12 +10,26 @@ const meta = {
     docs: {
       description: {
         component:
-          'Dumb-Comonent: The DateNumberWithStatus is a component that is a mix of DateNumberAtom and AvailableDot. <br> It is used in the calendar. (FancyDatepicker)',
+          'Dumb-Comonent: DateOutputFromTo is a component that renders two DateOutput components and a FancyVR component in between. It is used to display a date range. It is used in the DateRangePicker component.',
       },
     },
   },
   argTypes: {
-
+    layer: {
+      control: {
+        type: 'range', min: 0, max: 10, step: 1,
+      },
+    },
+    themeType: {
+      control: {
+        type: 'select',
+      },
+    },
+    whichIsSelecting: {
+      control: {
+        type: 'radio',
+      },
+    },
   },
   tags: ['autodocs'],
 } satisfies Meta<typeof DateOutputFromTo>;
@@ -26,5 +40,11 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   render: (args) => <DateOutputFromTo {...args} />,
   args: {
+    dateFrom: new Date(),
+    dateTo: new Date(),
+    handler: (wich: 'from' | 'to') => console.log(wich),
+    whichIsSelecting: 'from',
+    themeType: 'primary',
+    layer: 2,
   },
 };
