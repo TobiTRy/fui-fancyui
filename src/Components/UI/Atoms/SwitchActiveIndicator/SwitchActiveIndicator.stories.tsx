0cb6248520a44ab9b6adb9945a86b5dffff4246e
MrTRyy <tobiastry@aol.de>
2023-10-11 10:49:04 +0200

    update: SwitchIndicator

--- src/Components/UI/Atoms/SwitchActiveIndicator/SwitchActiveIndicator.stories.tsx
+++ src/Components/UI/Atoms/SwitchActiveIndicator/SwitchActiveIndicator.stories.tsx
@@ -17,12 +17,25 @@ const meta = {
   },
   // Define arguments for the story
   argTypes: {
+    $type: {
+      description: 'The type of the indicator, it can be a bolb or a underline',
+      control: {
+        type: 'radio',
+      },
+      options: ['bolb', 'underline'],
+      defaultValue: {
+        summary: 'bolb',
+      }
+    },
     $direction: {
       description: 'The direction of the indicator where it should be moved horizontal or vertical',
       control: {
         type: 'radio',
       },
       options: ['horizontal', 'vertical'],
+      defaultValue: {
+        summary: 'horizontal',
+      }
     },
     $itemNumber: {
       description: 'The itemnumber is to calc the position of the indicator for each item',
@@ -35,27 +48,38 @@ const meta = {
       control: {
         type: 'select',
       },
-
       options: ['sm', 'md', 'lg', 'xl', 'xxl', 'xxxl', 'complete'],
+      defaultValue: {
+        summary: 'md',
+      }
     },
     $tabSpacing: {
       description: 'The spacing is to calc with the offset the position of the indicator for each item',
       control: {
         type: 'select',
       },
+      defaultValue: {
+        summary: '',
+      
+      }
     },
     $outlined: {
       description: 'The outlined is to set the border of the indicator',
       control: {
         type: 'boolean',
       },
+      defaultValue: {
+        summary: false,
+      }
     },
     $themeType: {
       description: 'The themeType is to set the color of the indicator',
       control: {
         type: 'select',
       },
-      options: ['primary', 'secondary', 'accent', 'transparent'],
+      defaultValue: {
+        summary: 'accent',
+      }
     },
   },
   // Add tags to the story
---
+++ src/Components/UI/Atoms/SwitchActiveIndicator/SwitchActiveIndicator.style.tsx
@@ -0,0 +1,116 @@
+import styled, { css } from 'styled-components';
+import { TUiColorsType } from '../../Design/color/designColor';
+import { spacing, borderRadius } from '../../Design/design';
+import { IActiveSwitchIndicator } from './SwitchActiveIndicator';
+import colorTransparencyCalculator from '../../Design/color/colorTransparencyCalculator';
+import { getBackgroundColor } from '../../Design/color/colorCalculatorForComponet';
+
+// Define the function to generate a blob background for the active indicator
+type IGenerateBlob = Pick<IActiveSwitchIndicator, '$themeType' | '$outlined' | '$rounded' | '$layer' > & { theme: TUiColorsType };
+const generateBlob = (props: IGenerateBlob) => {
+  const { $themeType, theme, $rounded, $outlined, $layer } = props;
+  let backgroundStyle;
+
+  // Get the background color for the active indicator
+  const backgroundColor = getBackgroundColor({ theme, $themeType: $themeType || 'accent', $layer: $layer || 0 });
+
+  // Check if the provided rounded value is a valid key in the borderRadius object
+  const isRadiusKey = $rounded ? Object.keys(borderRadius).includes($rounded) : false;
+
+  // If the outlined prop is true, generate a slightly transparent background color and a border
+  if ($outlined) {
+    const generateSlightBackgroundColor = colorTransparencyCalculator(
+      getBackgroundColor({ theme, $themeType: $themeType || 'accent', $layer: $layer || 3 }),
+      0.2
+    );
+
+    backgroundStyle = css`
+      background-color: ${generateSlightBackgroundColor};
+      border: 1px solid ${backgroundColor};
+      box-sizing: border-box;
+    `;
+  } else {
+    // If the outlined prop is false, use the background color as the background style
+    backgroundStyle = css`
+      background-color: ${backgroundColor};
+    `;
+  }
+
+  // Return the styled-component CSS for the active indicator
+  return css`
+    top: 0;
+    height: 100%;
+    border-radius: ${isRadiusKey ? borderRadius[$rounded as keyof typeof borderRadius] : $rounded};
+    ${backgroundStyle}
+  `;
+};
+
+// Define the function to generate an underline for the active indicator
+type IGenerateUnderline = Pick<IActiveSwitchIndicator, '$themeType' |'$layer'> & { theme: TUiColorsType };
+const generateUnderline = ({ theme, $themeType = 'accent', $layer }: IGenerateUnderline) => {
+  // Get the background color for the active indicator
+  const backgroundColor = getBackgroundColor({ theme, $themeType, $layer });
+
+  // Return the styled-component CSS for the active indicator
+  return css`
+    top: 0;
+    box-sizing: border-box;
+    height: 100%;
+    border-bottom: 1px solid ${backgroundColor};
+    border-radius: 0;
+  `;
+};
+
+// Define the function to calculate the current position of the active indicator
+type IClacCurrentPosition = Pick<IActiveSwitchIndicator, '$itemNumber' | '$tabSpacing' | '$direction'>;
+const clacCurrentPosition = (props: IClacCurrentPosition) => {
+  const { $itemNumber, $tabSpacing, $direction } = props;
+
+  // Calculate the position in percent
+  const itemPosition = ($itemNumber - 1) * 100 + '%';
+
+  // Calculate the gap between the items
+  const gapSpacing = $tabSpacing ? ($itemNumber - 1) * spacing[$tabSpacing] : 0;
+
+  // Calculate the current position of the active indicator
+  const currentPosition = $itemNumber
+    ? css`
+        ${$direction === 'vertical'
+          ? css`
+              transform: translateY(calc(${itemPosition} + ${gapSpacing + 'px'}));
+            `
+          : css`
+              transform: translateX(calc(${itemPosition} + ${gapSpacing + 'px'}));
+            `}
+      `
+    : css`
+        transform: translateX(0);
+      `;
+
+  // Return the styled-component CSS for the active indicator
+  return currentPosition;
+};
+
+// --------------------------------------------------------------------------- //
+// -------- Here is the main Generator Function of the activ indicator ------- //
+// --------------------------------------------------------------------------- //
+export const ActiveSwitchIndicator = styled.i<IActiveSwitchIndicator & { theme: TUiColorsType }>`
+  position: absolute;
+  left: 0;
+  width: 100%;
+
+  /* Build a switch case */
+  ${(props) => {
+    switch (props.$type) {
+      case 'underline':
+        return generateUnderline({ ...props });
+      case 'bolb':
+      default:
+        return generateBlob({ ...props });
+    }
+  }}
+
+/* Calculate the current position of the active indicator */
+${({ $itemNumber, $tabSpacing, $direction }) => clacCurrentPosition({ $itemNumber, $tabSpacing, $direction })}
+  transition: transform 0.2s ease;
+`;
