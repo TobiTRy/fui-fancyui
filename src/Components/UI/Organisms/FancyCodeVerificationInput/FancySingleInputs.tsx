99698f37a73e35b935ac5adb8e89111986a2b1f0
MrTRyy <tobiastry@aol.de>
2023-10-13 08:55:08 +0200

    cleanUp: FancysingleInputs

--- src/Components/UI/Organisms/FancyCodeVerificationInput/FancySingleInputs.tsx
+++ src/Components/UI/Organisms/FancyCodeVerificationInput/FancySingleInputs.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+
 import SingleInputs from '../../Molecules/SingleInputs/SingleInputs';
 import { Container, WarpperComponent, Message, MessageContainer } from './FancySingleInputs.style';
 import { Typography } from '../../Atoms/Typography';
@@ -8,42 +9,26 @@ import { Typography } from '../../Atoms/Typography';
 // --------------------------------------------------------------------------- //
 interface IFancySingleInputs {
   length?: number;
-  keyCode?: string;
+  errorMessage?: string;
+  isSuccess?: boolean;
   handler?: (value: string) => void;
+  automaticCase?: 'upper' | 'lower';
+  debounceTime?: number;
 }
 export default function FancySingleInputs(props: IFancySingleInputs) {
-  const { length, keyCode, handler } = { ...defaultProps, ...props };
-  const [status, setStatus] = useState({ isError: false, isSucceed: false });
+  const { length, errorMessage, isSuccess, handler, automaticCase, debounceTime } = { ...defaultProps, ...props };
   const [inputValue, setInputValue] = useState('');
   const debounceTimeoutRef = useRef<NodeJS.Timeout>();
 
-  // this function validates the input key with the api
-  const validateValue = () => {
-    if (keyCode === inputValue) {
-      setStatus({
-        isError: false,
-        isSucceed: true,
-      });
-    } else {
-      setStatus({
-        isError: true,
-        isSucceed: false,
-      });
-    }
-  };
-
   // hanldes the input value change and validate it with the api
   useEffect(() => {
-    setStatus({ isError: false, isSucceed: false });
     if (inputValue.length === length) {
       if (debounceTimeoutRef.current) {
         clearTimeout(debounceTimeoutRef.current);
-        
       }
-      debounceTimeoutRef.current = setTimeout(validateValue, 700);
+      debounceTimeoutRef.current = setTimeout(() => handler && handler(inputValue), debounceTime || 700);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inputValue, keyCode]);
+  }, [inputValue, handler, length, debounceTime]);
 
   const valueHandler = (value: string) => {
     setInputValue(value);
@@ -51,14 +36,16 @@ export default function FancySingleInputs(props: IFancySingleInputs) {
 
   return (
     <WarpperComponent>
-      <Container $status={status}>
-        <SingleInputs length={length} handler={valueHandler} status={status} />
+      <Container>
+        <SingleInputs
+          length={length}
+          handler={valueHandler}
+          status={{ isError: Boolean(errorMessage), isSucceed: Boolean(isSuccess) }}
+          automaticCase={automaticCase}
+        />
         <MessageContainer>
-          <Message $status={status.isSucceed}>
-            <Typography type="content">Value is valid!</Typography>
-          </Message>
-          <Message $status={status.isError}>
-            <Typography type="content">An error occurred, please try again.</Typography>
+          <Message $isError={Boolean(errorMessage)}>
+            <Typography type="content">{errorMessage}</Typography>
           </Message>
         </MessageContainer>
       </Container>
@@ -68,5 +55,4 @@ export default function FancySingleInputs(props: IFancySingleInputs) {
 
 const defaultProps: IFancySingleInputs = {
   length: 6,
-  keyCode: '123456',
 };
