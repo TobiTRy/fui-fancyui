bf51340476c5f078f222944e03f4105dc22ce938
MrTRyy <tobiastry@aol.de>
2023-10-23 15:59:24 +0200

    buildIn: fancyContent

---
+++ src/Components/UI/Molecules/FancyContent/FancyContent.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Typography, { TypographyList } from '../../Atoms/Typography/Typography';
+import FancySVGAtom from '../../Atoms/FancySVGAtom/FancySVGAtom';
+import styled from 'styled-components';
+import { spacingPx } from '../../Design/design';
+
+const sizes = {
+  sm: {
+    fontSize: 'smText' as keyof typeof TypographyList,
+    padding: spacingPx.xs,
+  },
+  md: {
+    fontSize: 'content' as keyof typeof TypographyList,
+    padding: spacingPx.sm,
+  },
+  lg: {
+    fontSize: 'button' as keyof typeof TypographyList,
+    padding: spacingPx.md,
+  },
+};
+
+type TWrapper = Pick<IFancyContentProps, 'flexDirection' | 'flexAlign' | 'flexJustify'>;
+const Wrapper = styled.span<TWrapper>`
+  display: flex;
+  flex-direction: ${({ flexDirection }) => flexDirection || 'row'};
+  justify-content: ${({ flexJustify }) => flexJustify || 'center'};
+  align-items: ${({ flexAlign }) => flexAlign || 'center'};
+  gap: ${spacingPx.xs};
+`;
+
+interface IFancyContentProps {
+  text?: string;
+  textType?: typeof TypographyList;
+  icon?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';
+  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  flexJustify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+  flexAlign?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+}
+export default function FancyContent(props: IFancyContentProps) {
+  const { text, icon, flexDirection, flexAlign, size } = props;
+
+  return (
+    <>
+      {text && (
+        <Wrapper flexAlign={flexAlign} flexDirection={flexDirection}>
+          {icon && (
+            <FancySVGAtom size={size} externalStyle={{ flexShrink: '0' }} isPassive>
+              {icon}
+            </FancySVGAtom>
+          )}
+          {text && (
+            <Typography variant={sizes[size || 'sm'].fontSize} weight="bold" type="button">
+              {text}
+            </Typography>
+          )}
+        </Wrapper>
+      )}
+      {(icon && !text) && icon}
+    </>
+  );
+}
