0ef7a90e5cbf8c7082d53664a5bf61cad30d24aa
MrTRyy <tobiastry@aol.de>
2023-10-23 15:58:30 +0200

    remove: svgatom from LoadingSVG arrows

--- src/Components/UI/Atoms/LoadingSVGArrows/LoadingSVGArrows.tsx
+++ src/Components/UI/Atoms/LoadingSVGArrows/LoadingSVGArrows.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import { css, keyframes } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import SVGLoadingArrows from '../../SVGIcons/SVGLoadingArrows';
-import { FancySVGAtom } from '../FancySVGAtom';
-import { ISVGAtomProps, ISizes } from '../FancySVGAtom/FancySVGAtom.model';
+import { ISVGAtomProps } from '../FancySVGAtom/FancySVGAtom.model';
 
 const loadingAnimation = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 `;
 
+const AnimatedSVGArrwos = styled.span`
+  animation: ${loadingAnimation} 2s infinite ease-in-out;
+  line-height: 0;
+`;
+
 interface ILoadingSVGArrowsProps {
-  size?: ISizes;
   isLoading?: boolean;
 }
 
@@ -19,19 +22,11 @@ interface ILoadingSVGArrowsProps {
 //  A loading animation with two arrows thats shown when something is loading  //
 // --------------------------------------------------------------------------- //
 export default function LoadingSVGArrows(props: ILoadingSVGArrowsProps & ISVGAtomProps) {
-  const { size = 'md', isLoading, externalStyle, ...SVGAtomProps } = props;
+  const { isLoading } = props;
 
   return isLoading ? (
-    <FancySVGAtom
-      size={size}
-      isPassive
-      externalStyle={css`
-        animation: ${loadingAnimation} 2s infinite ease-in-out;
-        ${externalStyle}
-      `}
-      {...SVGAtomProps}
-    >
+    <AnimatedSVGArrwos>
       <SVGLoadingArrows />
-    </FancySVGAtom>
+    </AnimatedSVGArrwos>
   ) : null;
 }
