a72740f386c9c88450c218a1dae586fff7906740
MrTRyy <tobiastry@aol.de>
2023-10-26 15:39:29 +0200

    split: indicator geneartion style

--- src/Components/UI/Atoms/SwitchActiveIndicator/SwitchActiveIndicator.tsx
+++ src/Components/UI/Atoms/SwitchActiveIndicator/SwitchActiveIndicator.tsx
@@ -3,22 +3,27 @@ import React from 'react';
 import { TUiColorsType } from '../../Design/color/designColor';
 import { IBorderRadius, spacingPx } from '../../Design/design';
 import { TLayer } from '../../Design/color/generateColorSteps';
-import { ActiveSwitchIndicator } from './SwitchActiveIndicator.style';
-
+import { ActiveSwitchIndicator, Wrapper } from './SwitchActiveIndicator.style';
 
 export interface IActiveSwitchIndicator {
   $itemNumber: number;
   $themeType?: keyof TUiColorsType;
   $layer?: TLayer;
   $tabSpacing?: keyof typeof spacingPx;
-  $type?: 'bolb' | 'underline';
+  $type?: 'bolb' | 'underline' | 'topline';
   $rounded?: keyof IBorderRadius | string;
   $outlined?: boolean;
   $direction?: 'horizontal' | 'vertical';
+  $width?: string;
 }
 // --------------------------------------------------------------------------- //
 // -------- Create a Incator for wich Item in a Switch List ist activ  ------- //
 // --------------------------------------------------------------------------- //
 export default function SwitchActiveIndicator(props: IActiveSwitchIndicator) {
-  return <ActiveSwitchIndicator {...props} />;
+  const { $itemNumber, $tabSpacing, $direction, ...switchProps } = props;
+  return (
+    <Wrapper $itemNumber={$itemNumber} $tabSpacing={$tabSpacing} $direction={$direction}>
+      <ActiveSwitchIndicator {...switchProps} />
+    </Wrapper>
+  );
 }
---
+++ src/Components/UI/Atoms/SwitchActiveIndicator/utils/generateBlob.ts
@@ -0,0 +1,46 @@
+import { css } from "styled-components";
+import { getBackgroundColor } from "../../../Design/color/colorCalculatorForComponet";
+import colorTransparencyCalculator from "../../../Design/color/colorTransparencyCalculator";
+import { TUiColorsType } from "../../../Design/color/designColor";
+import { IActiveSwitchIndicator } from "../SwitchActiveIndicator";
+import { borderRadius } from "../../../Design/design";
+
+// Define the function to generate a blob background for the active indicator
+type IGenerateBlob = Pick<IActiveSwitchIndicator, '$themeType' | '$outlined' | '$rounded' | '$layer'> & { theme: TUiColorsType };
+export const generateBlob = (props: IGenerateBlob) => {
+  const { $themeType, theme, $rounded, $outlined, $layer } = props;
+  let backgroundStyle;
+
+  // Get the background color for the active indicator
+  const backgroundColor = getBackgroundColor({ theme, $themeType: $themeType ?? 'accent', $layer: $layer ?? 0 });
+
+  // Check if the provided rounded value is a valid key in the borderRadius object
+  const isRadiusKey = $rounded ? Object.keys(borderRadius).includes($rounded) : false;
+
+  // If the outlined prop is true, generate a slightly transparent background color and a border
+  if ($outlined) {
+    const generateSlightBackgroundColor = colorTransparencyCalculator(
+      getBackgroundColor({ theme, $themeType: $themeType ?? 'accent', $layer: $layer ?? 3 }),
+      0.2
+    );
+
+    backgroundStyle = css`
+      background-color: ${generateSlightBackgroundColor};
+      border: 1px solid ${backgroundColor};
+      box-sizing: border-box;
+    `;
+  } else {
+    // If the outlined prop is false, use the background color as the background style
+    backgroundStyle = css`
+      background-color: ${backgroundColor};
+    `;
+  }
+
+  // Return the styled-component CSS for the active indicator
+  return css`
+    top: 0;
+    height: 100%;
+    border-radius: ${isRadiusKey ? borderRadius[$rounded as keyof typeof borderRadius] : $rounded};
+    ${backgroundStyle}
+  `;
+};
