6a46b8810d22f67cc82a5c5e1a545d67b37f6704
MrTRyy <tobiastry@aol.de>
2023-10-27 22:16:13 +0200

    update: make fancy button realy fancy

--- src/Components/UI/Organisms/FancyButton/FancyButton.tsx
+++ src/Components/UI/Organisms/FancyButton/FancyButton.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 
 import LoadingSVGArrows from '../../Atoms/LoadingSVGArrows/LoadingSVGArrows';
+import { borderRadius } from '../../Design/design';
 import Button, { IButtonProps } from '../../Molecules/Button/Button';
 import FancyContent from '../../Molecules/FancyContent/FancyContent';
+import { generateFancyButton } from './FancyButton.style';
+import { css } from 'styled-components';
 
-type IFancyButtonProps = {
+export type IFancyButtonProps = {
   isLoading?: boolean;
   label?: string;
   align?: 'left' | 'right' | 'center';
   iconAlign?: 'left' | 'right';
+  size?: 'sm' | 'md' | 'lg';
+  borderRadius?: keyof typeof borderRadius;
+  oneToOne?: boolean;
   icon?: React.ReactNode;
 };
+
 // --------------------------------------------------------------------------- //
 // ---------- The Fancy Button has a bit more options than another  ---------- //
 // --------------------------------------------------------------------------- //
 type IFancyButton = IFancyButtonProps & IButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
 export default function FancyButton(props: IFancyButton) {
-  const { icon, label, isLoading, iconAlign, size, ...ButtonProps } = {
+  const { icon, label, isLoading, iconAlign, size, externalStyle, ...ButtonProps } = {
     ...defaultProps,
     ...props,
   };
+
+  const generateFancyStyle = generateFancyButton({
+    size,
+    borderRadius: props.borderRadius,
+    oneToOne: Boolean(!label) && Boolean(icon),
+    outlined: props.outlined,
+  });
 
   // hanlde loadingstate with
   const showIcon = icon && !isLoading;
@@ -27,7 +41,11 @@ export default function FancyButton(props: IFancyButton) {
   const alignIcon = iconAlign === 'left' ? 'row' : 'row-reverse';
 
   return (
-    <Button size={size} oneToOne={Boolean(!label) && Boolean(icon)} {...ButtonProps}>
+    <Button
+      size={size}
+      externalStyle={css`${generateFancyStyle} ${externalStyle}`}
+      {...ButtonProps}
+    >
       <FancyContent
         flexDirection={alignIcon}
         text={label}
