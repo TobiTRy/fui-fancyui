853d5cb0b381434f663d576fccf7a648d4f445a8
MrTRyy <tobiastry@aol.de>
2023-10-27 22:16:50 +0200

    make button more like a simple button

--- src/Components/UI/Molecules/Button/Button.style.tsx
+++ src/Components/UI/Molecules/Button/Button.style.tsx
@@ -1,22 +1,8 @@
-import { css, styled } from 'styled-components';
-import { IGenerateThemeDesignForComponent } from '../../HelperFunctions/designFunctions/generateThemeDesignForComponent/generateThemeDesignForComponent';
-import { generateComponentPadding } from '../../HelperFunctions/designFunctions/generatePaddingForComponent/generatepaddingForComponent';
-import { generateBorderRadiusForComponent } from '../../HelperFunctions/designFunctions/generateBorderRadiusForComponent/generateBorderRadiusForComponent';
-import IStyledPrefixAndPicker from '../../Interface/IStyledPrefixAndPicker.model';
+import { styled } from 'styled-components';
 import { disabledStyle } from '../../HelperFunctions/designFunctions/disableStyle';
+import generateThemeDesignForComponent, { IGenerateThemeDesignForComponent } from '../../HelperFunctions/designFunctions/generateThemeDesignForComponent/generateThemeDesignForComponent';
+import IStyledPrefixAndPicker from '../../Interface/IStyledPrefixAndPicker.model';
 import { IButtonProps } from './Button';
-import generateThemeDesignForComponent from '../../HelperFunctions/designFunctions/generateThemeDesignForComponent/generateThemeDesignForComponent';
-
-const generate1To1Button = ($size: 'sm' | 'md' | 'lg', $outlined?: boolean) => {
-  //this makes the button a square (1/1) if there is no $label and a $icon
-  const padding = generateComponentPadding({ size: $size ?? 'md', borderThinkness: $outlined ? 1.2 : 0 });
-
-  return css`
-    aspect-ratio: 1/1;
-    justify-content: center;
-    ${padding}
-  `;
-};
 
 export const StyledButton = styled.button<IGenerateThemeDesignForComponent & IStyledPrefixAndPicker<IButtonProps>>`
   display: inline-flex;
@@ -29,15 +15,10 @@ export const StyledButton = styled.button<IGenerateThemeDesignForComponent & ISt
   min-width: fit-content;
   transition: background-color 0.125s ease-in-out;
 
-  ${({ $size, $borderRadius }) => generateBorderRadiusForComponent($size, $borderRadius)};
-
   ${(props: IGenerateThemeDesignForComponent) => generateThemeDesignForComponent({ ...props, $backgroundState: 'hover' })};
-  ${({ $size, $outlined }) =>
-    generateComponentPadding({ size: $size ?? 'md', borderThinkness: $outlined ? 1.2 : 0, doublePaddingLeftRight: true })}
+
   ${({ $externalStyle }) => $externalStyle && $externalStyle}
 
-  ${({ $size, $outlined, $oneToOne }) => $oneToOne && generate1To1Button($size ?? 'md', $outlined)};
-  
   &:disabled {
     ${disabledStyle}
   }
--- src/Components/UI/Molecules/Button/Button.tsx
+++ src/Components/UI/Molecules/Button/Button.tsx
@@ -1,48 +1,38 @@
 import React from 'react';
 import { CSSProp } from 'styled-components';
 
-import { borderRadius } from '../../Design/design';
 import { IGenerateThemeDesignForComponentProps } from '../../HelperFunctions/designFunctions/generateThemeDesignForComponent/generateThemeDesignForComponent';
 import { StyledButton } from './Button.style';
 
 export type IButtonProps = {
   size?: 'sm' | 'md' | 'lg';
   wide?: boolean;
-  borderRadius?: keyof typeof borderRadius;
   children?: React.ReactNode;
   externalStyle?: CSSProp;
-  oneToOne?: boolean;
   disabled?: boolean;
-} & IGenerateThemeDesignForComponentProps
-
+} & IGenerateThemeDesignForComponentProps;
 
 type ButtonHTML = React.ButtonHTMLAttributes<HTMLButtonElement>;
 type AnchorHTML = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 // Using conditional type based on the 'as' prop
-export type IButton = IButtonProps & (
-  { as?: 'button' } & ButtonHTML | 
-  { as: 'a' } & AnchorHTML
-) ;
+export type IButton = IButtonProps & (({ as?: 'button' } & ButtonHTML) | ({ as: 'a' } & AnchorHTML));
 // --------------------------------------------------------------------------- //
 // --------------- A normal Button with Style from the Theme ----------------- //
 // --------------------------------------------------------------------------- //
 export default function Button(props: IButton) {
-  const { children, size, wide, borderRadius, themeType, layer, textColor, hoverColor, outlined, externalStyle, oneToOne, ...htmlProps } = {
+  const { children, wide, themeType, layer, textColor, hoverColor, outlined, externalStyle, ...htmlProps } = {
     ...defaultProps,
     ...props,
   };
   return (
     <StyledButton
-      $size={size}
       $wide={wide}
-      $borderRadius={borderRadius}
       $themeType={themeType}
       $layer={layer}
       $textColor={textColor}
       $hoverColor={hoverColor}
       $outlined={outlined}
-      $oneToOne={oneToOne}
       $externalStyle={externalStyle}
       {...htmlProps}
     >
