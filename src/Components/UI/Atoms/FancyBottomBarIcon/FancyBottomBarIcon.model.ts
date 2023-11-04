776ada0cc9f4eeab46dea625c44e5906053c1072
MrTRyy <tobiastry@aol.de>
2023-11-04 21:28:19 +0100

    Refactor FancyBottomBarIcon component to support native button and anchor elements

--- src/Components/UI/Atoms/FancyBottomBarIcon/FancyBottomBarIcon.model.ts
+++ src/Components/UI/Atoms/FancyBottomBarIcon/FancyBottomBarIcon.model.ts
@@ -2,14 +2,17 @@ import React from "react";
 import { TUiColorsType } from "../../Design/color/designColor";
 import { TLayer } from "../../Design/color/generateColorSteps";
 
-export interface IFancyBottomBarIcon {
+type TNativeButtonProps = React.HTMLAttributes<HTMLButtonElement> & { as?: 'button' };  
+type TNativeAnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & { as: 'a' };
+
+export type IFancyBottomBarIcon = {
   id?: string;
   icon: React.ReactNode;
   label: string;
   active?: boolean;
   disabled?: boolean;
-  onClick?: () => void;
   themeType?: keyof TUiColorsType;
   layer?: TLayer;
   uniqekey?: string | number;
-}
+} & (TNativeButtonProps | TNativeAnchorProps);
+
