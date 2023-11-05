b64ca021eaa4dbf9c0494f5efb9f5533d5de6e1d
MrTRyy <tobiastry@aol.de>
2023-11-05 22:55:29 +0100

    create: component

--- src/Components/UI/Atoms/ListDivider/ListDivider.tsx
+++ src/Components/UI/Atoms/ListDivider/ListDivider.tsx
@@ -1,73 +1,33 @@
 import React from 'react';
-import { css, styled } from 'styled-components';
-import { TUiColorsType } from '../../Design/color/designColor';
-import { TLayer } from '../../Design/color/generateColorSteps';
-import { Typography } from '../Typography';
 
-const StyledI = styled.div`
-  height: 1px;
-  background-color: #656565;
-  margin: 8px 0;
-`;
-
-const StyledTextDiv = styled.div<{ $align?: IListDivider['textAlignment'] }>`
-  display: flex;
-  color: #656565;
-  justify-content: center;
-  align-items: center;
-  margin: 8px 0;
-  line-height: 0;
-
-  span {
-    padding: 0 12px;
-  }
+import { StyledI, StyledTextDiv } from './ListDivider.style';
+import FancyContent from '../../Molecules/FancyContent/FancyContent';
+import { IListDivider } from './ListDivider.model';
 
 
-  ${({ $align }) =>
-    ($align === 'right' || $align === 'center') &&
-    css`
-      &:before {
-        content: ' ';
-        margin-left: 12px;
-        width: 100%;
-        height: 1px;
-        background-color: #656565;
-      }
-    `}
-
-  ${({ $align }) =>
-    ($align === 'left' || $align === 'center') &&
-    css`
-      &:after {
-        content: ' ';
-        width: 100%;
-        margin-right: 12px;
-        height: 1px;
-        background-color: #656565;
-      }
-    `}
-`;
-
-type IListDivider = {
-  label: string;
-  themeType?: TUiColorsType;
-  layer?: TLayer;
-} & {
-  label?: string;
-  textAlignment?: 'left' | 'center' | 'right';
-};
-
+// --------------------------------------------------------------------------- //
+// ------- A Divider that displays only a line or line with informations ----- //
+// --------------------------------------------------------------------------- //
 export default function ListDivider(props: IListDivider) {
-  const { label = 'hhsd', textAlignment = 'center' } = props;
+  const { label, textAlignment = 'center', themeType, layer, icon } = props;
 
   return (
     <>
+      {/* Render a Seperator(Divider) with a optional label and icon  */}
       {label ? (
-        <StyledTextDiv role="separator" $align={textAlignment}>
-          <Typography type="inlineElement">{label || 'MOoiin'}</Typography>
+        <StyledTextDiv $textAlignment={textAlignment} tabIndex={0} $themeType={themeType} $layer={layer}>
+          <FancyContent>
+            {label && (
+              <FancyContent.Title fontVariant="inlineElement" bold={false}>
+                {label}
+              </FancyContent.Title>
+            )}
+            {icon && <FancyContent.Icon>{icon}</FancyContent.Icon>}
+          </FancyContent>
         </StyledTextDiv>
       ) : (
-        <StyledI role="separator"></StyledI>
+        // Render a Seperator(Divider) only a line
+        <StyledI role="separator" />
       )}
     </>
   );
