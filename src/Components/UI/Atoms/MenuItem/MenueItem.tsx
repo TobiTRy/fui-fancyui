ebf930b70388547190a201dae8198427e7f254c5
MrTRyy <tobiastry@aol.de>
2023-11-06 16:02:44 +0100

    splitOut: MenueItem

---
+++ src/Components/UI/Atoms/MenuItem/MenueItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MenuItem } from './MenueItem.style';
+import FancyContent from '../../Molecules/FancyContent/FancyContent';
+
+interface MenueItemProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  label?: string;
+  icon?: React.ReactNode;
+}
+
+export default function MenueItem(props: MenueItemProps) {
+  const { children, label, icon, ...HTMLProps } = props;
+  return (
+    <MenuItem {...HTMLProps}>
+      <FancyContent>
+        <FancyContent.Title bold={false}>{label}</FancyContent.Title>
+        <FancyContent.Icon>{icon}</FancyContent.Icon>
+      </FancyContent>
+      {children}
+    </MenuItem>
+  );
+}
