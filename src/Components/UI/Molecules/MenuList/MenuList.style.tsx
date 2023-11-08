e40bc852ea6ccd3abfd8c5d2604463c1a050b1f1
MrTRyy <tobiastry@aol.de>
2023-11-08 15:28:29 +0100

    update: Menulist to only container

---
+++ src/Components/UI/Molecules/MenuList/MenuList.style.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+import { TUiColorsType } from '../../Design/color/designColor';
+import { getBackgroundColor } from '../../Design/color/colorCalculatorForComponet';
+import { borderRadius } from '../../Design/design';
+import { MenuListProps } from './MenuList';
+
+import IStyledPrefixAndOmitter from '../../Interface/IStyledPrefixAndOmiter.model';
+import generateThemeForCard from '../../HelperFunctions/designFunctions/generateThemeForCard/generateThemeForCard';
+
+type StyledMenuProps = IStyledPrefixAndOmitter<MenuListProps> & { theme: TUiColorsType };
+
+export const MenuContainer = styled.div<StyledMenuProps>`
+  display: flex;
+  flex-direction: column;
+  padding: 8px 0;
+  ${({ theme, $themeType, $layer, $outlined }) =>
+    generateThemeForCard({ theme, $themeType: $themeType ?? 'primary', $layer: $layer ?? 2, $outlined })}
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: ${borderRadius.md};
+`;
