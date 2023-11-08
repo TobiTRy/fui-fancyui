fe8c52e89d889cc73ab313555070342fe0917c73
MrTRyy <tobiastry@aol.de>
2023-11-08 18:25:57 +0100

    fix: typo

--- src/Components/UI/Templates/FancyMenueComponent/FancyMenuItem/FancyMenuItem.tsx
+++ src/Components/UI/Templates/FancyMenueComponent/FancyMenuItem/FancyMenuItem.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-import MenueItem from '../../../Atoms/MenuItem/MenueItem';
+import MenuItem from '../../../Atoms/MenuItem/MenuItem';
 import FancyContent from '../../../Molecules/FancyContent/FancyContent';
-
 
 type TFancyMenueItemProps = {
   label?: string;
   icon?: React.ReactNode;
-} & React.ComponentProps<typeof MenueItem>;
+} & React.ComponentProps<typeof MenuItem>;
 // --------------------------------------------------------------------------- //
 // ---------- The FancyMenueItem is a template for a finished Item  ---------- //
 // --------------------------------------------------------------------------- //
 export default function FancyMenuItem(props: TFancyMenueItemProps) {
   const { label, icon, ...menuItemProps } = props;
   return (
-    <MenueItem {...menuItemProps}>
+    <MenuItem {...menuItemProps}>
       <FancyContent flexAlign="flex-start">
         <FancyContent.Title bold={false}>{label}</FancyContent.Title>
         <FancyContent.Icon>{icon}</FancyContent.Icon>
       </FancyContent>
-    </MenueItem>
+    </MenuItem>
   );
 }
-
