a375d52c4dfdbaccba5b2c5b3b5ac0016528bda7
MrTRyy <tobiastry@aol.de>
2023-11-08 15:29:23 +0100

    test: cases

--- src/Routes/MenuRoute/MenueRoute.tsx
+++ src/Routes/MenuRoute/MenueRoute.tsx
@@ -2,10 +2,11 @@ import React, { useRef, useState } from 'react';
 
 import { FancyButton } from '../../lib';
 
-import Menu from '../../Components/UI/Molecules/Menu/Menu';
 import { DesignArea, DesignWrapper } from '../DesignWrapper/Wrapper';
 import Popover from '../../Components/UI/HelperFunctions/FancyPopover/Popover';
 import FancyPopover from '../../Components/UI/HelperFunctions/FancyPopover/FancyPopover';
+import FancyMenueItem from '../../Components/UI/Templates/FancyMenueComponent/FancyMenuItem/FancyMenuItem';
+import FancyMenuList from '../../Components/UI/Templates/FancyMenueComponent/FancyMenuList/FancyMenuList';
 
 export default function MenueRoute() {
   const [isPopoverVisible, setPopoverVisible] = useState(false);
@@ -14,7 +15,21 @@ export default function MenueRoute() {
   return (
     <DesignWrapper>
       <DesignArea title="Teststs" style={{ justifyContent: 'flex-end' }}>
-        <FancyPopover offsetY={10} refComponent={<FancyButton label="Hi 2" />} contentComponent={<Menu />}></FancyPopover>
+        <FancyPopover
+          offsetY={10}
+          refComponent={<FancyButton label="Hi 2" />}
+          contentComponent={
+            <FancyMenuList outlined>
+              <FancyMenuList.Item label="Muuusettingshhh" icon={<>⚙️</>} />
+              <FancyMenuList.Divider />
+              <FancyMenueItem label="Logout" icon={<>🚪</>} />
+              <FancyMenueItem label="Logout" icon={<>🚪</>} />
+              <FancyMenueItem label="Logout" icon={<>🚪</>} />
+              <FancyMenueItem label="Logout" icon={<>🚪</>} />
+              <FancyMenueItem label="Logout" icon={<>🚪</>} />
+            </FancyMenuList>
+          }
+        ></FancyPopover>
       </DesignArea>
     </DesignWrapper>
   );
