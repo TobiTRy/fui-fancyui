e719342333dee0ad32f0960f9ca0ab8a952ab574
MrTRyy <tobiastry@aol.de>
2023-11-09 22:41:59 +0100

    update: sizes

--- src/Components/UI/Templates/FancyInfoCard/FancyInfoCard.tsx
+++ src/Components/UI/Templates/FancyInfoCard/FancyInfoCard.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import InfoCard from '../../Molecules/InfoCard/InfoCard';
 import FancyContent from '../../Molecules/FancyContent/FancyContent';
 import { TypographyList } from '../../Atoms/Typography/Typography';
+import { TSizes } from '../../Interface/TComponentSizes';
 
 type TSizeObj = {
-  iconSize: 'sm' | 'md' | 'lg';
+  iconSize: TSizes;
   title: keyof typeof TypographyList;
   descriptionSize: keyof typeof TypographyList;
 };
@@ -16,19 +17,19 @@ type TSizesObject = {
 
 const sizes: TSizesObject = {
   sm: {
-    iconSize: 'sm',
+    iconSize: 'md',
     title: 'h6',
     descriptionSize: 'smText',
   },
   md: {
-    iconSize: 'sm',
-    title: 'h6',
-    descriptionSize: 'smText',
+    iconSize: 'lg',
+    title: 'h5',
+    descriptionSize: 'content',
   },
   lg: {
-    iconSize: 'sm',
-    title: 'h6',
-    descriptionSize: 'smText',
+    iconSize: 'lg',
+    title: 'h4',
+    descriptionSize: 'button',
   },
 };
 
@@ -45,8 +46,8 @@ export default function FancyInfoCard(props: TFancyInfoCardProps) {
     <InfoCard {...infoCardProps} size={size}>
       <FancyContent flexAlign="flex-start" flexJustify="flex-start">
         {icon && <FancyContent.Icon size={sizes[size].iconSize}>{icon}</FancyContent.Icon>}
-        {title && <FancyContent.Title fontVariant={size[size].title}>{title}</FancyContent.Title>}
-        {description && <FancyContent.Description>{description}</FancyContent.Description>}
+        {title && <FancyContent.Title fontVariant={sizes[size].title}>{title}</FancyContent.Title>}
+        {description && <FancyContent.Description fontVariant={sizes[size].descriptionSize}>{description}</FancyContent.Description>}
       </FancyContent>
     </InfoCard>
   );
