127e79cca1cdc021fa7b732faacbad0ddbbdfb45
MrTRyy <tobiastry@aol.de>
2023-11-12 21:34:30 +0100

    buildIn: Speperator (not working)

--- src/Components/UI/Templates/FancyFlexBox/FancyFlexBox.tsx
+++ src/Components/UI/Templates/FancyFlexBox/FancyFlexBox.tsx
@@ -1,45 +1,29 @@
 import React from 'react';
-import { CSSProp, css, styled } from 'styled-components';
-import IStyledPrefixAndPicker from '../../Interface/IStyledPrefixAndPicker.model';
-import { spacingPx } from '../../Design/design';
 
-const generateFlexSytles = (props: TStyledFlexBoxProps) => {
-  const { $flexDirection, $flexJustify, $flexAlign, $gap } = props;
+import { FancyFlexBoxProps } from './FancyFlexBox.model';
+import { StyledFlexBox, StyledInlineFlexBox } from './FancyFlexBox.style';
 
-  return css`
-    display: flex;
-    flex-direction: ${$flexDirection ?? 'column'};
-    justify-content: ${ $flexJustify ?? 'center'};
-    align-items: ${$flexAlign ?? 'flex-start'};
-    gap: ${$gap?? spacingPx.xxs};
-  `;
-};
-
-type TStyledFlexBoxProps = IStyledPrefixAndPicker<TStyleProps>;
-const StyledFlexBox = styled.div<TStyledFlexBoxProps>`
-  ${(props:TStyledFlexBoxProps) => generateFlexSytles({...props}) }
-`;
-
-
-const StyledInlineFlexBox = styled.span<TStyledFlexBoxProps>`
-  ${(props:TStyledFlexBoxProps) => generateFlexSytles({...props}) }
-`;
-
-type TStyleProps = {
-  flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
-  flexJustify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
-  flexAlign?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
-  gap?: string;
-}
-
-interface FancyFlexBoxProps {
-  children?: React.ReactNode;
-  inline?: boolean;
-}
-
+// --------------------------------------------------------------------------- //
+// ----------  ---------- //
+// --------------------------------------------------------------------------- //
 export default function FancyFlexBox(props: FancyFlexBoxProps) {
-  const { children, inline } = props;
-  const Container = inline ? StyledInlineFlexBox : StyledFlexBox;
+  const { children, inline, seperator } = props;
 
-  return <Container>{children}</Container>;
+  const Container = inline ? StyledInlineFlexBox : StyledFlexBox; 
+
+  const modifiedChilds = React.Children.map(children, (child, index) => {
+    if (index === 0) {
+      return child;
+    }
+
+    return (
+      <>
+        {seperator && seperator}
+        {child}
+      </>
+    );
+  });
+
+
+  return <Container>{modifiedChilds}</Container>;
 }
