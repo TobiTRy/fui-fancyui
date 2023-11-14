9e51dc3ef3d30c6b40c03f2c47df6f68566e6081
MrTRyy <tobiastry@aol.de>
2023-11-14 16:07:26 +0100

    build: new fancyBottom bar template

--- src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
+++ src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import SwitchList from '../../Molecules/SwitchList/SwitchList';
+import useFancyHandyNavStore from './FancyHandyNav.store';
+import BottomBar from '../../Molecules/BottomBar/BottomBar';
+
+
+
 
 export default function FancyHandyNav() {
+  const isVisibleState = useFancyHandyNavStore((state) => state.isVisible);
+  const setIsVisible = useFancyHandyNavStore((state) => state.setIsVisible);
+  const whichIsActive = useFancyHandyNavStore((state) => state.whichIsActive);
+  const setWhichIsActive = useFancyHandyNavStore((state) => state.setWhichIsActive);
+
+
+
   return (
-    <div>FancyHandyNav</div>
-  )
+    <BottomBar>
+      <SwitchList whichIndexIsSelected={Number(whichIsActive)} $indicatorWidth={'70%'}>
+        {ItterateButtons()}
+      </SwitchList>
+    </BottomBar>
+  );
 }
