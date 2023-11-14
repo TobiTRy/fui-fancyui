64400c82e28b3771e56fe6757021c48b388ae199
MrTRyy <tobiastry@aol.de>
2023-11-14 22:40:07 +0100

    BuildIn: FancyAction wrapper

---
+++ src/Components/UI/Atoms/FancyActionWrapper/FancyActionWrapper.style.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const StyledButton = styled.button`
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+  width: 100%;
+`
+
+export const StyledAnchor = styled.a`
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
+  width: 100%;
+`
