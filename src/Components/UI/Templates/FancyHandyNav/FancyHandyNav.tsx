100daf53067db9e1b1cb7d8f0a75a577876ae9f2
MrTRyy <tobiastry@aol.de>
2023-11-15 22:36:47 +0100

    buildIn: FanyBottom bar icon

--- src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
+++ src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
+
 import SwitchList from '../../Molecules/SwitchList/SwitchList';
 import useFancyHandyNavStore from './FancyHandyNav.store';
 import BottomBar from '../../Molecules/BottomBar/BottomBar';
+import FancyBottomBarIcon, { IFancyBottomBarIcon } from '../FancyBottomBarIcon/FancyBottomBarIcon';
 
 
+interface IFancyHandyNav {
+  items?: IFancyBottomBarIcon[];
 
+}
+export default function FancyHandyNav(props: IFancyHandyNav) {
+  const { items } = props;
 
-export default function FancyHandyNav() {
   const isVisibleState = useFancyHandyNavStore((state) => state.isVisible);
   const setIsVisible = useFancyHandyNavStore((state) => state.setIsVisible);
   const whichIsActive = useFancyHandyNavStore((state) => state.whichIsActive);
   const setWhichIsActive = useFancyHandyNavStore((state) => state.setWhichIsActive);
 
-
-
   return (
     <BottomBar>
       <SwitchList whichIndexIsSelected={Number(whichIsActive)} $indicatorWidth={'70%'}>
-        {ItterateButtons()}
+        {items?.map((item, index) => (
+          <FancyBottomBarIcon
+            key={index}
+            isActive={whichIsActive === index.toString()}
+            {...item}
+            onClick={() => {
+              item.onClick && item.onClick();
+              setWhichIsActive(index.toString());
+              setIsVisible(false);
+            }}
+          />
+        ))}
       </SwitchList>
     </BottomBar>
   );
