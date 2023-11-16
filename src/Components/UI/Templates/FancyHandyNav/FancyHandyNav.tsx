5070037ac3a98682f25c4558f90f884efab927fb
MrTRyy <tobiastry@aol.de>
2023-11-16 15:02:49 +0100

    finish: Handynav component

--- src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
+++ src/Components/UI/Templates/FancyHandyNav/FancyHandyNav.tsx
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import SwitchList from '../../Molecules/SwitchList/SwitchList';
 import useFancyHandyNavStore from './FancyHandyNav.store';
 import BottomBar from '../../Molecules/BottomBar/BottomBar';
 import FancyBottomBarIcon, { IFancyBottomBarIcon } from '../FancyBottomBarIcon/FancyBottomBarIcon';
-
+import RawNav from '../../Atoms/RawNav/RawNav';
+import { TLayer } from '../../Design/color/generateColorSteps';
+import { TUiColorsType } from '../../Design/color/designColor';
 
 interface IFancyHandyNav {
   items?: IFancyBottomBarIcon[];
-
+  isVisible?: boolean;
+  wichIndexIsActive?: string;
+  themeType?: keyof TUiColorsType;
+  themeTypeIcons?: keyof TUiColorsType;
+  themeTypeSwitchList?: keyof TUiColorsType;
+  layer?: TLayer;
 }
+// --------------------------------------------------------------------------- //
+// ---------- A handyNavBar that can dynamicly generated via objects---------- //
+// --------------------------------------------------------------------------- //
 export default function FancyHandyNav(props: IFancyHandyNav) {
-  const { items } = props;
+  const { items, isVisible, wichIndexIsActive, themeType, themeTypeIcons, themeTypeSwitchList, layer } = props;
 
+  // setup a global zustand store for the visibility and the active index
   const isVisibleState = useFancyHandyNavStore((state) => state.isVisible);
   const setIsVisible = useFancyHandyNavStore((state) => state.setIsVisible);
-  const whichIsActive = useFancyHandyNavStore((state) => state.whichIsActive);
-  const setWhichIsActive = useFancyHandyNavStore((state) => state.setWhichIsActive);
+  const stateWhichIsActive = useFancyHandyNavStore((state) => state.whichIsActive);
+  const setWhichIsActiveState = useFancyHandyNavStore((state) => state.setWhichIsActive);
+
+  // Set the default values / initial values
+  useEffect(() => {
+    setWhichIsActiveState(wichIndexIsActive ?? '0');
+  }, []);
+
+  // handle the visibility of the nav
+  useEffect(() => {
+    setIsVisible(isVisible ?? true);
+  }, [isVisible]);
+
 
   return (
-    <BottomBar>
-      <SwitchList whichIndexIsSelected={Number(whichIsActive)} $indicatorWidth={'70%'}>
-        {items?.map((item, index) => (
-          <FancyBottomBarIcon
-            key={index}
-            isActive={whichIsActive === index.toString()}
-            {...item}
-            onClick={() => {
-              item.onClick && item.onClick();
-              setWhichIsActive(index.toString());
-              setIsVisible(false);
-            }}
-          />
-        ))}
-      </SwitchList>
-    </BottomBar>
+    <>
+      {isVisibleState && (
+        // The Navbar container
+        <RawNav>
+          <BottomBar themeType={themeType} layer={layer} externalStyle={{borderBottom: 'none'}}>
+            {/* The List with the items  */}
+            <SwitchList whichIndexIsSelected={Number(stateWhichIsActive)} $themeType={themeTypeSwitchList} $indicatorWidth={'70%'}>
+              {items?.map((item, index) => (
+                <FancyBottomBarIcon
+                  key={index}
+                  themeType={themeTypeIcons}
+                  layer={layer}
+                  isActive={Number(stateWhichIsActive) === index}
+                  onClick={() => setWhichIsActiveState(`${index}`)}
+                  {...item}
+                />
+              ))}
+            </SwitchList>
+          </BottomBar>
+        </RawNav>
+      )}
+    </>
   );
 }
