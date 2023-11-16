57d445589f543677193192b95cb49008c321b79f
MrTRyy <tobiastry@aol.de>
2023-11-16 15:01:11 +0100

    buildIn: Docu

--- src/Components/UI/Atoms/ComponentAsWrapper/ComponentAsWrapper.tsx
+++ src/Components/UI/Atoms/ComponentAsWrapper/ComponentAsWrapper.tsx
@@ -4,7 +4,9 @@ interface Props {
   wrapper: React.ReactElement;
   children: React.ReactNode;
 }
-
+// --------------------------------------------------------------------------- //
+// --------- This component is used when you have a dynamic wrapper ---------- //
+// --------------------------------------------------------------------------- //
 export default function ComponentAsWrapper({ wrapper, children }: Props) {
   // Clone the element to inject children
   return React.cloneElement(wrapper, {}, children);
