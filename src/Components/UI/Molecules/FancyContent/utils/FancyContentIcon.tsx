ebaf00937c1e7d876b8eeeddc0009233f27b756a
MrTRyy <tobiastry@aol.de>
2023-11-16 14:37:47 +0100

    update: make fancy content Icon accessible

--- src/Components/UI/Molecules/FancyContent/utils/FancyContentIcon.tsx
+++ src/Components/UI/Molecules/FancyContent/utils/FancyContentIcon.tsx
@@ -3,11 +3,12 @@ import { FancySVGAtom } from '../../../Atoms/FancySVGAtom';
 interface IFancyContentIconProps {
   children?: React.ReactNode;
   size?: React.ComponentProps<typeof FancySVGAtom>['size'];
+  'aria-label'?: string;
 }
 export function FancyContentIcon(props: IFancyContentIconProps) {
-  const { children, size = 'md' } = props;
+  const { children, size = 'md',  } = props;
   return (
-    <FancySVGAtom size={size} externalStyle={{ flexShrink: '0' }} isPassive>
+    <FancySVGAtom aria-label={props['aria-label']} size={size} externalStyle={{ flexShrink: '0' }} isPassive>
       {children}
     </FancySVGAtom>
   );
