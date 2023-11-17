5a1788862f6ef4368956e0d52cfeae9b41e1f6ef
MrTRyy <tobiastry@aol.de>
2023-11-17 21:25:44 +0100

    buildIn: absolut imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'node:path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
+      },
     plugins: [
         react(),
         dts({
