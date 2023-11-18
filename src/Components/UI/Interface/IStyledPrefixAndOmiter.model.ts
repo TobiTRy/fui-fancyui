d859ad082a37bf50c358c003a5751f68f8acc653
MrTRyy <tobiastry@aol.de>
2023-11-18 20:24:31 +0100

    remove: comments

--- src/Components/UI/Interface/IStyledPrefixAndOmiter.model.ts
+++ src/Components/UI/Interface/IStyledPrefixAndOmiter.model.ts
@@ -1,20 +1,3 @@
-// This sets a Prefix for each key in an object PrefixKeys<ISVGAtom, '$'>
-// type IStyledSVGAtom = {
-//     $children: React.ReactNode;
-//     $size: ISizes;
-//     $isActive?: boolean;
-//     $passivElement?: boolean;
-//     $errorMessage?: string;
-//     $externalStyle?: CSSProp;
-//   };
-
-
-
-// type IStyledPrefixAndOmiter<T extends Record<string, any>, U extends keyof any = never> = {
-//     [P in Exclude<keyof T, U> & string as `$${P}`]: T[P];
-//   };
-
-
 type StringKeys<T> = Extract<keyof T, string>;
 
 type RequiredKeys<T> = {
@@ -38,5 +21,3 @@ type IStyledPrefixAndOmitter<T extends Record<string, any>, U extends StringKeys
 
 
 export default IStyledPrefixAndOmitter;
-
-//type IStyledSVGAtom = PrefixKeys<ISVGAtom, '$children'>;
--- src/Components/UI/Interface/IStyledPrefixAndPicker.model.ts
+++ src/Components/UI/Interface/IStyledPrefixAndPicker.model.ts
@@ -1,30 +1,3 @@
-// This sets a Prefix for each key in an object PrefixKeys<ISVGAtom, '$'>
-// type IStyledSVGAtom = {
-//     $children: React.ReactNode;
-//     $size: ISizes;
-//     $isActive?: boolean;
-//     $passivElement?: boolean;
-//     $errorMessage?: string;
-//     $externalStyle?: CSSProp;
-//   };
-
-// type IStyledPrefixAndPicker<T extends Record<string, any>, U extends keyof T = never> = {
-//   [P in Extract<U, string> as `$${P}`]: T[P];
-// };
-
-// type IStyledPrefixAndPicker<T extends Record<string, any>, U extends keyof T = keyof T> = {
-//   [P in Extract<U, string> as `$${P}`]?: T[P];
-// };
-
-
-// type IStyledPrefixAndPicker<T extends Record<string, any>, U extends keyof T = keyof T> = {
-//   [P in Extract<U, string> as `$${P}`]: T[P];
-// };
-
-// type IStyledPrefixAndPicker<T extends Record<string, any>, U extends keyof T = keyof T> = {
-//   [P in Extract<U, string> as `$${P}`]?: T[P];
-// };
-
 type StringKeys<T> = Extract<keyof T, string>;
 
 type RequiredKeysOfSubset<T, U extends StringKeys<T>> = {
@@ -42,4 +15,3 @@ type IStyledPrefixAndPicker<T extends Record<string, any>, U extends StringKeys<
 };
 
 export default IStyledPrefixAndPicker;
-//type IStyledSVGAtom = PrefixKeys<ISVGAtom, '$children'>;
