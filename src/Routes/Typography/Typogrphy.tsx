5dd0140d01d6bf6af4607eaed0d22d17a923c9b2
MrTRyy <tobiastry@aol.de>
2023-11-19 19:02:12 +0100

    cleanup: typorgraphy

--- src/Routes/Typography/Typogrphy.tsx
+++ src/Routes/Typography/Typogrphy.tsx
@@ -5,12 +5,13 @@ import { DesignWrapper, DesignArea } from '../DesignWrapper/Wrapper';
 import FancyCard from '../../Components/UI/Atoms/FancyCard/FancyCard';
 
 export default function Typogrphy() {
+
   return (
     <FancyCard>
       <DesignWrapper>
         <DesignArea title="Typography">
           <div style={{ flexDirection: 'column' }}>
-            <Typography type="h1">h1. Heading Lorem ipsum dolor sit amet, </Typography>
+            <Typography type="h1" variant='inlineElement' weight={'bold'}>h1. Heading Lorem ipsum dolor sit amet, </Typography>
             <Typography type="h1">h1. Heading Heading Lorem ipsum dolor sit amet,</Typography>
             <Typography type="h2">h2. Headin Heading Lorem ipsum dolor sit amet, </Typography>
             <Typography type="h3">h3. Heading Heading Lorem ipsum dolor sit amet, </Typography>
@@ -24,6 +25,10 @@ export default function Typogrphy() {
             <Typography type="smText" weight="bold">
               smText. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
             </Typography>
+          </div>
+        </DesignArea>
+        <DesignArea title="Typography">
+          <div style={{ flexDirection: 'column' }}>
           </div>
         </DesignArea>
       </DesignWrapper>
