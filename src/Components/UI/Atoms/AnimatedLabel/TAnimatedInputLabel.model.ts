32c1e70a49757ba538586a94f7a6766ab12af21a
MrTRyy <tobiastry@aol.de>
2023-11-21 08:04:25 +0100

    update: pipe

--- src/Components/UI/Atoms/AnimatedLabel/TAnimatedInputLabel.model.ts
+++ src/Components/UI/Atoms/AnimatedLabel/TAnimatedInputLabel.model.ts
@@ -1,6 +1,6 @@
-import { TLayer } from "@/Components/UI/Interface/TLayer";
-import { TTheme } from "@/Components/UI/Interface/TTheme";
-import { TUiColors } from "@/Components/UI/Interface/TUiColors";
+import { TLayer } from '@/Components/UI/Interface/TLayer';
+import { TTheme } from '@/Components/UI/Interface/TTheme';
+import { TUiColors } from '@/Components/UI/Interface/TUiColors';
 
 export interface IAnimatedInputLabel {
   $align?: 'center' | 'left';
