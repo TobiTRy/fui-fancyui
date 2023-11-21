a3d77f39355f114e391e0b6a239293f8fbcb2edc
MrTRyy <tobiastry@aol.de>
2023-11-21 21:30:24 +0100

    fix: backgroundColor

--- src/Components/UI/Molecules/MenuList/MenuList.style.tsx
+++ src/Components/UI/Molecules/MenuList/MenuList.style.tsx
@@ -12,7 +12,7 @@ export const MenuContainer = styled.div<StyledMenuProps>`
   flex-direction: column;
   padding: 8px 0;
   ${({ theme, $themeType, $layer, $outlined }) =>
-    generateThemeForCard({ theme, $themeType: $themeType ?? 'primary', $layer: $layer ?? 2, $outlined })}
+    generateThemeForCard({ theme, $themeType: $themeType ?? 'primary', $layer: $layer ?? 2, $outlined, $outlinedBackgroundStrength: 1 })}
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
   border-radius: ${({ theme }) => theme.borderRadius.md};
 `;
