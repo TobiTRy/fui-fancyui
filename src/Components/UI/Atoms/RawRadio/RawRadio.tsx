cf3f3eeb7e8f457cb011262235e834ea76c82318
MrTRyy <tobiastry@aol.de>
2023-11-22 22:42:52 +0100

    update: with native props

--- src/Components/UI/Atoms/RawRadio/RawRadio.tsx
+++ src/Components/UI/Atoms/RawRadio/RawRadio.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import { HiddenRadio, RadioWrapper, StyledRadio } from './RawRadio.style';
 
-interface RawRadioProps {
+type TRawRadioProp = {
   id: string;
   name: string;
   value: string;
   checked: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-export default function RawRadio(props: RawRadioProps) {
-  const { id, name, value, checked, onChange } = props;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+export default function RawRadio(props: TRawRadioProp) {
+  const { id, name, value, checked, onChange, ...inputProps } = props;
 
   return (
     <RadioWrapper>
       <StyledRadio checked={checked} />
-      <HiddenRadio id={id} name={name} value={value} checked={checked} onChange={onChange} />
+      <HiddenRadio id={id} name={name} value={value} checked={checked} onChange={onChange} tabIndex={0} {...inputProps} />
     </RadioWrapper>
   );
 }
