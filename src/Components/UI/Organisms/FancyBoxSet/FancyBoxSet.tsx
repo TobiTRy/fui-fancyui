1a7a606e0c3c39e6aed78a431b059b04c59cbbe9
MrTRyy <tobiastry@aol.de>
2023-11-24 14:29:30 +0100

    buildIn: FancyBoxSet

---
+++ src/Components/UI/Organisms/FancyBoxSet/FancyBoxSet.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Fieldset } from '@/Components/UI/Molecules/Fieldset';
+import { FancyListBox } from '@/Components/UI/Organisms/FancyListBox';
+import { FancyLine } from '@/Components/UI/Atoms/FancyLine';
+
+type TFieldSetProps = React.ComponentProps<typeof Fieldset>;
+type TFancyListProps = React.ComponentProps<typeof FancyListBox>;
+
+type TFancyBoxSet = TFieldSetProps & TFancyListProps;
+export default function FancyBoxSet(props: TFancyBoxSet) {
+  const { label, alignLabel, fontVariantLegend, disabled, children, ...HTMLProps } = props;
+
+  const childArray = React.Children.toArray(children);
+
+  return (
+    <Fieldset label={label} alignLabel={alignLabel} fontVariantLegend={fontVariantLegend} disabled={disabled}>
+      <FancyListBox {...HTMLProps}>
+        {childArray.map((child, i) => (
+          <>
+            <FancyListBox.Item>{child}</FancyListBox.Item>
+            {childArray.length - 1 !== i && <FancyLine themeType="primary" layer={3} />}
+          </>
+        ))}
+      </FancyListBox>
+    </Fieldset>
+  );
+}
