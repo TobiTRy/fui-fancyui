4fa2b1ded8806e1124f9970874d39aa3960c86ae
MrTRyy <tobiastry@aol.de>
2023-11-25 11:44:34 +0100

    buildIn: ThemeType und layer FancyBoxSet

--- src/Components/UI/Organisms/FancyBoxSet/FancyBoxSet.tsx
+++ src/Components/UI/Organisms/FancyBoxSet/FancyBoxSet.tsx
@@ -3,26 +3,37 @@ import React from 'react';
 import { Fieldset } from '@/Components/UI/Molecules/Fieldset';
 import { FancyListBox } from '@/Components/UI/Molecules/FancyListBox';
 import { FancyLine } from '@/Components/UI/Atoms/FancyLine';
+import { TLayer } from '../../Interface/TLayer';
 
 type TFieldSetProps = React.ComponentProps<typeof Fieldset>;
 type TFancyListProps = React.ComponentProps<typeof FancyListBox>;
 
-type TFancyBoxSet = TFieldSetProps & TFancyListProps;
+type TFancyBoxSet = TFieldSetProps & TFancyListProps & { displayLine?: boolean };
+// --------------------------------------------------------------------------- //
+// ----- The FancyBoxSet to render the ListBox with a Fieldset and a Line ---- //
+// --------------------------------------------------------------------------- //
 export default function FancyBoxSet(props: TFancyBoxSet) {
-  const { label, alignLabel, fontVariantLegend, disabled, children, ...HTMLProps } = props;
+  const { label, alignLabel, fontVariantLegend, disabled, children, themeType, displayLine, layer, ...HTMLProps } = props;
 
+  // is needed to get the length of the children for displaying the line
   const childArray = React.Children.toArray(children);
 
   return (
     <Fieldset label={label} alignLabel={alignLabel} fontVariantLegend={fontVariantLegend} disabled={disabled}>
-      <FancyListBox {...HTMLProps}>
+      <FancyListBox themeType={themeType} layer={layer} {...HTMLProps}>
         {childArray.map((child, i) => (
           <>
             <FancyListBox.Item>{child}</FancyListBox.Item>
-            {childArray.length - 1 !== i && <FancyLine themeType="primary" layer={3} />}
+            {!displayLine && childArray.length - 1 !== i && (
+              <FancyLine themeType={themeType ?? 'primary'} layer={Math.min(layer ? layer + 2 : 2, 10) as TLayer} />
+            )}
           </>
         ))}
       </FancyListBox>
     </Fieldset>
   );
 }
+
+FancyBoxSet.defaultProps = {
+  layer: 1,
+};
