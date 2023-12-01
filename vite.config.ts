63d017408139e7e2f3b9229bc503954cf8eb6ed1
MrTRyy <tobiastry@aol.de>
2023-12-01 22:45:40 +0100

    mini updates:

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'node:path';
 import dts from 'vite-plugin-dts';
 
@@ -15,8 +15,11 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    visualizer(),
   ],
   build: {
+    copyPublicDir: false,
+    minify: true,
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
       name: 'FUI',
