7281ebe12d35932fcdb97318f72cdbe28a437159
MrTRyy <tobiastry@aol.de>
2023-12-06 08:49:42 +0100

    refactor: isColorValid

--- src/utils/validations/isColorValid/isColorValid.ts
+++ src/utils/validations/isColorValid/isColorValid.ts
@@ -1,11 +1,25 @@
 import Color from 'color';
 
-// this function checks if a color is valid
+/**
+ * Validates if a given string is a valid color representation.
+ * Logs a warning to the console if the color is invalid or input is not a string.
+ */
 export default function isColorValid(color: string) {
+  if (typeof color !== 'string') {
+    console.warn('Input is not a string.');
+    return false;
+  }
+
+  if (!color.trim()) {
+    console.warn('Input is an empty string.');
+    return false;
+  }
+
   try {
     Color(color);
     return true;
-  } catch (error) {
+  } catch {
+    console.warn('Invalid color format.');
     return false;
   }
 }
