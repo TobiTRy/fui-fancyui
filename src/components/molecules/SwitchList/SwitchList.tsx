a66171ca9b7af2030383d925a97031dc660364d5
MrTRyy <tobiastry@aol.de>
2023-12-06 15:43:35 +0100

    refactor: Switchlist

--- src/components/molecules/SwitchList/SwitchList.tsx
+++ src/components/molecules/SwitchList/SwitchList.tsx
@@ -3,14 +3,15 @@ import { CSSProp } from 'styled-components';
 
 import SwitchActiveIndicator, { IActiveSwitchIndicator } from '@/components/atoms/SwitchActiveIndicator/SwitchActiveIndicator';
 import { ItemWrapper, StyledList } from './SwitchList.style';
+import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
 
-type TSwitchActiveIndicator = Omit<IActiveSwitchIndicator, '$itemNumber'>;
+type TSwitchActiveIndicator = Omit<IActiveSwitchIndicator, 'itemNumber'>;
 
 interface IBottomBarListProps {
   children?: React.ReactNode;
   whichIndexIsSelected?: number;
   activeItemHandler?: (activeKey: number) => void;
-  indicatorType?: React.ComponentProps<typeof SwitchActiveIndicator>['$type'];
+  indicatorType?: React.ComponentProps<typeof SwitchActiveIndicator>['type'];
   externalStyle?: CSSProp;
 }
 
@@ -21,7 +22,7 @@ interface IKey {
 // -------------- The Switch List Indicates wich item is active -------------- //
 // --------------------------------------------------------------------------- //
 export default function SwitchList(props: IBottomBarListProps & TSwitchActiveIndicator) {
-  const { children, whichIndexIsSelected, activeItemHandler, indicatorType, externalStyle, $direction, ...indicatorProps } = props;
+  const { children, whichIndexIsSelected, activeItemHandler, indicatorType, externalStyle, direction, ...indicatorProps } = props;
   const [currentActive, setCurrentActive] = useState('');
 
   const activeHandler = (uniqueKey: string) => {
@@ -34,31 +35,33 @@ export default function SwitchList(props: IBottomBarListProps & TSwitchActiveInd
   }, [whichIndexIsSelected]);
 
   return (
-    <StyledList $externalStyle={externalStyle} $direction={$direction}>
-      {React.Children.map(children, (child, index) => {
-        if (React.isValidElement(child)) {
-          // Generate a unique key (or use any other unique identifier logic)
-          const uniqueKey = `${index + 1}`;
+    <FancyFlexBox externalStyle={externalStyle} flexDirection={direction === 'vertical' ? 'column' : 'row'}>
+      <StyledList $externalStyle={externalStyle} $direction={direction}>
+        {React.Children.map(children, (child, index) => {
+          if (React.isValidElement(child)) {
+            // Generate a unique key (or use any other unique identifier logic)
+            const uniqueKey = `${index + 1}`;
 
-          // Clone the child and add the uniqueKey prop
-          const clonedChild = React.cloneElement(child as React.ReactElement<IKey>, { $uniquekey: uniqueKey });
+            // Clone the child and add the uniqueKey prop
+            const clonedChild = React.cloneElement(child as React.ReactElement<IKey>);
 
-          return (
-            <ItemWrapper key={uniqueKey} onClick={() => activeHandler(uniqueKey)}>
-              {clonedChild}
-              {index === 0 && (
-                <SwitchActiveIndicator
-                  $direction={$direction}
-                  $itemNumber={Number(currentActive)}
-                  $type={indicatorType ?? 'underline'}
-                  {...indicatorProps}
-                />
-              )}
-            </ItemWrapper>
-          );
-        }
-        return child;
-      })}
-    </StyledList>
+            return (
+              <ItemWrapper key={uniqueKey} onClick={() => activeHandler(uniqueKey)}>
+                {clonedChild}
+                {index === 0 && (
+                  <SwitchActiveIndicator
+                    direction={direction}
+                    type={indicatorType ?? 'underline'}
+                    {...indicatorProps}
+                    itemNumber={Number(currentActive)}
+                  />
+                )}
+              </ItemWrapper>
+            );
+          }
+          return child;
+        })}
+      </StyledList>
+    </FancyFlexBox>
   );
 }
