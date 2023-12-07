5482328d0d144c67b278abb7d0cf9d6b42d97052
MrTRyy <tobiastry@aol.de>
2023-12-07 15:38:08 +0100

    refactor: FancyHeader Nav make it more extenxible

--- src/components/molecules/FancyNavBar/FancyNavBar.tsx
+++ src/components/molecules/FancyNavBar/FancyNavBar.tsx
@@ -1,32 +1,18 @@
 import React from 'react';
 
 import { RawNav } from '@/components/atoms/RawNav';
-import { RawLI } from '@/components/atoms/RawLI';
-import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
-import { SwitchList } from '@/components/molecules/SwitchList';
+import { FlexBoxTemplateNav } from '@/components/molecules/FancyNavBar/utils/FlexBoxTemplateNav';
+import { SwitchIndicatorList } from './utils/SwitchIndicatorList';
 
 type TFancyNavBar = {
   children?: React.ReactNode;
-  switchIndicator?: boolean;
-} & React.ComponentProps<typeof FancyFlexBox>;
+};
 
 export default function FancyNavBar(props: TFancyNavBar) {
-  const { children, switchIndicator, ...flexProps } = props;
+  const { children } = props;
 
-  return (
-    <RawNav>
-      {switchIndicator ? (
-        <SwitchList>{children}</SwitchList>
-      ) : (
-        <FancyFlexBox as="ul" flexDirection={flexProps.flexDirection ?? 'row'} flexAlign={flexProps.flexAlign ?? 'center'}>
-          {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              return <RawLI>{child}</RawLI>;
-            }
-            return child;
-          })}
-        </FancyFlexBox>
-      )}
-    </RawNav>
-  );
+  return <RawNav $externalStyle={{ height: 'auto' }}>{children}</RawNav>;
 }
+
+FancyNavBar.SwitchIndicatorList = SwitchIndicatorList;
+FancyNavBar.Ul = FlexBoxTemplateNav;
