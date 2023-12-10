e7e04280a3bba192aa9242b1552782c58a8d3332
MrTRyy <Tobiastry@aol.de>
2023-12-10 22:12:21 +0100

    renameProp: linkTo --> href

--- src/components/molecules/Header/Header.stories.tsx
+++ src/components/molecules/Header/Header.stories.tsx
@@ -47,7 +47,7 @@ export const Primary: Story = {
         <FancyMiniProfile
           alignText="left"
           headingText="@TobiTRy"
-          imageURL="https://www.az-online.de/bilder/2019/08/23/12938342/2113799823-tobias-rester-2tyMMSkM2R73.jpg"
+          src="https://www.az-online.de/bilder/2019/08/23/12938342/2113799823-tobias-rester-2tyMMSkM2R73.jpg"
         />
       </div>
     </Header>
