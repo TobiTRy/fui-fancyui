42f4f25b0fe95dc4c1dabd03eec70ae20aaa0417
MrTRyy <Tobiastry@aol.de>
2023-12-10 21:56:41 +0100

    buildIn: Native props

--- src/components/atoms/FancyLI/FancyLI.tsx
+++ src/components/atoms/FancyLI/FancyLI.tsx
@@ -5,33 +5,33 @@ import IStyledPrefixAndOmiter from '@/interface/IStyledPrefixAndOmiter.model';
 import { TSpacings } from '@/interface/TSpacings';
 import { TTheme } from '@/interface/TTheme';
 
-interface FancyLIProps {
+type TFancyLIProps = {
   aligned?: 'left' | 'center' | 'right';
   children?: React.ReactNode;
   size?: TSpacings;
-}
+} & React.HTMLAttributes<HTMLLIElement>;
 // --------------------------------------------------------------------------- //
 // ------------- The fancy LI Item that can dynamicly adjusted --------------- //
 // --------------------------------------------------------------------------- //
-export default function FancyLI(props: FancyLIProps) {
-  const { children, size, aligned } = { ...defaultProps, ...props };
+export default function FancyLI(props: TFancyLIProps) {
+  const { children, size, aligned, ...htmlProps } = { ...defaultProps, ...props };
 
   return (
-    <FancyLiItem $size={size} $aligned={aligned}>
+    <FancyLiItem $size={size} $aligned={aligned} {...htmlProps}>
       {children}
     </FancyLiItem>
   );
 }
 
-const defaultProps: FancyLIProps = {
-  size: 'md',
-  aligned: 'left',
+const defaultProps = {
+  size: 'md' as const,
+  aligned: 'left' as const,
 };
 
 // ------------------------------------------- //
 // ------- The style for the component ------- //
 // ------------------------------------------- //
-type StyledFancyLi = IStyledPrefixAndOmiter<FancyLIProps, 'children'> & { theme: TTheme };
+type StyledFancyLi = IStyledPrefixAndOmiter<TFancyLIProps, 'children'> & { theme: TTheme };
 const FancyLiItem = styled.li<StyledFancyLi>`
   list-style: none;
   display: flex;
