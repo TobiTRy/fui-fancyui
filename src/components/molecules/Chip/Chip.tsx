3b0382dfb6671c45c507cd87caf5f40776da2336
MrTRyy <Tobiastry@aol.de>
2023-12-11 21:57:48 +0100

    refactor: chip with pill

--- src/components/molecules/Chip/Chip.tsx
+++ src/components/molecules/Chip/Chip.tsx
@@ -1,36 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 
-import { StyledChip } from './Chip.style';
 import { IChipProps } from './IChip.model';
+import Pill from '@/components/atoms/Pill/Pill';
 // --------------------------------------------------------------------------- //
 // --------------- A Simple chip you can put everthing in it  ---------------- //
 // --------------------------------------------------------------------------- //
-type TChipProps = IChipProps & React.HTMLAttributes<HTMLDivElement>;
+type TChipProps = IChipProps & React.ComponentProps<typeof Pill>;
 export default function Chip(props: TChipProps) {
-  const { size, outlined, themeType, textColor, layer, textLayer, isActive, externalStyle, ...htmlProps } = props;
+  const { size, outlined, themeType, layer,  isActive, externalStyle, ...htmlProps } = props;
 
   return (
-    <StyledChip
-      $isActive={isActive}
-      $size={size}
-      $outlined={outlined}
-      $themeType={themeType}
-      $textColor={textColor}
-      $layer={layer}
-      $textLayer={textLayer}
-      $externalStyle={externalStyle}
-      role={props.onClick ? 'button' : undefined}
-      tabIndex={0}
-      onClick={props.onClick}
-      onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-        if (props.onClick && (e.key === 'Enter' || e.key === 'Space')) {
-          e.preventDefault();
-          props.onClick();
-        }
-      }}
-      {...htmlProps}
-    >
-      {props.children}
-    </StyledChip>
+    <>
+
+      {/* <StyledChip
+        $isActive={isActive}
+        $size={size}
+        $outlined={outlined}
+        $themeType={themeType}
+        $layer={layer}
+        $externalStyle={externalStyle}
+        $textColor={textColor}
+        $textLayer={textLayer}
+        role={props.onClick ? 'button' : undefined}
+        tabIndex={0}
+        onClick={props.onClick}
+        onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+          if (props.onClick && (e.key === 'Enter' || e.key === 'Space')) {
+            e.preventDefault();
+            props.onClick();
+          }
+        }}
+        {...htmlProps}
+      >
+        {props.children}
+      </StyledChip> */}
+      <Pill
+        outlined={outlined}
+        role={props.onClick ? 'button' : undefined}
+        tabIndex={0}
+        themeType={themeType}
+        layer={layer}
+        externalStyle={externalStyle}
+        {...htmlProps}
+      >
+        {props.children}
+      </Pill>
+    </>
   );
 }
