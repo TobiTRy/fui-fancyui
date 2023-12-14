411cf00697d22bf35ca24cbecbc8a602635182cd
MrTRyy <tobiastry@aol.de>
2023-12-14 16:58:46 +0100

    buildIn: rawButton

---
+++ src/components/atoms/RawButton/RawButton.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const RawButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  background-color: transparent;
+  padding: 0;
+  display: flex;
+  align-items: center;
+`;
+
+export default RawButton;
