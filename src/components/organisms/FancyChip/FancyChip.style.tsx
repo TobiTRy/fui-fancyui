c09f9c9bc28e92c95aa9e64b4ec209d489f94494
MrTRyy <tobiastry@aol.de>
2023-12-14 16:17:10 +0100

    remove: fancyChip

--- src/components/organisms/FancyChip/FancyChip.style.tsx
+++
@@ -1,6 +0,0 @@
-
-
-// Define the type for the spacing position
-export type TSpacingPosition = 'left' | 'right' | 'booth';
-
-// Define the styled component for the X button
--- src/components/organisms/FancyChip/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as FancyChip } from './FancyChip';
