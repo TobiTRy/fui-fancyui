7f26763812e087586c8ba2e605d63160f1730889
MrTRyy <Tobiastry@aol.de>
2023-12-16 18:49:01 +0100

    Refactor generateSpacing function to use themeStore for spacing settings

--- src/components/templates/FancyChip/utils/generateSpacings.ts
+++ src/components/templates/FancyChip/utils/generateSpacings.ts
@@ -1,13 +1,33 @@
 import { css } from 'styled-components';
-
-import { sizesSettings } from '@/components/molecules/Chip/sizeSettings';
+import { themeStore } from "@/design/theme/themeStore";
+import { TComponentSizes } from "@/interface/TComponentSizes";
 
 export type TSpacingPosition = 'left' | 'right' | 'booth';
+
+
+const getSpacingFromTheme = themeStore.getState().theme.spacing;
+const sizesSettings = {
+  sm: {
+    padding: getSpacingFromTheme.xs,
+    paddingRight: getSpacingFromTheme.xxs,
+    paddingLeft: getSpacingFromTheme.xs,
+  },
+  md: {
+    padding: getSpacingFromTheme.sm,
+    paddingRight: getSpacingFromTheme.xs,
+    paddingLeft: getSpacingFromTheme.sm,
+  },
+  lg: {
+    padding: getSpacingFromTheme.sm,
+    paddingRight: getSpacingFromTheme.sm,
+    paddingLeft: getSpacingFromTheme.md,
+  },
+}
 
 // Define a function to generate the spacing based on the spacing position
 interface IGenerateSpacing {
   spacingPosition?: TSpacingPosition;
-  size?: keyof typeof sizesSettings;
+  size?: TComponentSizes;
 }
 export const generateSpacing = ({ spacingPosition, size }: IGenerateSpacing) => {
   const pickedSize = size ? size : 'md';
@@ -15,15 +35,15 @@ export const generateSpacing = ({ spacingPosition, size }: IGenerateSpacing) => 
   switch (spacingPosition) {
     case 'left':
       return css`
-        padding-left: ${sizesSettings[pickedSize].paddingLeft + 2 + 'px'};
+        padding-left: ${parseFloat(sizesSettings[pickedSize].paddingLeft) + 2 + 'px'};
       `;
     case 'right':
       return css`
-        padding-right: ${sizesSettings[pickedSize].paddingRight + 2 + 'px'};
+        padding-right: ${parseFloat(sizesSettings[pickedSize].paddingRight) + 2 + 'px'};
       `;
     case 'booth':
       return css`
-        padding: 0 ${sizesSettings[pickedSize].padding + 2 + 'px'};
+        padding: 0 ${parseFloat(sizesSettings[pickedSize].padding) + 2 + 'px'};
       `;
     default:
       return null;
