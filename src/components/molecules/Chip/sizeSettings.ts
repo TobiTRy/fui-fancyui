dcd80aa327e8f6c97b02ae533ccb3e95653399ef
MrTRyy <Tobiastry@aol.de>
2023-12-16 18:46:09 +0100

    remove: sizes from fancychip

--- src/components/molecules/Chip/sizeSettings.ts
+++ src/components/molecules/Chip/sizeSettings.ts
@@ -1,35 +1,18 @@
-import { themeStore } from '@/design/theme/themeStore';
-
-const getSpacingFromTheme = themeStore.getState().theme.spacing;
-const spacings = {
-  xs: parseFloat(getSpacingFromTheme.xs),
-  sm: parseFloat(getSpacingFromTheme.sm),
-  md: parseFloat(getSpacingFromTheme.md),
-};
 
 export const sizesSettings = {
   sm: {
     height: '16px',
     deleteButtonSize: '10px',
-    padding: spacings.xs,
-    paddingRight: spacings.xs,
-    paddingLeft: spacings.xs,
     icon: '50%',
   },
   md: {
     height: '22px',
     deleteButtonSize: '12px',
-    padding: spacings.sm,
-    paddingRight: spacings.sm,
-    paddingLeft: spacings.sm,
     icon: '18px',
   },
   lg: {
     height: '28px',
     deleteButtonSize: '14px',
-    padding: spacings.sm,
-    paddingRight: spacings.sm,
-    paddingLeft: spacings.md,
-    icon: '20px',
+    icon: '22px',
   },
 };
