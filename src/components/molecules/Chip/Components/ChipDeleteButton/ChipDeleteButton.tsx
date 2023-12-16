4c2cf106771bb74192622f987383b90c41a626ec
MrTRyy <Tobiastry@aol.de>
2023-12-16 18:48:45 +0100

    refactor: ChipDeleteButton for theme variations

--- src/components/molecules/Chip/Components/ChipDeleteButton/ChipDeleteButton.tsx
+++ src/components/molecules/Chip/Components/ChipDeleteButton/ChipDeleteButton.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { sizesSettings } from '@/components/molecules/Chip/sizeSettings';
-import { TTheme } from '@/interface/TTheme';
 import { styled } from 'styled-components';
+
+import { TTheme } from '@/interface/TTheme';
+import { TLayer } from '@/interface/TLayer';
+import IStyledPrefixAndOmitter from '@/interface/IStyledPrefixAndOmiter.model';
+import { TThemeTypesNotTransparent } from '@/interface/TThemeTypesNotTransparent';
+
+import { RawButton } from '@/components/atoms/RawButton';
 import { SVGXCircle } from '@/components/icons/SVGXCircle';
-import RawButton from '@/components/atoms/RawButton/RawButton';
+import { sizesSettings } from '@/components/molecules/Chip/sizeSettings';
+import { getBackgroundColor } from '@/design/designFunctions/colorCalculatorForComponent';
 
 type IXButtonProps = {
   size?: keyof typeof sizesSettings;
   children?: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
-export default function ChipDeleteButton(props: IXButtonProps) {
-  const { size, onClick, children, 'aria-label': ariaLabel, ...htmlProps } = props;
+  themeType?: TThemeTypesNotTransparent;
+  layer?: TLayer;
+};
+
+export default function ChipDeleteButton(props: IXButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { size, onClick, children, 'aria-label': ariaLabel, layer, themeType, ...htmlProps } = props;
 
   return (
     <StyledXButton
       aria-label={ariaLabel || 'delete'}
+      $themeType={themeType}
+      $layer={layer}
       $size={size}
       onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
         e.stopPropagation();
@@ -27,13 +38,9 @@ export default function ChipDeleteButton(props: IXButtonProps) {
   );
 }
 
-interface IXButton {
-  $size?: keyof typeof sizesSettings;
-  theme: TTheme;
-}
-export const StyledXButton = styled(RawButton)<IXButton>`
+type TXButton = IStyledPrefixAndOmitter<IXButtonProps, 'children'>;
+export const StyledXButton = styled(RawButton)<TXButton & { theme: TTheme }>`
   border: none;
-  background-color: transparent;
   cursor: pointer;
   color: inherit;
   background-color: transparent;
@@ -41,6 +48,7 @@ export const StyledXButton = styled(RawButton)<IXButton>`
   line-height: 1;
   display: flex;
   align-items: center;
+  color: ${({ theme, $themeType = 'secondary', $layer = 3 }) => getBackgroundColor({ theme, $themeType, $layer })};
 
   svg {
     width: ${({ $size }) => ($size ? sizesSettings[$size].deleteButtonSize : sizesSettings.md.deleteButtonSize)};
