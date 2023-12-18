3eaafc123555bffce22792b7fc78187f751c9ec3
MrTRyy <tobiastry@aol.de>
2023-12-18 16:13:31 +0100

    Update: padding and border radius values in designFunctions and theme

--- src/design/designFunctions/generatePaddingForComponent/generatePadding.ts
+++ src/design/designFunctions/generatePaddingForComponent/generatePadding.ts
@@ -5,14 +5,14 @@ export function generatePadding(offsetPx?: number, spacingLeftRightx2?: boolean)
   const getSpacingFromTheme = themeStore.getState().theme.spacing;
 
   const sm =
-    `${parseFloat(getSpacingFromTheme.xxs) * 1.75 + offset}px` +
-    (spacingLeftRightx2 ? ` ${parseFloat(getSpacingFromTheme.xxs) * 3.5 + offset}px` : '');
+    `${parseFloat(getSpacingFromTheme.xs) + offset}px` +
+    (spacingLeftRightx2 ? ` ${parseFloat(getSpacingFromTheme.xs) * 2 + offset}px` : '');
   const md =
-    `${parseFloat(getSpacingFromTheme.xs) * 1.25 + offset}px` +
-    (spacingLeftRightx2 ? ` ${parseFloat(getSpacingFromTheme.xs) * 2.5 + offset}px` : '');
-  const lg =
-    `${parseFloat(getSpacingFromTheme.sm) * 1.5 + offset}px` +
+    `${parseFloat(getSpacingFromTheme.sm) + offset}px` +
     (spacingLeftRightx2 ? ` ${parseFloat(getSpacingFromTheme.sm) * 2 + offset}px` : '');
+  const lg =
+    `${parseFloat(getSpacingFromTheme.md) + offset}px` +
+    (spacingLeftRightx2 ? ` ${parseFloat(getSpacingFromTheme.md) * 2 + offset}px` : '');
 
   return { sm, md, lg };
 }
--- src/design/theme/designSizes.ts
+++ src/design/theme/designSizes.ts
@@ -57,11 +57,13 @@ export const fontSizes = {
 };
 
 export const borderRadius = {
-  sm: '4px',
+  xxs: '2px',
+  xs: '4px',
+  sm: '6px',
   md: '8px',
   lg: '12px',
-  xl: '18px',
-  xxl: '24px',
-  xxxl: '48px',
+  xl: '16px',
+  xxl: '20px',
+  xxxl: '32px',
   complete: '50px',
 };
