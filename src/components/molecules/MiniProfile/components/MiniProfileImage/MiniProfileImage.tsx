52e87f62f402f8797e12b91d2cb9e858f2e8e99f
MrTRyy <Tobiastry@aol.de>
2023-12-19 17:54:18 +0100

    Delete MiniProfile component and related files

--- src/components/molecules/MiniProfile/components/MiniProfileImage/MiniProfileImage.tsx
+++
@@ -1,9 +0,0 @@
-import { FancyProfilePicture } from '@/components/atoms/FancyProfilePicture';
-
-type IMiniProfileImage = React.ComponentProps<typeof FancyProfilePicture>;
-
-export default function MiniProfileImage(props: IMiniProfileImage) {
-  const { size = 'complete', rounded = 'complete', ...htmlProps } = props;
-
-  return <FancyProfilePicture size={size} rounded={rounded} {...htmlProps} />;
-}
