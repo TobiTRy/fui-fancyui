1b2227fc49bfa80f8acd6411786730a2bbd3f23d
MrTRyy <Tobiastry@aol.de>
2023-12-19 22:04:54 +0100

    Refactor FancyTabSwitch component

--- src/components/molecules/FancyTabSwitch/FancyTabSwitch.tsx
+++ src/components/molecules/FancyTabSwitch/FancyTabSwitch.tsx
@@ -12,18 +12,7 @@ import { ITabSwitchProps } from '@/components/molecules/TabSwitch/TabSwitch.mode
 // -----  The FancyTabswich has more advanced features like Label etc. ------- //
 // --------------------------------------------------------------------------- //
 export default function FancyTabSwitch(props: ITabSwitchProps & { label?: string }) {
-  const {
-    values,
-    label,
-    layer = 3,
-    themeType,
-    size,
-    disabled,
-    outlined,
-    rounded,
-    wide,
-    ...tabSwitchProps
-  } = props;
+  const { values, label, layer = 3, themeType, size, disabled, outlined, rounded, wide, ...tabSwitchProps } = props;
 
   /* Generate the unordered list for the tab switch */
   return (
