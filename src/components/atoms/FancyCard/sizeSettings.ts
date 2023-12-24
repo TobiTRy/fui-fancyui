e5f08c2b61f46dd331283ba0d7e5849c87b87262
MrTRyy <67070141+MrTRyy@users.noreply.github.com>
2023-12-24 23:58:28 +0100

    Refactor sizeSettings in FancyCard component

--- src/components/atoms/FancyCard/sizeSettings.ts
+++ src/components/atoms/FancyCard/sizeSettings.ts
@@ -1,17 +1,17 @@
-import { TSpacings } from '@/interface/TSpacings';
 import { TBorderRadiusSizes } from '@/interface/TBorderRadiusSizes';
+import { TArrayToCssValues } from '@/design/designFunctions/arrayToCssValues';
 
 export const sizeSettings = {
   sm: {
-    padding: 'xl' as TSpacings,
+    padding: ['xl'] as TArrayToCssValues,
     borderRadius: 'sm' as TBorderRadiusSizes,
   },
   md: {
-    padding: 'xl' as TSpacings,
+    padding: ['xl'] as TArrayToCssValues,
     borderRadius: 'sm' as TBorderRadiusSizes,
   },
   lg: {
-    padding: 'xl' as TSpacings,
+    padding: ['xl'] as TArrayToCssValues,
     borderRadius: 'xl' as TBorderRadiusSizes,
   },
 };
