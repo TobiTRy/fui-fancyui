d7bcc2137ec427acb9a54193f1fb4f2a7af7dfac
MrTRyy <Tobiastry@aol.de>
2023-12-26 23:24:46 +0100

    Refactor FancyMiniContentCard component

--- src/components/templates/FancyMiniContentCard/FancyMiniContentCard.style.tsx
+++ src/components/templates/FancyMiniContentCard/FancyMiniContentCard.style.tsx
@@ -1,10 +1,47 @@
-import { styled } from 'styled-components';
+import { css } from 'styled-components';
 
-import { FancyCard } from '@/components/atoms/FancyCard';
 import { TFancyMiniContentCardProps } from './FancyMiniContentCard';
 import IStyledPrefixAndPicker from '@/interface/IStyledPrefixAndPicker';
+import { themeStore } from '@/design/theme/themeStore';
 
 type IStyledCard = IStyledPrefixAndPicker<TFancyMiniContentCardProps, 'size'>;
-export const StyledCard = styled(FancyCard)<IStyledCard>`
-  flex: 1;
-`;
+
+export const generateFancyMiniContentCardStyle = (props: IStyledCard) => {
+  const getTheme = themeStore.getState().theme;
+  const { $size } = props;
+  console.log($size);
+
+  return css`
+    flex: 1;
+    border-radius: ${getTheme.borderRadius.lg};
+    border: none;
+
+    .content {
+      padding: 0 ${getTheme.spacing.md} ${getTheme.spacing.lg} ${getTheme.spacing.md};
+      width: 100%;
+      box-sizing: border-box;
+      gap: 4px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .card {
+      &_title {
+        margin-bottom: ${getTheme.spacing.xs};
+      }
+
+      &_subtitle {
+      }
+
+      &_description {
+        font-size: 0.8rem;
+        font-weight: 300;
+      }
+
+      &_image {
+        margin-bottom: ${getTheme.spacing.xs};
+        border-radius: ${getTheme.borderRadius.lg};
+      }
+    }
+  `;
+};
--- src/components/templates/FancyMiniContentCard/FancyMiniContentCard.tsx
+++ src/components/templates/FancyMiniContentCard/FancyMiniContentCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import { TComponentSizes } from '@/interface/TComponentSizes';
-import FancyCard, { IFancyCard } from '@/components/atoms/FancyCard/FancyCard';
-
-
-
+import { IFancyCard } from '@/components/atoms/FancyCard/FancyCard';
+import { generateFancyMiniContentCardStyle } from '@/components/templates/FancyMiniContentCard/FancyMiniContentCard.style';
+import { css } from 'styled-components';
+import { FancyBox } from '@/components/atoms/FancyBox';
+import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
+import CardImage from '@/components/templates/FancyMiniContentCard/Components/CardImage';
+import CardTitle from '@/components/templates/FancyMiniContentCard/Components/CardTitle';
+import CardSubTitle from '@/components/templates/FancyMiniContentCard/Components/CardSubTitle';
 
 export type TFancyMiniContentCardProps = {
   symbol?: React.ReactNode;
@@ -12,22 +16,39 @@ export type TFancyMiniContentCardProps = {
   subTitle?: string;
   description?: string;
   size?: TComponentSizes;
+  src?: string;
 } & IFancyCard;
 
-function FancyMiniContentCard(props: TFancyMiniContentCardProps) {
-  const { size, padding=['xl', 'lg'], ...cardProps } = props;
+function FancyContentCard(props: TFancyMiniContentCardProps) {
+  const { size, externalStyle, src, ...cardProps } = props;
+
+  const generateCardStyle = generateFancyMiniContentCardStyle({ $size: size });
 
   return (
-    <FancyCard size={size} padding={padding} {...cardProps}>
-      <FancyCard.FlexBox flexDirection='column'>
-        <FancyCard.Title>{props.title}</FancyCard.Title>
-        <FancyCard.SubTitle>{props.subTitle}</FancyCard.SubTitle>
-        <FancyCard.Descritpion>{props.description}</FancyCard.Descritpion>
-      </FancyCard.FlexBox>
-    </FancyCard>
+    <FancyBox
+      externalStyle={css`${generateCardStyle} ${externalStyle}`}
+      {...cardProps}
+    >
+      <FancyContentCard.FlexBox flexDirection="column">
+        <FancyContentCard.Image className="card_image" src={src} />
+        <div className="content">
+          <FancyContentCard.SubTitle layer={5} className="card_subtitle">
+            {props.subTitle}
+          </FancyContentCard.SubTitle>
+          <FancyContentCard.Title className="card_title">{props.title}</FancyContentCard.Title>
+          <FancyContentCard.Descritpion layer={3} className="card_description">
+            {props.description}
+          </FancyContentCard.Descritpion>
+        </div>
+      </FancyContentCard.FlexBox>
+    </FancyBox>
   );
 }
 
+FancyContentCard.FlexBox = FancyFlexBox;
+FancyContentCard.Image = CardImage;
+FancyContentCard.Title = CardTitle;
+FancyContentCard.SubTitle = CardSubTitle;
+FancyContentCard.Descritpion = CardSubTitle;
 
-
-export default FancyMiniContentCard;
+export default FancyContentCard;
