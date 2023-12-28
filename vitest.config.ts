fed093b83410aad87117645634b46930197222a6
MrTRyy <Tobiastry@aol.de>
2023-12-28 20:23:12 +0100

    Add isOneToZero function and its documentation

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-    include: ['**/?(*.)test.tsx?(x)'],
+    include: ['**/?(*.)test.ts?(x)', '**/?(*.)test.ts'],
   },
   resolve: {
     alias: {
