9bbf34f34b0446671b98c2c37b0b328fcc68b8be
MrTRyy <Tobiastry@aol.de>
2023-12-29 20:20:44 +0100

    Refactor FancyAlignBox component and add TFancyAlignBox model

--- src/components/templates/FancyAlignBox/FancyAlignBox.tsx
+++ src/components/templates/FancyAlignBox/FancyAlignBox.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { ElementType } from 'react';
 
-import { FancyFlexBox } from '@/components/templates/FancyFlexBox'
-import { SpacingsContainer } from '@/components/atoms/SpacingContainer'
+import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
+import { SpacingsContainer } from '@/components/atoms/SpacingContainer';
 
-export default function FancyBox() {
+import { TFancyAlignBox } from './TFancyAlignBox.model';
+
+export default function FancyAlignBox<T extends ElementType>(props: TFancyAlignBox<T>) {
+  const { justify, align, direction, gap, padding, margin, externalStyle, children, as = 'div' } = props;
   return (
-    <div>FancyBox</div>
-  )
+    <SpacingsContainer as={as} padding={padding} margin={margin}>
+      <FancyFlexBox as={as} justify={justify} align={align} direction={direction} gap={gap}>
+        {children}
+      </FancyFlexBox>
+    </SpacingsContainer>
+  );
 }
