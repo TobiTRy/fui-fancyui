eac96cb41a398b2471f425553599a37f80f37e52
MrTRyy <Tobiastry@aol.de>
2023-12-30 23:42:32 +0100

    Refactor line-height in TypographyStyleVariants

--- src/components/atoms/Typography/TypographyStyleVariants.tsx
+++ src/components/atoms/Typography/TypographyStyleVariants.tsx
@@ -18,74 +18,85 @@ const responsiveFontSize = (mobileSize: string, desktopSize: string) => css<{ th
 
 export const generateFontVariants = (themeFonts: TFontSizes, addLineHeight?: boolean) => {
   const { desktop, mobile } = themeFonts;
+
+  const getLineHeight = (addLineHeight: boolean) => {
+    if (addLineHeight) {
+      return 1.4;
+    }
+
+    return 1;
+  };
+
   const VARLineHeight = addLineHeight ? 1.4 : 1;
 
   return {
     h1: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h1, desktop.headings.h1)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     h2: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h2, desktop.headings.h2)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     h3: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h3, desktop.headings.h3)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     h4: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h4, desktop.headings.h4)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     h5: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h5, desktop.headings.h5)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     h6: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.headings.h6, desktop.headings.h6)}
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     button: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.button, desktop.textElements.button)}
       font-weight: bold;
       letter-spacing: 0.4px;
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     label: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.label, desktop.textElements.label)}
       font-weight: 400;
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     inlineElement: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.caption, desktop.textElements.caption)}
       font-weight: 400;
       line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? false)};
     `,
     content: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.caption, desktop.textElements.caption)}
       font-weight: 400;
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     smText: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.smText, desktop.textElements.smText)}
       font-weight: 400;
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
     verysmText: css`
       ${nullifyStyle};
       ${responsiveFontSize(mobile.textElements.verysmText, desktop.textElements.verysmText)}
       font-weight: 400;
-      line-height: ${VARLineHeight};
+      line-height: ${getLineHeight(addLineHeight ?? true)};
     `,
   };
 };
