4f98c9f3de868d6fbc49218340dbcd2daca4c041
MrTRyy <Tobiastry@aol.de>
2024-01-04 17:39:29 +0100

    Refactor InfoCardRoute component

--- src/Routes/InfoCardRoute/InfoCardRoute.tsx
+++ src/Routes/InfoCardRoute/InfoCardRoute.tsx
@@ -9,39 +9,41 @@ export default function InfoCardRoute() {
   return (
     <DesignWrapper>
       <DesignArea title="InfoCard">
-        <InfoCard size="sm" themeType="success" />
-        <InfoCard size="md" themeType="error" />
-        <InfoCard size="lg" themeType="warning" layer={0} />
-        <InfoCard size="lg" themeType="info" layer={0} />
-        <InfoCard>
-          <span>InfoCard</span>
-        </InfoCard>
-        <FancyInfoCard
-          size="sm"
-          themeType="info"
-          title="A Vergy good title"
-          description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
-        />
-        <FancyInfoCard
-          size="md"
-          themeType="success"
-          title="A Vergy good title"
-          description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
-          icon={<SVGCheckMark />}
-        />
-        <FancyInfoCard
-          size="lg"
-          themeType="warning"
-          title="A Vergy good title"
-          description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
-          icon={<SVGCheckMark />}
-        />
-        <FancyInfoCard
-          themeType="error"
-          title="A Vergy good title"
-          description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
-          icon={<SVGCheckMark />}
-        />
+        <div style={{display: 'flex', flexDirection: 'column', gap: '12px'}}>
+          <InfoCard size="sm" themeType="success" />
+          <InfoCard size="md" themeType="error" />
+          <InfoCard size="lg" themeType="warning" layer={0} />
+          <InfoCard size="lg" themeType="info" layer={0} />
+          <InfoCard>
+            <span>InfoCard</span>
+          </InfoCard>
+          <FancyInfoCard
+            size="sm"
+            themeType="info"
+            title="A Vergy good title"
+            description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
+          />
+          <FancyInfoCard
+            size="md"
+            themeType="success"
+            title="A Vergy good title"
+            description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
+            icon={<SVGCheckMark />}
+          />
+          <FancyInfoCard
+            size="lg"
+            themeType="warning"
+            title="A Vergy good title"
+            description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
+            icon={<SVGCheckMark />}
+          />
+          <FancyInfoCard
+            themeType="error"
+            title="A Vergy good title"
+            description="Mooiin Saying mooii jkjkjkdf jkjsjkfdsjfj sjkjkfsj kjjkfjsjkf jjsfjkskjfkjs jjkjk fsjjkfskjjkf n"
+            icon={<SVGCheckMark />}
+          />
+        </div>
       </DesignArea>
     </DesignWrapper>
   );
