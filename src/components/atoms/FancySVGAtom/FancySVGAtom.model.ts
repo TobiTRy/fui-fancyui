d4f8ed8a7db950202e27d67a2456dcf3c75aca4b
MrTRyy <Tobiastry@aol.de>
2024-01-05 15:10:34 +0100

    test: husky

--- src/components/atoms/FancySVGAtom/FancySVGAtom.model.ts
+++ src/components/atoms/FancySVGAtom/FancySVGAtom.model.ts
@@ -21,3 +21,26 @@ export type ISVGAtom = {
 export type ISVGAtomProps = ISVGAtom & Omit<React.HTMLAttributes<HTMLElement>, 'style'>;
 
 export type IStyledSVGAtom = IStyledPrefixAndOmiter<ISVGAtom, 'children'>;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
