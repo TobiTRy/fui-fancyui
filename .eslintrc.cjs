ea216e4127cf2bd560b1eea454448cf93ad3958d
MrTRyy <Tobiastry@aol.de>
2024-01-05 17:20:41 +0100

    Fix linting and formatting issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ module.exports = {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/restrict-plus-operands': 'off',
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off'
   },
   ignorePatterns: ['vite.config.ts', 'vitest.config.ts', '.eslintrc.cjs'],
 };
