18281c61b0f9e294dcf9e2e72e909fef1ebdbae5
MrTRyy <Tobiastry@aol.de>
2024-01-13 23:21:37 +0100

    Delete LabeledRangeSlider component and related files

--- src/components/molecules/LabeledRangeSlider/TLabeledRangeSlider.model.ts
+++
@@ -1,7 +0,0 @@
-import { TRawSliderWithNativeAttrs } from '@/components/atoms/RawSlider';
-
-export type TLabeledRangeSlider = {
-  label?: string;
-};
-
-export type TLabeledRangeSliderWithNativeAttrs = TLabeledRangeSlider & TRawSliderWithNativeAttrs;
--- src/components/molecules/LabeledRangeSlider/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LabeledRangeSlider } from './LabeledRangeSlider';
-
-export type { TLabeledRangeSlider, TLabeledRangeSliderWithNativeAttrs } from './TLabeledRangeSlider.model';
