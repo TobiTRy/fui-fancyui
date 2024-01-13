52cd56852cd7b10ac31fbdb5c104fc8b81a4f369
MrTRyy <Tobiastry@aol.de>
2024-01-13 22:36:38 +0100

    Add generateInputWrapperStyles function to FancyRangeSlider.style.tsx

--- src/components/organisms/FancyRangeSlider/FancyRangeSlider.style.tsx
+++ src/components/organisms/FancyRangeSlider/FancyRangeSlider.style.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 type TgenerateInputWrapperStyles = {
   hasNumberInput?: boolean;
 };
+// eslint-disable-next-line react-refresh/only-export-components
 export const generateInputWrapperStyles = ({ hasNumberInput }: TgenerateInputWrapperStyles) => {
   if (hasNumberInput) {
     return css`
