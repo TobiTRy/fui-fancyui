17fb821315dd9372e770aaa942aceec95ea96df9
MrTRyy <Tobiastry@aol.de>
2024-01-15 19:05:48 +0100

    Refactor ChipImg component

--- src/components/molecules/Chip/Components/ChipImg/ChipImg.tsx
+++ src/components/molecules/Chip/Components/ChipImg/ChipImg.tsx
@@ -1,23 +1,5 @@
-import { styled } from 'styled-components';
-
-import { TTheme } from '@/types/TTheme';
-
 import { FancyImageWrapper, TFancyImageWrapper } from '@/components/atoms/FancyImageWrapper';
 
 export default function ChipImg(props: TFancyImageWrapper) {
-  return (
-    <WrapperImage>
-      <FancyImageWrapper aspectRatio="1/1" sizeH="fit" objectFit="cover" borderRadius="complete" {...props} />
-    </WrapperImage>
-  );
+  return <FancyImageWrapper aspectRatio="1/1" sizeH="fit" objectFit="cover" borderRadius="complete" {...props} />;
 }
-
-// Define the styled component for the wrapper image
-
-export const WrapperImage = styled.div<{ theme: TTheme }>`
-  height: 100%;
-
-  img {
-    grid-row: 1;
-  }
-`;
