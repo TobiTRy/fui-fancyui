de26e1bdd664e8a5c6f31a73239466c48d739645
MrTRyy <Tobiastry@aol.de>
2024-01-22 21:32:39 +0100

    update: searchbar types

---
+++ src/components/molecules/SearchBar/TSeachbar.model.tsx
@@ -0,0 +1,15 @@
+import { TComponentSizes } from '@/types/TComponentSizes';
+import { TLayer } from '@/types/TLayer';
+import { TTextAlignLC } from '@/types/TTextAlignLC';
+import { TThemeTypesNotTransparent } from '@/types/TThemeTypesNotTransparent';
+
+export type TSearchBar = {
+  searchValue?: string;
+  align?: TTextAlignLC;
+  activeHandler?: (isActive: boolean) => void;
+  handler?: (value: string) => void;
+  themeType?: TThemeTypesNotTransparent;
+  layer?: TLayer;
+  placerholder?: string;
+  size?: TComponentSizes;
+};
