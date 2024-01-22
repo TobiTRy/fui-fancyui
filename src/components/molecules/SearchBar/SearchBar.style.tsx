08d4021b59fe018b3a8501e80dd1eb9c14a9e131
MrTRyy <tobiastry@aol.de>
2024-01-22 15:59:30 +0100

    update: searchbar better design

--- src/components/molecules/SearchBar/SearchBar.style.tsx
+++ src/components/molecules/SearchBar/SearchBar.style.tsx
@@ -8,17 +8,17 @@ import { sizeSettings } from './sizeSettings';
 // ------------------------------------------- //
 
 // Styled component for the search bar
-export const StyledSearchBar = styled.div<{ $isActive?: boolean; theme: TTheme; size: TComponentSizes }>`
+export const StyledSearchBar = styled.div<{ $isActive?: boolean; theme: TTheme; $size: TComponentSizes }>`
   display: flex;
   align-items: center;
-  border-radius: ${({ $isActive, theme, size }) =>
+  border-radius: ${({ $isActive, theme, $size }) =>
     $isActive
       ? `${theme.borderRadius.lg} ${theme.borderRadius.lg} 0px 0px`
-      : sizeSettings[size].borderRadius}; // Set the border radius based on whether the search bar list is active
+      : sizeSettings[$size].borderRadius}; // Set the border radius based on whether the search bar list is active
   z-index: 1;
 
-  height: ${({ size }) => sizeSettings[size].height};
-  padding: ${({ size }) => sizeSettings[size].padding};
+  height: ${({ $size }) => sizeSettings[$size].height};
+  padding: ${({ $size }) => sizeSettings[$size].padding};
   gap: 4px;
 `;
 
@@ -26,10 +26,20 @@ export const SizeWrapper = styled.div`
   width: auto;
 `;
 
-export const InputWrapper = styled.div`
+export const InputWrapper = styled.div<{ $size: TComponentSizes }>`
   width: 100%;
 
   div {
     padding: 0;
+    div {
+      input {
+        font-size: ${({ $size }) => ($size === 'sm' ? '16px' : '')} !important;
+      }
+      gap: ${({ $size }) => sizeSettings[$size].gap};
+    }
   }
+`;
+
+export const IconWrapper = styled.span<{ $size: TComponentSizes }>`
+  width: ${({ $size }) => sizeSettings[$size].iconSize};
 `;
--- src/components/molecules/SearchBar/SearchBar.tsx
+++ src/components/molecules/SearchBar/SearchBar.tsx
@@ -1,15 +1,11 @@
 import { ChangeEvent, useState } from 'react';
-import { css } from 'styled-components';
 
-import { FancySVGAtom } from '@/components/atoms/FancySVGAtom';
 import { SVGSearch } from '@/components/icons/SVGSearch';
 import { FancyTextInput } from '@/components/organisms/FancyTextInput';
 
 import { InputWrapper, SizeWrapper, StyledSearchBar } from '@/components/molecules/SearchBar/SearchBar.style';
 import { TSearchBar } from '@/components/molecules/SearchBar/TSeachbar.model';
-import { RawInput } from '@/components/atoms/RawInput';
-import { TextInput } from '@/components/atoms/TextInput';
-import { InputUnderline } from '@/components/atoms/InputUnderline';
+import { IconWrapper } from './SearchBar.style';
 
 // --------------------------------------------------------------------------- //
 // ---------- Seachbar like for a header to search something  ---------------- //
@@ -17,7 +13,6 @@ import { InputUnderline } from '@/components/atoms/InputUnderline';
 export default function SearchBar(props: TSearchBar) {
   const { activeHandler, handler, searchValue, align = 'center', themeType, layer = 0, size = 'sm' } = props;
   const [isActive, setIsActive] = useState(false); // The state for the isActive state of the search bar
-  const [isOpen, setIsOpen] = useState(false); // The state for the isOpen state of the search bar
 
   // Function to handle changes to the isActive state
   const focusHandler = (isFocused: boolean) => {
@@ -32,11 +27,13 @@ export default function SearchBar(props: TSearchBar) {
 
   return (
     <SizeWrapper>
-      <StyledSearchBar size={size} $isActive={isActive}>
+      <StyledSearchBar $size={size} $isActive={isActive}>
         {/* The search icon */}
-        <InputWrapper>
+        <InputWrapper $size={size}>
           <FancyTextInput
-            icon={<span>{SVGSearch}</span>}
+            layer={layer}
+            themeType={themeType}
+            icon={<IconWrapper $size={size}>{SVGSearch}</IconWrapper>}
             transparentBackground
             value={searchValue}
             placeholder="Search"
