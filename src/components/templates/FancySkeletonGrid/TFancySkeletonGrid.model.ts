cfd16e9829ce91058c3c25dea0c10cfc17817e57
MrTRyy <Tobiastry@aol.de>
2024-01-23 21:27:49 +0100

    split: types out

---
+++ src/components/templates/FancySkeletonGrid/TFancySkeletonGrid.model.ts
@@ -0,0 +1,4 @@
+import { TSkeletonBox } from '@/components/atoms/SkeletonBox';
+import { TFancyGridTemplate } from '@/components/templates/FancyGridTemplate';
+
+export type TFancySkeletonGrid = Omit<TFancyGridTemplate, 'children'> & Omit<TSkeletonBox, 'aspectRatio'>;
