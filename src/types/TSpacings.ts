78a920c6b99bb6577f57914f6e0da0920420a89a
MrTRyy <tobiastry@aol.de>
2024-01-24 09:15:30 +0100

    Add TSpacingArray type definition

--- src/types/TSpacings.ts
+++ src/types/TSpacings.ts
@@ -1,3 +1,5 @@
 import { spacingPx } from '../design/theme/designSizes';
 
 export type TSpacings = keyof typeof spacingPx;
+
+export type TSpacingArray = [TSpacings?, TSpacings?, TSpacings?, TSpacings?];
