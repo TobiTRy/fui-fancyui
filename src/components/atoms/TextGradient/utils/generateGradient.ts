c6a793e61c370785b55be1b2716dc9d5b58e752c
MrTRyy <tobiastry@aol.de>
2024-02-03 00:14:06 +0100

    Refactor generateGradient function to adjust radial position

--- src/components/atoms/TextGradient/utils/generateGradient.ts
+++ src/components/atoms/TextGradient/utils/generateGradient.ts
@@ -1,4 +1,8 @@
-import { TGradientColor, TTextGradientProps } from '@/components/atoms/TextGradient/TextGradient.model';
+import {
+  GradientPosition,
+  TGradientColor,
+  TTextGradientProps,
+} from '@/components/atoms/TextGradient/TextGradient.model';
 import { getLinearGradientPosition } from '@/components/atoms/TextGradient/utils/getPositionToGradientPosition';
 
 export const generateGradient = (props: Omit<TTextGradientProps, 'children'>) => {
@@ -29,22 +33,22 @@ export const generateGradient = (props: Omit<TTextGradientProps, 'children'>) =>
       gradient = `linear-gradient(${linearPosition}, ${colorString})`;
       break;
     }
-    case 'radial':
-      gradient = `radial-gradient(circle at ${position || 'center'}, ${colorString})`;
-      break;
-    case 'elliptical':
-      gradient = `radial-gradient(ellipse at ${position || 'center'}, ${colorString})`;
-      break;
     case 'repeatLinear': {
       const linearPosition = position ? getLinearGradientPosition(position) : `${degree || 90}deg`;
       gradient = `repeating-linear-gradient(${linearPosition}, ${colorString})`;
       break;
     }
+    case 'radial':
+      gradient = `radial-gradient(circle at ${adjustPositionForRadial(position)}, ${colorString})`;
+      break;
     case 'repeatRadial':
-      gradient = `repeating-radial-gradient(circle at ${position || 'center'}, ${colorString})`;
+      gradient = `repeating-radial-gradient(circle at ${adjustPositionForRadial(position)}, ${colorString})`;
+      break;
+    case 'elliptical':
+      gradient = `radial-gradient(ellipse at ${adjustPositionForRadial(position)}, ${colorString})`;
       break;
     case 'repeatElliptical':
-      gradient = `repeating-radial-gradient(ellipse at ${position || 'center'}, ${colorString})`;
+      gradient = `repeating-radial-gradient(ellipse at ${adjustPositionForRadial(position)}, ${colorString})`;
       break;
     default:
       gradient = `linear-gradient(${degree || 90}deg, ${colorString})`;
@@ -52,4 +56,8 @@ export const generateGradient = (props: Omit<TTextGradientProps, 'children'>) =>
   }
 
   return gradient;
+};
+
+const adjustPositionForRadial = (position?: GradientPosition) => {
+  return position?.replace('-', ' ') || 'center';
 };
