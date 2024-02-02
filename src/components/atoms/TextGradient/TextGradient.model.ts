f7f7f8ffafef2721cccc4b4686a8dc561a2c4e57
MrTRyy <tobiastry@aol.de>
2024-02-03 00:13:26 +0100

    Refactor GradientOrientation type and add new union types for linear and non-linear gradients

--- src/components/atoms/TextGradient/TextGradient.model.ts
+++ src/components/atoms/TextGradient/TextGradient.model.ts
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export type GradientOrientation =
-  | 'linear'
-  | 'radial'
-  | 'elliptical'
-  | 'repeatLinear'
-  | 'repeatRadial'
-  | 'repeatElliptical';
+export type LinearOrientation = 'linear' | 'repeatLinear';
+export type NonLinearOrientation = 'radial' | 'elliptical' | 'repeatRadial' | 'repeatElliptical';
+
+export type GradientOrientation = LinearOrientation | NonLinearOrientation;
+
 export type GradientPosition =
   | 'left'
   | 'right'
@@ -23,10 +21,33 @@ export type TGradientColor = {
   stop?: string; // This can be a percentage or a pixel value (e.g., '50%', '100px')
 };
 
-export type TTextGradientProps = {
+type BaseGradientProps = {
   children: React.ReactNode;
-  degree?: number;
   colors: string[] | TGradientColor[];
   orientation?: GradientOrientation;
+};
+
+// This is a union type that will only allow one of the two properties to be present
+type avalableAlignment =
+  | {
+      degree: number;
+      position?: never;
+    }
+  | {
+      degree?: never;
+      position: GradientPosition;
+    };
+
+// This is a union type that will only allow one of the two properties to be present
+type LinearGradientProps = BaseGradientProps & {
+  orientation: LinearOrientation;
+} & avalableAlignment;
+
+// This is a union type that will only allow one of the two properties to be present
+type NonLinearGradientProps = BaseGradientProps & {
+  orientation?: NonLinearOrientation;
+  degree?: never;
   position?: GradientPosition;
 };
+
+export type TTextGradientProps = LinearGradientProps | NonLinearGradientProps;
