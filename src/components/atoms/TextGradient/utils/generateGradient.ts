5a226bf94964f9fa69c17a0268068d448c90b1ec
MrTRyy <tobiastry@aol.de>
2024-02-02 15:01:56 +0100

    buildIn: TextGradientStyle

---
+++ src/components/atoms/TextGradient/utils/generateGradient.ts
@@ -0,0 +1,55 @@
+import { TGradientColor, TTextGradientProps } from '@/components/atoms/TextGradient/TextGradient.model';
+import { getLinearGradientPosition } from '@/components/atoms/TextGradient/utils/getPositionToGradientPosition';
+
+export const generateGradient = (props: Omit<TTextGradientProps, 'children'>) => {
+  const { degree, colors, orientation, position } = props;
+
+  // Construct color string with stop positions
+  let modifidColors: string[] | undefined;
+
+  // If colors is an object, it means it's an array of TGradientColor
+  if (typeof colors[0] === 'object') {
+    const colorObj = colors as TGradientColor[];
+    modifidColors = colorObj.map((colorObj) => {
+      return `${colorObj.color}${colorObj.stop ? ' ' + colorObj.stop : ''}`;
+    });
+  } else {
+    // If colors is an array of strings, just use it as is
+    modifidColors = colors as string[];
+  }
+
+  // Join the colors with a comma
+  const colorString = modifidColors.join(', ');
+
+  let gradient = '';
+
+  switch (orientation) {
+    case 'linear': {
+      const linearPosition = position ? getLinearGradientPosition(position) : `${degree || 90}deg`;
+      gradient = `linear-gradient(${linearPosition}, ${colorString})`;
+      break;
+    }
+    case 'radial':
+      gradient = `radial-gradient(circle at ${position || 'center'}, ${colorString})`;
+      break;
+    case 'elliptical':
+      gradient = `radial-gradient(ellipse at ${position || 'center'}, ${colorString})`;
+      break;
+    case 'repeatLinear': {
+      const linearPosition = position ? getLinearGradientPosition(position) : `${degree || 90}deg`;
+      gradient = `repeating-linear-gradient(${linearPosition}, ${colorString})`;
+      break;
+    }
+    case 'repeatRadial':
+      gradient = `repeating-radial-gradient(circle at ${position || 'center'}, ${colorString})`;
+      break;
+    case 'repeatElliptical':
+      gradient = `repeating-radial-gradient(ellipse at ${position || 'center'}, ${colorString})`;
+      break;
+    default:
+      gradient = `linear-gradient(${degree || 90}deg, ${colorString})`;
+      break;
+  }
+
+  return gradient;
+};
---
+++ src/components/atoms/TextGradient/utils/getPositionToGradientPosition.ts
@@ -0,0 +1,25 @@
+import { GradientPosition } from '@/components/atoms/TextGradient/TextGradient.model';
+
+// Helper function to get position for linear gradients
+export const getLinearGradientPosition = (position: GradientPosition | undefined) => {
+  switch (position) {
+    case 'left':
+      return 'to right';
+    case 'right':
+      return 'to left';
+    case 'top':
+      return 'to bottom';
+    case 'bottom':
+      return 'to top';
+    case 'top-left':
+      return 'to bottom right';
+    case 'top-right':
+      return 'to bottom left';
+    case 'bottom-left':
+      return 'to top right';
+    case 'bottom-right':
+      return 'to top left';
+    default:
+      return 'to bottom';
+  }
+};
