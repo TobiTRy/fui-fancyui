e2fcfc2acf898281ca15af9379408cfc7e435b32
MrTRyy <tobiastry@aol.de>
2024-02-06 18:05:56 +0100

    Add new hooks and refactor RangeCalendar component

--- src/components/molecules/RangeCalendar/RangeCalendar.tsx
+++ src/components/molecules/RangeCalendar/RangeCalendar.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useRef, useState } from 'react';
+import { RefObject, useEffect, useMemo, useRef, useState } from 'react';
 
 import { MonthContainer, StyledCalendar } from './RangeCalendar.style';
 
@@ -8,6 +8,8 @@ import useSelectedDates from './helperFunctions/useSelectedDates';
 import { IExternalMonthWithDays } from '@/components/molecules/MonthWithDays/utils/types/IExternalMonthWithDays.model';
 import { TRangeCalendar } from '@/components/molecules/RangeCalendar/TRangeCalendar.model';
 import { scrollToElm } from '@/utils/functions/scrollToElementInContainer/scrollToElementInContainer';
+import { useShowAreaOfArray } from '@/utils/hooks/useShowAreaOfArray';
+import { useIntersectionObserver } from '@/utils/hooks/useIntersectionObserver';
 
 // --------------------------------------------------------------------------- //
 // -------- The main calenader wich can select a date, or date range --------- //
@@ -24,8 +26,20 @@ export default function RangeCalendar(props: TRangeCalendar) {
     themeType,
     layer,
   } = props;
-  const monthRefs = useRef<(HTMLDivElement | null)[]>([]);
   const ContainerRef: RefObject<HTMLDivElement> = useRef(null);
+  const [monthDummyArray] = useState<number[]>(Array.from({ length: 12 }, (_, index) => index));
+
+  const monthRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const lastRef = useRef<HTMLDivElement | null>(null);
+
+  const isVisible = useIntersectionObserver(lastRef);
+
+  const { areaItems } = useShowAreaOfArray({
+    areaBackward: 1,
+    areaForward: 1,
+    areaStart: new Date().getMonth(),
+    array: monthDummyArray,
+  });
 
   const [externalMonthsData, setExternalMonthsData] = useState<IExternalMonthWithDays[]>([]);
   // hooks for selected dates and visible months
@@ -39,10 +53,12 @@ export default function RangeCalendar(props: TRangeCalendar) {
 
   // Scroll to current month on mount and set isScrolled to true
   useEffect(() => {
-    const currentMonth = new Date().getMonth();
-    scrollToElm(ContainerRef.current as HTMLElement, monthRefs.current[currentMonth] as HTMLElement, 0);
-  }, []);
+    if (monthRefs.current.length > 0) {
+      scrollToElm(ContainerRef.current as HTMLElement, monthRefs.current[new Date().getMonth()] as HTMLElement, 0);
+    }
+  }, [areaItems]);
 
+  // this handles the external state of the months with days
   useEffect(() => {
     // compare the year of the external state with the selected year
     if (externalMonthsWithDays) {
@@ -59,28 +75,33 @@ export default function RangeCalendar(props: TRangeCalendar) {
 
   return (
     <StyledCalendar ref={ContainerRef}>
-      {Array.from({ length: 12 }).map((_, idx) => {
-        return (
-          <MonthContainer
-            key={idx}
-            ref={(ref: HTMLDivElement) => {
-              monthRefs.current[idx] = ref;
-            }}
-          >
-            <MonthWithDays
-              disabledDateSetting={disabledDateSetting}
-              monthIdx={idx}
-              externalMonthWithDays={externalMonthsData[idx]}
-              year={selectedYear}
-              themeType={themeType}
-              layer={layer}
-              handleDateClick={handleDateClick}
-              isRangePicking={rangeCalendar}
-              selectedDates={selectedDates}
-            />
-          </MonthContainer>
-        );
-      })}
+      {areaItems.length > 0 &&
+        areaItems.map((monthIdx) => {
+          return (
+            <MonthContainer
+              key={monthIdx}
+              data-month={monthIdx}
+              ref={(ref) => {
+                monthRefs.current[monthIdx] = ref;
+                if (monthIdx === areaItems[areaItems.length - 1]) {
+                  lastRef.current = ref;
+                }
+              }}
+            >
+              <MonthWithDays
+                disabledDateSetting={disabledDateSetting}
+                monthIdx={monthIdx}
+                externalMonthWithDays={externalMonthsData[monthIdx]}
+                year={selectedYear}
+                themeType={themeType}
+                layer={layer}
+                handleDateClick={handleDateClick}
+                isRangePicking={rangeCalendar}
+                selectedDates={selectedDates}
+              />
+            </MonthContainer>
+          );
+        })}
     </StyledCalendar>
   );
 }
