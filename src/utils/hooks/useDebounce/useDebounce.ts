3c32233260908f079cc83344b5a24ccacebf908b
MrTRyy <Tobiastry@aol.de>
2024-02-08 22:55:59 +0100

    Add debounce utility function and useDebounce hook

---
+++ src/utils/hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,30 @@
+import { useCallback, useRef } from 'react';
+
+type CallbackFunction = () => void;
+
+export default function useDebounce(callback: CallbackFunction, delay: number) {
+  const callbackRef = useRef<CallbackFunction>(callback);
+  // Verwenden Sie useRef, um den Timer zu speichern, anstatt eine globale Variable zu ändern
+  const timerRef = useRef<NodeJS.Timeout | number>();
+
+  callbackRef.current = callback;
+
+  const debouncedAction = useCallback(() => {
+    const handler = () => callbackRef.current();
+
+    // Löschen des vorhandenen Timers und Starten eines neuen
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(handler, delay);
+
+    // Cleanup-Funktion, um den Timer zu löschen, wenn die Komponente entladen wird
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [delay]);
+
+  return debouncedAction;
+}
