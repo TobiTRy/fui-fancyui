3bd4511b761367f0ff235cc8f58d5d6e084bb832
MrTRyy <Tobiastry@aol.de>
2024-02-11 20:09:04 +0100

    Refactor debounce function and add documentation

--- src/utils/functions/debounce/debounce.ts
+++ src/utils/functions/debounce/debounce.ts
@@ -1,3 +1,6 @@
+// --------------------------------------------------------------------------- //
+// ------------------- da function that handles a debounce ------------------- //
+// --------------------------------------------------------------------------- //
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function debounce<T extends (...args: any[]) => void>(func: T, delay: number): (...args: Parameters<T>) => void {
   let timerId: number | undefined;
