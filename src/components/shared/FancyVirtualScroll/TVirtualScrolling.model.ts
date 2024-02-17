4330db1d8eaf379065df27f079f10c8e986aa566
MrTRyy <Tobiastry@aol.de>
2024-02-17 21:43:14 +0100

    Add FancyVirtualScroll component with styled components

---
+++ src/components/shared/FancyVirtualScroll/TVirtualScrolling.model.ts
@@ -0,0 +1,14 @@
+export type TItem = {
+  content: React.ReactNode;
+  originalIndex: number;
+};
+
+export type TVirtualScrollProps = {
+  children: React.ReactNode[];
+  containerHeight?: string;
+  itemHeight?: number;
+  initialItemIndex?: number;
+  enableScrollSnap?: boolean;
+  itemGap?: number; // New prop for specifying the gap between children
+  currentItemsInView?: (index: number) => void;
+};
