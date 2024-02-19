d71a5a3aaaa4a3f7e1013aeb9d140f91d6481858
MrTRyy <tobiastry@aol.de>
2024-02-19 16:39:51 +0100

    Add throttle function to utils

---
+++ src/utils/functions/throttle/throttle.ts
@@ -0,0 +1,26 @@
+function throttle<T extends (...args: any[]) => any>(func: T, limit: number): (...args: Parameters<T>) => void {
+  let inThrottle: boolean;
+  let lastFunc: ReturnType<typeof setTimeout>;
+  let lastRan: number;
+
+  return function (...args: Parameters<T>) {
+    if (!inThrottle) {
+      func(...args);
+      lastRan = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(
+        () => {
+          if (Date.now() - lastRan >= limit) {
+            func(...args);
+            lastRan = Date.now();
+          }
+        },
+        limit - (Date.now() - lastRan)
+      );
+    }
+  };
+}
+
+export default throttle;
