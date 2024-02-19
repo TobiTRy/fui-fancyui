376eb35fd51e0e7120dc76fd44555f34c2179083
MrTRyy <tobiastry@aol.de>
2024-02-19 16:40:49 +0100

    Refactor FancyVirtualScroll component

--- src/components/shared/FancyVirtualScroll/FancyVirtualScroll.tsx
+++ src/components/shared/FancyVirtualScroll/FancyVirtualScroll.tsx
@@ -1,96 +1,83 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { Item, Wrapper } from './FancyVirtualScroll.style';
-
-import { TItem, TVirtualScrollProps } from './TVirtualScroll.model';
-import { debounce } from '@/utils/functions/debounce';
+import { TVirtualScrollProps } from './TVirtualScroll.model';
 
 export default function FancyVirtualScroll(props: TVirtualScrollProps) {
   const {
     children,
     containerHeight = '300px',
     itemHeight = 300,
-    preRenderCount = 3,
+    preRenderCount = 1,
     itemIndexInView = 2,
-    scrollSnap = 'mandatory',
+    scrollSnap = 'proximity',
     itemGap = 0,
     currentItemsInViewHandler,
   } = props;
+
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [viewportItems, setViewportItems] = useState<TItem[]>([]);
-  const [itemIndexInViewState, setitemIndexInViewState] = useState(itemIndexInView);
+  const [itemIndexInViewState, setItemIndexInViewState] = useState(itemIndexInView);
 
-  const rangeHeight = children.length * (itemHeight + itemGap) - itemGap;
+  // Memoized items to render
+  // This is to avoid recalculating the items to render on every render
+  const renderdItems = useMemo(() => {
+    const startIdx = Math.max(0, itemIndexInViewState - preRenderCount);
+    const endIdx = Math.min(children.length - 1, itemIndexInViewState + preRenderCount);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const debouncedOnVisibleItemsChange = useCallback(
-    debounce((index: number) => currentItemsInViewHandler?.(index), 100),
-    [currentItemsInViewHandler]
-  );
+    // give the index of the item that is currently in view to the parent component
+    currentItemsInViewHandler?.(itemIndexInViewState);
 
-  const calcHeightFromIndex = (index: number) => {
-    console.log(index, 'index');
-    return index * (itemHeight + itemGap);
-  };
+    return children.slice(startIdx, endIdx + 1).map((content, index) => ({
+      content,
+      originalIndex: startIdx + index,
+    }));
+  }, [children, preRenderCount, itemIndexInViewState]);
 
-  const calcIndexFromHeight = (scrollTop: number, itemHeight: number) => {
-    // This calculates the index at the current scroll position by dividing the scrollTop height by the height of each item.
-    // Math.floor is used to ensure the index is a whole number, indicating the first fully visible item at the scrollTop position.
-    return Math.floor(scrollTop / itemHeight);
-  };
-
-  const calculateVisibleItems = useCallback(() => {
+  // Calculate the index of the item that is currently in view and update the state
+  const calculateVisibleItemIdx = useCallback(() => {
     const container = containerRef.current;
     if (!container) return;
 
     const containerScrollTop = container.scrollTop;
-    const containerHeight = container.clientHeight;
     const itemHeightWithGap = itemHeight + itemGap;
+    const indexFromHeight = Math.floor(containerScrollTop / itemHeightWithGap);
 
-    const scrollTopIndexPosition = itemHeightWithGap * itemIndexInViewState - itemHeight;
+    setItemIndexInViewState(indexFromHeight);
+  }, [itemHeight, itemGap]);
 
-    const toTopOfIndexItem = calcHeightFromIndex(itemIndexInViewState);
-
-    const indexFromHeight = calcIndexFromHeight(containerScrollTop, itemHeightWithGap);
-    console.log('indexFromHeight', indexFromHeight, toTopOfIndexItem);
-
-    // get index of the main index item
-    // add the preRenderCount to the index and calculate the start and end index from that
-
-    console.log('indexOfIndexItem', toTopOfIndexItem);
-
-    const start = Math.max(0, Math.floor(toTopOfIndexItem - preRenderCount));
-    const end = Math.min(start, Math.floor(toTopOfIndexItem + preRenderCount) + 1);
-
-    //generate the start and end index from
-
-    console.log('start', start, 'end', end);
-
-    //update index
-    setitemIndexInViewState(start);
-
-    const items = children.slice(start, end);
-    setViewportItems(items.map((content, index) => ({ content, originalIndex: start + index })));
-
-    console.log('scrollTopIndexPosition', scrollTopIndexPosition);
-  }, []);
-
+  // this effect is for the initial setup of the scroll event
+  // and the cleanup of the event listener
   useEffect(() => {
-    calculateVisibleItems();
     const container = containerRef.current;
-    container?.addEventListener('scroll', calculateVisibleItems);
+    if (!container) return;
 
-    return () => container?.removeEventListener('scroll', calculateVisibleItems);
-  }, [calculateVisibleItems]);
+    // Initial calculation and setup for scroll event
+    container.addEventListener('scroll', () => {
+      calculateVisibleItemIdx();
+    });
 
+    return () => {
+      container.removeEventListener('scroll', calculateVisibleItemIdx);
+    };
+  }, [calculateVisibleItemIdx]);
+
+  // Adjusts scroll position when itemIndexInView prop changes
   useEffect(() => {
-    setitemIndexInViewState(itemIndexInView);
-  }, [itemIndexInView]);
+    const container = containerRef.current;
+    if (!container) return;
+
+    const newScrollTop = itemIndexInView * (itemHeight + itemGap); // Adjust so the item is fully in view
+    container.scrollTop = newScrollTop;
+    setItemIndexInViewState(itemIndexInView);
+
+    // Force recalculation after adjusting scroll position
+    calculateVisibleItemIdx();
+  }, [itemIndexInView, itemHeight, itemGap, calculateVisibleItemIdx]);
 
   return (
     <Wrapper ref={containerRef} $scrollSnap={scrollSnap} $containerHeight={containerHeight}>
-      <div style={{ position: 'relative', height: `${rangeHeight}px` }}>
-        {viewportItems.map(({ content, originalIndex }) => (
+      <div style={{ position: 'relative', height: `${children.length * (itemHeight + itemGap) - itemGap}px` }}>
+        {renderdItems.map(({ content, originalIndex }) => (
           <Item
             style={{ top: `${originalIndex * (itemHeight + itemGap)}px` }}
             key={originalIndex}
