95ace58e1922e3843f3f82b38d82ec4cf766d266
MrTRyy <tobiastry@aol.de>
2024-02-20 20:19:27 +0100

    Update FancyVirtualScroll styles

--- src/components/shared/FancyVirtualScroll/FancyVirtualScroll.style.tsx
+++ src/components/shared/FancyVirtualScroll/FancyVirtualScroll.style.tsx
@@ -1,23 +1,28 @@
-import { styled } from 'styled-components';
+import { CSSProp, styled } from 'styled-components';
 import { TVirtualScrollProps } from './TVirtualScroll.model';
 import { TStyledPrefixAndPicker } from '@/types/TStyledPrefixAndPicker';
 
-type TWrapper = TStyledPrefixAndPicker<TVirtualScrollProps, 'containerHeight' | 'scrollSnap'>;
+type TWrapper = TStyledPrefixAndPicker<TVirtualScrollProps, 'containerHeight' | 'scrollSnap'> & {
+  $externalStyle?: CSSProp;
+  $scrollBehaviorOn?: boolean;
+};
 
 export const Wrapper = styled.div<TWrapper>`
   height: ${({ $containerHeight }) => $containerHeight};
-  overflow-y: scroll;
   scroll-snap-type: ${({ $scrollSnap }) => ($scrollSnap ? `y ${$scrollSnap}` : 'none')};
+  overflow-y: scroll;
 
   &::-webkit-scrollbar {
     width: 4px;
     overflow: hidden;
   }
+  ${({ $externalStyle }) => $externalStyle};
 `;
 
 type TItem = {
   $itemHeight: string;
   $enableScrollSnap: boolean;
+  $externalStyle?: CSSProp;
 };
 export const Item = styled.div<TItem>`
   position: absolute;
@@ -27,4 +32,5 @@ export const Item = styled.div<TItem>`
   overflow: hidden;
   height: ${({ $itemHeight }) => $itemHeight};
   scroll-snap-align: ${({ $enableScrollSnap }) => ($enableScrollSnap ? 'start' : 'none')};
+  ${({ $externalStyle }) => $externalStyle};
 `;
