a9f1789858a8d301b98a38487c47c9f9b8af925a
MrTRyy <tobiastry@aol.de>
2024-02-20 20:20:42 +0100

    Refactor FancyVirtualScroll component and add new props

--- src/components/shared/FancyVirtualScroll/FancyVirtualScroll.tsx
+++ src/components/shared/FancyVirtualScroll/FancyVirtualScroll.tsx
@@ -9,42 +9,56 @@ export default function FancyVirtualScroll(props: TVirtualScrollProps) {
     containerHeight = '300px',
     itemHeight = 300,
     preRenderCount = 1,
-    itemIndexInView = 0,
-    scrollSnap = 'proximity',
+    firstItemIndexInView = 0,
+    scrollSnap = 'none',
     itemGap = 0,
     currentItemsInViewHandler,
+    attributesContainer,
+    attributesItems,
+    onScrollingStateChange,
   } = props;
+
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [itemIndexInViewState, setItemIndexInViewState] = useState(itemIndexInView);
+  const [firstItemIndexInViewState, setfirstItemIndexInViewState] = useState(firstItemIndexInView);
   const [indexChange, setIndexChange] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const scrollTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   // Memoized items to render
   // This is to avoid recalculating the items to render on every render
   const renderdItems = useMemo(() => {
-    const startIdx = Math.max(0, itemIndexInViewState - preRenderCount);
-    const endIdx = Math.min(children.length - 1, itemIndexInViewState + preRenderCount);
+    const indexToUse = indexChange ? firstItemIndexInView : firstItemIndexInViewState;
+
+    const startIdx = Math.max(0, indexToUse - preRenderCount);
+    const endIdx = Math.min(children.length - 1, indexToUse + preRenderCount);
 
     // give the index of the item that is currently in view to the parent component
-    currentItemsInViewHandler?.(itemIndexInViewState);
+    currentItemsInViewHandler?.(indexToUse);
+    setIndexChange(false);
 
     return children.slice(startIdx, endIdx + 1).map((content, index) => ({
       content,
       originalIndex: startIdx + index,
     }));
-  }, [children, preRenderCount, itemIndexInViewState]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [children, preRenderCount, firstItemIndexInViewState, currentItemsInViewHandler, indexChange]);
 
   // Calculate the index of the item that is currently in view and update the state
   const calculateVisibleItemIdx = useCallback(() => {
     const container = containerRef.current;
     if (!container) return;
 
+    if (indexChange) {
+      return;
+    }
+
     const containerScrollTop = container.scrollTop;
     const itemHeightWithGap = itemHeight + itemGap;
-    let indexFromHeight = Math.floor(containerScrollTop / itemHeightWithGap);
+    const indexFromHeight = Math.floor(containerScrollTop / itemHeightWithGap);
 
-    if (indexChange) indexFromHeight = itemIndexInView;
-
-    setItemIndexInViewState(indexFromHeight);
+    setfirstItemIndexInViewState(indexFromHeight);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [itemHeight, itemGap]);
 
   // this effect is for the initial setup of the scroll event
@@ -54,39 +68,66 @@ export default function FancyVirtualScroll(props: TVirtualScrollProps) {
     if (!container) return;
 
     // Initial calculation and setup for scroll event
-    container.addEventListener('scroll', () => {
-      calculateVisibleItemIdx();
-    });
+    container.addEventListener('scroll', () => calculateVisibleItemIdx());
 
     return () => {
-      container.removeEventListener('scroll', calculateVisibleItemIdx);
+      if (scrollTimeoutRef.current) clearTimeout(scrollTimeoutRef.current);
+      container.removeEventListener('scroll', () => calculateVisibleItemIdx());
     };
   }, [calculateVisibleItemIdx, indexChange]);
 
-  // Adjusts scroll position when itemIndexInView prop changes
+  // Adjusts scroll position when firstItemIndexInView prop changes
   useEffect(() => {
     const container = containerRef.current;
     if (!container) return;
 
-    const newScrollTop = itemIndexInView * (itemHeight + itemGap); // Adjust so the item is fully in view
+    const newScrollTop = firstItemIndexInView * (itemHeight + itemGap); // Adjust so the item is fully in view
     container.scrollTop = newScrollTop;
 
     setIndexChange(true);
-    setItemIndexInViewState(itemIndexInView);
+    setfirstItemIndexInViewState(firstItemIndexInView);
+    setIsScrolling(false);
 
     // Force recalculation after adjusting scroll position
     calculateVisibleItemIdx();
-  }, [itemIndexInView, itemHeight, itemGap, calculateVisibleItemIdx]);
+  }, [firstItemIndexInView, itemHeight, itemGap, calculateVisibleItemIdx]);
+
+  const handleScroll = useCallback(() => {
+    console.log('scrolling');
+    if (!isScrolling) setIsScrolling(true); // Set isScrolling to true when scrolling starts
+    onScrollingStateChange?.(true); // Notify parent component that scrolling has started
+    // Clear the timeout if it's already set
+    if (scrollTimeoutRef.current) clearTimeout(scrollTimeoutRef.current);
+    // Set a timeout to update the isScrolling state after scrolling stops
+    scrollTimeoutRef.current = setTimeout(() => {
+      setIsScrolling(false);
+      onScrollingStateChange?.(false); // Notify parent component that scrolling has stopped
+    }, 200); // Adjust the timeout duration as needed
+  }, [isScrolling, onScrollingStateChange]);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container || indexChange) return;
+
+    container.addEventListener('scroll', handleScroll);
+
+    return () => {
+      if (scrollTimeoutRef.current) clearTimeout(scrollTimeoutRef.current);
+      container.removeEventListener('scroll', handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [handleScroll, firstItemIndexInView]);
 
   return (
-    <Wrapper ref={containerRef} $scrollSnap={scrollSnap} $containerHeight={containerHeight}>
+    <Wrapper ref={containerRef} $scrollSnap={scrollSnap} $containerHeight={containerHeight} {...attributesContainer}>
       <div style={{ position: 'relative', height: `${children.length * (itemHeight + itemGap) - itemGap}px` }}>
         {renderdItems.map(({ content, originalIndex }) => (
           <Item
-            style={{ top: `${originalIndex * (itemHeight + itemGap)}px` }}
+            style={{ top: `${originalIndex * (itemHeight + itemGap)}px`, marginBottom: '24px' }}
             key={originalIndex}
             $itemHeight={`${itemHeight}px`}
             $enableScrollSnap={Boolean(scrollSnap)}
+            {...attributesItems}
           >
             {content}
           </Item>
--- src/components/shared/FancyVirtualScroll/TVirtualScroll.model.ts
+++ src/components/shared/FancyVirtualScroll/TVirtualScroll.model.ts
@@ -1,10 +1,17 @@
+import { CSSProp } from 'styled-components';
+
 export type TVirtualScrollProps = {
   children: React.ReactNode[];
   containerHeight?: string;
   preRenderCount?: number;
   itemHeight?: number;
-  itemIndexInView?: number;
-  scrollSnap?: 'mandatory' | 'proximity';
+  firstItemIndexInView?: number;
+  scrollSnap?: 'mandatory' | 'proximity' | 'none';
   itemGap?: number; // New prop for specifying the gap between children
   currentItemsInViewHandler?: (index: number) => void;
+  attributesContainer?: HTMLDivProps;
+  attributesItems?: HTMLDivProps;
+  onScrollingStateChange?: (isScrolling: boolean) => void;
 };
+
+type HTMLDivProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'style'> & { $externalStyle: CSSProp };
