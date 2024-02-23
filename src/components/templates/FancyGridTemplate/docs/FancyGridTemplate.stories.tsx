0e4db0758beeaea03198da666886d06617b71ef3
MrTRyy <Tobiastry@aol.de>
2024-02-23 23:18:22 +0100

    Add FancyGridTemplate and FancyGridTemplateItem components

---
+++ src/components/templates/FancyGridTemplate/docs/FancyGridTemplate.stories.tsx
@@ -0,0 +1,87 @@
+// Import necessary dependencies
+import { Meta, StoryObj } from '@storybook/react';
+
+// Import the component to be tested
+import FancyGridTemplate from '../FancyGridTemplate';
+
+// Define metadata for the story
+const meta = {
+  component: FancyGridTemplate,
+  title: 'components/templates/FancyGridTemplate',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Layout-Component: The FancyPasswordInput Comonent with surrounding icon, label and underline',
+      },
+    },
+  },
+  // Define arguments for the story
+  argTypes: {
+    gridAreas: {
+      description:
+        'Array of strings defining the grid layout. Each string represents a row in the grid, and each space-separated value within the string defines a grid area.',
+      defaultValue: [],
+      control: {
+        type: 'object',
+      },
+    },
+    gridAreasBreakPoints: {
+      description: 'The breakpoints for the grid areas',
+      defaultValue: [],
+      control: {
+        type: 'object',
+      },
+    },
+    gapColumn: {
+      description: 'The gap sizeC between columns. Defaults to sm',
+      defaultValue: {
+        summary: 'sm',
+      },
+      control: {
+        type: 'number',
+      },
+    },
+    gapRow: {
+      description: 'The gap sizeC between rows. Defaults to sm',
+      defaultValue: {
+        summary: 'sm',
+      },
+      control: {
+        type: 'number',
+      },
+    },
+    width: {
+      description: 'The width of the grid container. Defaults to 100%',
+      defaultValue: {
+        summary: '100%',
+      },
+      control: {
+        type: 'text',
+      },
+    },
+    height: {
+      description: 'The height of the grid container.',
+      defaultValue: {
+        summary: '100%',
+      },
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} satisfies Meta<typeof FancyGridTemplate>;
+
+// Export the metadata
+export default meta;
+// Define the story object
+type Story = StoryObj<typeof meta>;
+
+// Define the primary story
+export const Primary: Story = {
+  render: (args) => <FancyGridTemplate {...args}></FancyGridTemplate>,
+  args: {
+    gridAreas: ['header', 'main', 'footer'],
+    width: '100%',
+    height: '100%',
+  },
+};
