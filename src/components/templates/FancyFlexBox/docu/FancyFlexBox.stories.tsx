4069ceb24dbb5699306251d54cf2e598f730c14f
MrTRyy <Tobiastry@aol.de>
2024-02-23 20:40:17 +0100

    Add FancyFlexBox component and its documentation

---
+++ src/components/templates/FancyFlexBox/docu/FancyFlexBox.stories.tsx
@@ -0,0 +1,102 @@
+// Import necessary dependencies
+import { Meta, StoryObj } from '@storybook/react';
+
+import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
+// Import the component to be tested
+
+// Define metadata for the story
+const meta = {
+  component: FancyFlexBox,
+  title: 'components/templates/FancyFlexBox',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The FancyFlexBox component is for displaying a fancy flex box, that acts as a container for flex items. ',
+      },
+    },
+  },
+
+  // Define arguments for the story
+  argTypes: {
+    as: {
+      description: 'The HTML element to be rendered.',
+      defaultValue: {
+        summary: 'div',
+      },
+      control: {
+        type: 'text',
+      },
+    },
+    direction: {
+      description: 'The direction of the flex box.',
+      control: {
+        type: 'select',
+        options: ['row', 'row-reverse', 'column', 'column-reverse', undefined],
+      },
+    },
+    justify: {
+      description: 'The justify content of the flex box.',
+      control: {
+        type: 'select',
+        options: ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly', undefined],
+      },
+    },
+    align: {
+      description: 'The align items of the flex box.',
+      control: {
+        type: 'select',
+        options: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline', undefined],
+      },
+    },
+    gap: {
+      description: 'The gap between the flex items.',
+      control: {
+        type: 'text',
+      },
+    },
+    fitBox: {
+      description: 'If the flex box should fill the entire container.',
+      control: {
+        type: 'boolean',
+      },
+    },
+    separator: {
+      description: 'A separator to be rendered between the flex items.',
+      control: {
+        type: 'object',
+      },
+    },
+  },
+} satisfies Meta<typeof FancyFlexBox>;
+
+// Export the metadata
+export default meta;
+// Define the story object
+type Story = StoryObj<typeof meta>;
+
+// Define the primary story
+export const Primary: Story = {
+  render: (args) => (
+    <FancyFlexBox {...args}>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </FancyFlexBox>
+  ),
+  args: {
+    as: 'ul',
+    externalStyle: {
+      backgroundColor: 'gray',
+      height: '300px',
+      width: '300px',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: '',
+      },
+    },
+  },
+};
