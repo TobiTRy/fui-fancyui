0f195842912d15064531e2fd7c08bc3a8efe91a5
MrTRyy <tobiastry@aol.de>
2024-02-23 15:41:14 +0100

    Update Storybook theme to use dark mode and change brand details

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,39 +3,14 @@ import { create } from '@storybook/theming';
 
 // Create a custom theme
 const myLightThemeStorybook = create({
-  base: 'light',
-  // Typography
+  base: 'dark',
 
-  brandTitle: 'My custom Storybook',
-  brandUrl: 'https://example.com',
+  brandTitle: 'FUI-FancyUI',
+  brandUrl: 'http://FUI.cool',
   brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
-  brandTarget: '_self',
+  brandTarget: '_blank',
 
-  //
-  colorPrimary: '#3A10E5',
-  colorSecondary: '#585C6D',
-
-  // UI
-  appBg: '#ffffff',
-  appContentBg: '#ffffff',
-  appPreviewBg: '#ffffff',
-  appBorderColor: '#585C6D',
-  appBorderRadius: 4,
-
-  // Text colors
-  textColor: '#10162F',
-  textInverseColor: '#ffffff',
-
-  // Toolbar default and active colors
-  barTextColor: '#9E9E9E',
-  barSelectedColor: '#585C6D',
-  barBg: '#ffffff',
-
-  // Form colors
-  inputBg: '#ffffff',
-  inputBorder: '#10162F',
-  inputTextColor: '#10162F',
-  inputBorderRadius: 2,
+  textColor: '#fff',
 });
 
 export default myLightThemeStorybook;
