1ff95b1551bba319a98c8e2cbdf59bc2ec12e4b6
MrTRyy <tobiastry@aol.de>
2024-02-26 18:52:51 +0100

    Add templateThemeType import and update argTypes in FancyLoadingSpinner.stories.tsx

--- src/components/atoms/FancyLoadingSpinner/FancyLoadingSpinner.tsx
+++ src/components/atoms/FancyLoadingSpinner/FancyLoadingSpinner.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-
-import { sizes } from './sizeSettings';
+import { TFancyLoadingSpinner } from '@/components/atoms/FancyLoadingSpinner/IFancyLoadingSpinner.model';
 import { SpinnerContainer, StyledFancyLoadingSpinner, StyledInnerSpinner } from './FancyLoadingSpinner.model';
-import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
-import { TLayer } from '@/types/TLayer';
 
 // --------------------------------------------------------------------------- //
 // ------------------ A Loadingspinner with different sizes ------------------ //
 // --------------------------------------------------------------------------- //
-interface IFancyLoadingSpinner {
-  sizeC?: keyof typeof sizes;
-  themeType?: TUiColorsNotTransparent;
-  layer?: TLayer;
-}
-export default function FancyLoadingSpinner({ sizeC = 'md', themeType, layer }: IFancyLoadingSpinner) {
+export default function FancyLoadingSpinner(props: TFancyLoadingSpinner) {
+  const { sizeC = 'md', themeType = 'accent', layer } = props;
+
   return (
     <SpinnerContainer $sizeC={sizeC}>
       <StyledFancyLoadingSpinner $sizeC={sizeC} $themeType={themeType} $layer={layer} />
-      <StyledInnerSpinner $sizeC={sizeC} />
+      <StyledInnerSpinner $themeType={themeType} $layer={layer} $sizeC={sizeC} />
     </SpinnerContainer>
   );
 }
