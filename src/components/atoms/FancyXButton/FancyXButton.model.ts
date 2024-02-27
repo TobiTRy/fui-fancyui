38c7ebaff94b1baadc73e50c0b01dd46d4146186
MrTRyy <tobiastry@aol.de>
2024-02-27 20:47:18 +0100

    Add FancyXButton component and its model and style files

---
+++ src/components/atoms/FancyXButton/FancyXButton.model.ts
@@ -0,0 +1,14 @@
+import { TLayer } from '@/types/TLayer';
+import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
+import React from 'react';
+import { CSSProp } from 'styled-components';
+
+export type TFancyXButton = {
+  themeType?: TUiColorsNotTransparent;
+  layer?: TLayer;
+  externalStyle?: CSSProp;
+};
+
+export type TFancyXButtonNativeAttrs = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'style'>;
+
+export type TFancyXButtonWithHTMLAttrs = TFancyXButton & TFancyXButtonNativeAttrs;
---
+++ src/components/atoms/FancyXButton/FancyXButton.style.tsx
@@ -0,0 +1,28 @@
+import { styled } from 'styled-components';
+
+import { TLayer } from '@/types/TLayer';
+import { TTheme } from '@/types/TTheme';
+import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
+import { simpleColorTransition } from '@/design/designFunctions/simpleColorTransition';
+import { getBackgroundColor } from '@/design/designFunctions/colorCalculatorForComponent';
+import Color from 'color';
+
+interface IStyledFancyXButton {
+  $themeType?: TUiColorsNotTransparent;
+  $layer?: TLayer;
+  theme: TTheme;
+}
+export const StyledFancyXButton = styled.button<IStyledFancyXButton>`
+  padding: 0;
+  background: none;
+  border: none;
+  font-weight: bolder;
+  cursor: pointer;
+  outline: none;
+  ${simpleColorTransition}
+
+  &:hover {
+    color: ${({ $themeType = 'accent', $layer, theme }) =>
+      Color(getBackgroundColor({ $themeType, $layer, theme })).darken(0.5).hex()};
+  }
+`;
