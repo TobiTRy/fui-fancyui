190c442606ede41a48a723c2f9d9581853d6b166
MrTRyy <Tobiastry@aol.de>
2024-03-02 17:13:35 +0100

    Update import in CardSpacing and TFancyAlignBox

--- src/components/molecules/Card/Components/CardSpacing.tsx
+++ src/components/molecules/Card/Components/CardSpacing.tsx
@@ -1,9 +1,9 @@
 import { ElementType } from 'react';
 
-import { TSpacingsContainerFullProps } from '@/components/atoms/SpacingContainer';
+import { TSpacingsContainerWithHTMLAttrs } from '@/components/atoms/SpacingContainer';
 import { FancyAlignBox } from '@/components/templates/FancyAlignBox';
 
-export default function CardSpacing<T extends ElementType>(props: TSpacingsContainerFullProps<T>) {
+export default function CardSpacing<T extends ElementType>(props: TSpacingsContainerWithHTMLAttrs<T>) {
   const { children, padding, className, ...htmlProps } = props;
 
   return (
--- src/components/templates/FancyAlignBox/TFancyAlignBox.model.ts
+++ src/components/templates/FancyAlignBox/TFancyAlignBox.model.ts
@@ -1,6 +1,6 @@
 import { ElementType } from 'react';
 
 import { TFancyFlexBox } from '@/components/templates/FancyFlexBox/FancyFlexBox';
-import { TSpacingsContainerFullProps } from '@/components/atoms/SpacingContainer';
+import { TSpacingsContainerWithHTMLAttrs } from '@/components/atoms/SpacingContainer';
 
-export type TFancyAlignBox<T extends ElementType> = TFancyFlexBox<T> & TSpacingsContainerFullProps<T>;
+export type TFancyAlignBox<T extends ElementType> = TFancyFlexBox<T> & TSpacingsContainerWithHTMLAttrs<T>;
