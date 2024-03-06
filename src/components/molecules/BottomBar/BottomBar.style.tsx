c0302b65ab104c328e16033a037accba6e70bfc1
MrTRyy <tobiastry@aol.de>
2024-03-06 17:23:45 +0100

    Remove BottomBar.style.tsx and BottomBar/index.ts

--- src/components/molecules/BottomBar/BottomBar.style.tsx
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components';
-
-import { TTheme } from '@/types/TTheme';
-
-export const fancyBarStyle = css<{ theme?: TTheme }>`
-  display: flex;
-  padding: ${({ theme }) => theme.spacing.sm + ' ' + theme.spacing.sm + ' 0 ' + theme.spacing.sm};
-  width: 100%;
-`;
