14f31bebf2f0a83e0a4364ae8adf27e2627c1a3a
MrTRyy <Tobiastry@aol.de>
2024-03-11 20:58:14 +0100

    Add DynamicScrollBar component and its stories

--- src/components/molecules/DynamicBottomScrollBar/docu/DynamicScrollBar.stories.tsx
+++ src/components/molecules/DynamicBottomScrollBar/docu/DynamicScrollBar.stories.tsx
@@ -1,17 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import DynamicBottomScrollBar from './DynamicBottomScrollBar';
+import DynamicBottomScrollBar from '../DynamicScrollBar';
 import { SVGCheckMark } from '@/components/icons/SVGCheckMark';
 import { FancyBottomBarIcon } from '@/components/templates/FancyBottomBarIcon';
 
 const meta = {
-  title: 'components/molecules/DynamicBottomScrollBar',
+  title: 'components/molecules/DynamicScrollBar',
   component: DynamicBottomScrollBar,
   parameters: {
     docs: {
       description: {
         component:
-          'Dumb-Comonent: This component renders only a wrapper that handles a scrollable bottom bar without style',
+          'The `DynamicScrollBar` component is designed to dynamically enable or disable a custom scrollbar based on specific props. It wraps its children within the `ScrollableBar` component if scrolling is enabled and the activation condition for the scrollbar is met. This component is particularly useful for creating flexible UI layouts where the overflow of content needs to be managed conditionally, providing a cleaner user experience especially in web applications where varying amounts of content need to be displayed.',
       },
     },
   },
@@ -22,11 +22,16 @@ const meta = {
         type: 'boolean',
       },
     },
+    activateScrollbar: {
+      description: 'If true, the scrollbar will be activated.',
+      control: {
+        type: 'boolean',
+      },
+    },
     children: {
       description: 'The children of the DynamicBottomScrollBar.',
     },
   },
-  tags: ['autodocs'],
 } satisfies Meta<typeof DynamicBottomScrollBar>;
 
 export default meta;
@@ -36,6 +41,7 @@ export const Primary: Story = {
   render: (args) => <DynamicBottomScrollBar {...args} />,
   args: {
     scrollable: true,
+    activateScrollbar: true,
     children: (
       <>
         <FancyBottomBarIcon icon={<SVGCheckMark />} label="Hiii" />
