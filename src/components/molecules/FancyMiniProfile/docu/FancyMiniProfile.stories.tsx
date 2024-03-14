f8c53575a6a3885c416d24cf78073e7904e43f73
MrTRyy <Tobiastry@aol.de>
2024-03-14 18:45:12 +0100

    Refactor FancyMiniProfile component and add related files

---
+++ src/components/molecules/FancyMiniProfile/docu/FancyMiniProfile.stories.tsx
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import FancyMiniProfile from '../FancyMiniProfile';
+import templateThemeType from '@/stories/templateSettingsForStorys/templatesForThemeType';
+
+const meta = {
+  component: FancyMiniProfile,
+  title: 'components/molecules/FancyMiniProfile',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The FancyMiniProfile component is designed to render a profile image accompanied by a title and a subtitle. This component utilizes a Chip molecule and a FancyProfilePicture atom to create a visually appealing mini profile card. The card´s layout, including the image alignment and spacing, is customizable through props, making it adaptable to various design requirements. The component also supports different sizes and themes for flexibility in presentation.',
+      },
+    },
+  },
+  argTypes: {
+    ...templateThemeType('notTransparent', 'primary', 0),
+    sizeC: {
+      description: 'The sizeC of the component',
+      control: {
+        type: 'select',
+      },
+      options: ['sm', 'md', 'lg'],
+    },
+    title: {
+      description: 'The title text displayed in the profile card.',
+      control: {
+        type: 'text',
+      },
+    },
+    subTitle: {
+      description: 'The subtitle text displayed below the title.',
+      control: {
+        type: 'text',
+      },
+    },
+    alignImage: {
+      description: 'The alignment of the image',
+      control: {
+        type: 'select',
+      },
+      options: ['left', 'right'],
+    },
+    src: {
+      description: 'The source URL for the profile image.',
+      control: {
+        type: 'text',
+      },
+    },
+    shadow: {
+      description: 'If true, applies a shadow effect to the component.',
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} satisfies Meta<typeof FancyMiniProfile>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: (args) => <FancyMiniProfile {...args} />,
+  args: {
+    alignImage: 'right',
+    title: 'Hans',
+    subTitle: 'Hans',
+    src: 'https://www.az-online.de/bilder/2019/08/23/12938342/2113799823-tobias-rester-2tyMMSkM2R73.jpg',
+    sizeC: 'sm',
+    themeType: 'primary',
+    layer: 3,
+    shadow: true,
+  },
+};
--- src/components/molecules/FancyMiniProfile/index.ts
+++ src/components/molecules/FancyMiniProfile/index.ts
@@ -1,1 +1,3 @@
 export { default as FancyMiniProfile } from './FancyMiniProfile';
+
+export type { TFancyMiniprofile, TFancyMiniprofileWithHTMLProps } from './TFancyMiniProfile.model';
