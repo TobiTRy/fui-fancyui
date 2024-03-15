a395b92056be729db800f8e437ba9e148c8d0743
MrTRyy <tobiastry@aol.de>
2024-03-15 17:15:15 +0100

    Refactor FancyProgressBar component and update import path in FancyProgressBar.tsx

--- src/components/molecules/FancyProgressBar/FancyProgressBar.tsx
+++ src/components/molecules/FancyProgressBar/FancyProgressBar.tsx
@@ -1,41 +1,43 @@
-import { TLayer } from '@/types/TLayer';
+import { ProgressBar } from '@/components/atoms/ProgressBar';
 
-import { ProgressBar, TProgressBar } from '@/components/atoms/ProgressBar';
-
-import { Wrapper } from './FancyProgressBar.style';
-import { TTextAlignLC } from '@/types/TTextAlignLC';
-import { TTextAlignLRC } from '@/types/TTextAlignLRC';
 import { AlignedLabel } from '@/components/atoms/AlignedLabel';
 import { Typography } from '@/components/atoms/Typography';
-import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
 
-// Define the props for the ProgressBar component
-interface IFancyProgressBar extends TProgressBar {
-  label?: string;
-  labelAlign?: TTextAlignLC;
-  progressCount?: boolean;
-  progressAlign?: TTextAlignLRC;
-  themeType?: TUiColorsNotTransparent;
-  textLayer?: TLayer;
-  layer?: TLayer;
-}
+import { Wrapper } from './FancyProgressBar.style';
+import { TFancyProgressBarWithHTMLAttrs } from './TFancyProgressBar.model';
+import { useId } from 'react';
+
 // --------------------------------------------------------------------------- //
 // ---------- The Progressbar with some Items Like Percent and Label --------- //
 // --------------------------------------------------------------------------- //
-export default function FancyProgressBar(props: IFancyProgressBar) {
-  const { progress, maxValue, id, label, labelAlign, progressAlign, progressCount, themeType, layer, textLayer } =
-    props;
+export default function FancyProgressBar(props: TFancyProgressBarWithHTMLAttrs) {
+  const {
+    progress,
+    maxValue,
+    id,
+    label,
+    labelAlign,
+    progressAlign,
+    progressCount,
+    themeType,
+    layer,
+    textLayer,
+    ...htmlProps
+  } = props;
+
+  const reactID = useId();
+  const usedId = id ? id : reactID;
 
   return (
-    <Wrapper $value={progress}>
+    <Wrapper $value={progress} {...htmlProps}>
       {/* A lable to describe the Progress for */}
       {label && (
-        <AlignedLabel htmlFor={id} align={labelAlign} layer={textLayer}>
+        <AlignedLabel htmlFor={usedId} align={labelAlign} layer={textLayer}>
           {label}
         </AlignedLabel>
       )}
       {/* The Progressbar Atom itself */}
-      <ProgressBar id={id} progress={progress} maxValue={maxValue || 100} themeType={themeType} layer={layer} />
+      <ProgressBar id={usedId} progress={progress} maxValue={maxValue || 100} themeType={themeType} layer={layer} />
       {/* The Progress in percent as Text */}
       {progressCount && (
         <AlignedLabel as={'span'} align={progressAlign} layer={textLayer}>
--- src/components/molecules/FancyProgressBar/index.ts
+++ src/components/molecules/FancyProgressBar/index.ts
@@ -1,1 +1,3 @@
 export { default as FancyProgressBar } from './FancyProgressBar';
+
+export type { TFancyProgressBar, TFancyProgressBarWithHTMLAttrs } from './TFancyProgressBar.model';
