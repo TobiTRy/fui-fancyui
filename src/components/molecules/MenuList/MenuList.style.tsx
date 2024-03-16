dacdc97b6ffc69b5b6910128e7c88ea9728e4434
MrTRyy <Tobiastry@aol.de>
2024-03-16 23:22:12 +0100

    Add TMenueList.model and update MenuList component

--- src/components/molecules/MenuList/MenuList.style.tsx
+++ src/components/molecules/MenuList/MenuList.style.tsx
@@ -2,23 +2,23 @@ import { styled } from 'styled-components';
 
 import { generateThemeForCard } from '@/design/designFunctions/generateThemeForCard';
 
-import { MenuListProps } from './MenuList';
+import { TMenuList } from './TMenueList.model';
 import { TStyledPrefixAndOmiter } from '@/types/TStyledPrefixAndOmiter';
 import { TTheme } from '@/types/TTheme';
 
-type StyledMenuProps = TStyledPrefixAndOmiter<MenuListProps> & { theme: TTheme };
+type StyledMenuProps = TStyledPrefixAndOmiter<TMenuList> & { theme: TTheme };
 
 export const MenuContainer = styled.div<StyledMenuProps>`
   display: flex;
   flex-direction: column;
   padding: 8px 0;
-  ${({ theme, $themeType, $layer, $outlined }) =>
+  ${({ theme, $themeType, $layer, $outlined, $outlinedBackgroundStrength }) =>
     generateThemeForCard({
       theme,
       $themeType: $themeType ?? 'primary',
       $layer: $layer ?? 2,
       $outlined,
-      $outlinedBackgroundStrength: 1,
+      $outlinedBackgroundStrength: $outlinedBackgroundStrength ?? 1,
     })}
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
   border-radius: ${({ theme }) => theme.borderRadius.md};
