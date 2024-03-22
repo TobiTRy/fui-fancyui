5ad2c6b3c9ce043f1e755aa56efcd1b0be773de8
MrTRyy <tobiastry@aol.de>
2024-03-22 15:47:10 +0100

    Update RawCheckbox component styling and props

--- src/components/atoms/RawCheckbox/TRawCheckbox.model.ts
+++ src/components/atoms/RawCheckbox/TRawCheckbox.model.ts
@@ -1,7 +1,12 @@
 import { HTMLAttributes } from 'react';
+import { TUiColorsMain } from '@/types/TUiColorsMain';
+import { TLayer } from '@/types/TLayer';
 
 // the props that will be passed to the input element
 type NativeAttrs = Omit<HTMLAttributes<HTMLInputElement>, 'type'>;
 
 // MAIN INCOMMING PROPS PASSED TO THE COMPONENT
-export type TRawCheckbox = NativeAttrs;
+export type TRawCheckbox = {
+  themeType?: TUiColorsMain;
+  layer?: TLayer;
+} & NativeAttrs;
