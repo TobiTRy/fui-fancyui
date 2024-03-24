0fd1c702636076164e4698511190bd631da2806b
MrTRyy <Tobiastry@aol.de>
2024-03-24 20:17:53 +0100

    Update SearchBar component title in documentation and stories

--- src/components/molecules/SearchBar/docu/SearchBar.stories.tsx
+++ src/components/molecules/SearchBar/docu/SearchBar.stories.tsx
@@ -5,6 +5,7 @@ import templateThemeType from '@/stories/templateSettingsForStorys/templatesForT
 
 const meta = {
   component: SearchBar,
+  title: 'components/molecules/SearchBar',
   parameters: {
     docs: {
       description: {
