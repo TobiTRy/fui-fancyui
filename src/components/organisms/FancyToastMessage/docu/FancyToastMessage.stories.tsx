d3affd3a334bba43a85a352b4f590cb9ea011443
MrTRyy <Tobiastry@aol.de>
2024-03-26 21:54:24 +0100

    Refactor component imports and file structure in FancyToastMessage

--- src/components/organisms/FancyToastMessage/docu/FancyToastMessage.stories.tsx
+++ src/components/organisms/FancyToastMessage/docu/FancyToastMessage.stories.tsx
@@ -2,9 +2,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 // Import the component to be tested
-import FancyToastMessage from './FancyToastMessage';
-import { useFancyToastMessageStore } from './FancyToastMessage.state';
-import { FancyButton } from '../FancyButton';
+import FancyToastMessage from '../FancyToastMessage';
+import { useFancyToastMessageStore } from '../FancyToastMessage.state';
+import { FancyButton } from '../../FancyButton';
 
 // Define metadata for the story
 const meta = {
