71a159a42f43e8bfedc3d586f0449efca67d7277
MrTRyy <Tobiastry@aol.de>
2024-03-27 21:29:14 +0100

    Update fontVariants logging in Typography component

--- src/components/atoms/Typography/Typography.tsx
+++ src/components/atoms/Typography/Typography.tsx
@@ -31,6 +31,8 @@ export default function Typography(props: TTypography) {
   // get the variant style based on the variant prop or the type prop;
   const fontVariants = generateFontVariants({ themeFonts, lineHeight, fontWeight, letterSpacing });
 
+  console.log('fontVariants', fontVariants, ComponentElement);
+
   return (
     <ComponentElement
       $variant={fontVariants[variant]}
