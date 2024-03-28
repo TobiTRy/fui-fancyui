cabb6802c9fa93f83e6a3be6fb86fa1a8aa43a9b
MrTRyy <tobiastry@aol.de>
2024-03-28 17:55:45 +0100

    Update designFunctions files and documentation

--- src/design/designFunctions/arrayToCssValues/TArrayValues.model.ts
+++ src/design/designFunctions/arrayToCssValues/TArrayValues.model.ts
@@ -1,24 +1,14 @@
-import { TSpacings } from '@/types/TSpacings';
-import { TBorderRadiusSizes } from '@/types/TBorderRadiusSizes';
-
-// Define a type that maps your inputs to the desired output types
-export type TValueMap = {
-  borderRadius: TBorderRadiusSizes;
-  spacing: TSpacings;
-  default: string | number;
-};
-
-// TThemeCSSValue will now be a generic type that can extend 'border' or 'spacing',
-// and it will return the corresponding type from TypeMapping
-export type TThemeCSSValue<T extends keyof TValueMap = 'default'> = TValueMap[T] | string | number;
+import { TThemeValueOrCSS, TThemeValueMap } from '@/design/designFunctions/getThemeOrValueAsCss';
 
 // the rounded edges are an array of 4 values
-export type TThemeArrayCssValues<T extends keyof TValueMap = 'default'> = [
-  TThemeCSSValue<T>?,
-  TThemeCSSValue<T>?,
-  TThemeCSSValue<T>?,
-  TThemeCSSValue<T>?,
+export type TThemeArrayCssValues<T extends keyof TThemeValueMap = 'default'> = [
+  TThemeValueOrCSS<T>?,
+  TThemeValueOrCSS<T>?,
+  TThemeValueOrCSS<T>?,
+  TThemeValueOrCSS<T>?,
 ];
 
 // the input can be an array or a single value
-export type TThemeArrayOrValueCSS<T extends keyof TValueMap = 'default'> = TThemeArrayCssValues<T> | TThemeCSSValue<T>;
+export type TThemeArrayOrValueCSS<T extends keyof TThemeValueMap = 'default'> =
+  | TThemeArrayCssValues<T>
+  | TThemeValueOrCSS<T>;
--- src/design/designFunctions/arrayToCssValues/arrayToCssValues.ts
+++ src/design/designFunctions/arrayToCssValues/arrayToCssValues.ts
@@ -1,42 +1,46 @@
-import getThemeOrValueForCSS from '@/design/designFunctions/getThemeOrValueAsCss/getThemeOrValueAsCss';
-import { TThemeArrayOrValueCSS, TValueMap } from './TArrayValues.model';
+import { getThemeOrValueAsCSS } from '@/design/designFunctions/getThemeOrValueAsCss';
+import { TThemeArrayOrValueCSS } from './TArrayValues.model';
+import { TThemeValueMap } from '@/design/designFunctions/getThemeOrValueAsCss';
 
 // --------------------------------------------------------------------------- //
 // ---------- this function is for the calc wich edege has a radius ---------- //
 // --------------------------------------------------------------------------- //
-const arrayToCssValues = <T extends keyof TValueMap = 'default'>(values?: TThemeArrayOrValueCSS, themeSetting?: T) => {
+const arrayToCssValues = <T extends keyof TThemeValueMap = 'default'>(
+  values?: TThemeArrayOrValueCSS,
+  themeSetting?: T
+) => {
   if (!values || !values.toString().length) return '';
 
   // Initialize an empty array for the cleaned values
-  let cleanedValues: (string | number | TValueMap[T])[] = [];
+  let cleanedValues: (string | number | TThemeValueMap[T])[] = [];
 
   // Handle the case where values is a single value
   if (typeof values === 'string' || typeof values === 'number') {
     cleanedValues.push(values);
   } else if (Array.isArray(values)) {
     // Filter out undefined values and assert the type to exclude 'undefined'
-    cleanedValues = values.filter((value): value is string | number | TValueMap[T] => value !== undefined);
+    cleanedValues = values.filter((value): value is string | number | TThemeValueMap[T] => value !== undefined);
   }
 
   switch (cleanedValues.length) {
     // if one value is given, all edges are the same
     case 1: {
-      const singleValue = getThemeOrValueForCSS(cleanedValues[0], themeSetting);
+      const singleValue = getThemeOrValueAsCSS(cleanedValues[0], themeSetting);
       return ` ${singleValue} `;
     }
     // if two values are given, top/bottom and left/right are the same
     case 2: {
-      const [topBottomValue, leftRightValue] = cleanedValues.map((item) => getThemeOrValueForCSS(item, themeSetting));
+      const [topBottomValue, leftRightValue] = cleanedValues.map((item) => getThemeOrValueAsCSS(item, themeSetting));
       return ` ${topBottomValue} ${leftRightValue} `;
     }
     // if three values are given, top, left/right and bottom are the same
     case 3: {
-      const [top, leftRight, bottom] = cleanedValues.map((item) => getThemeOrValueForCSS(item, themeSetting));
+      const [top, leftRight, bottom] = cleanedValues.map((item) => getThemeOrValueAsCSS(item, themeSetting));
       return ` ${top} ${leftRight} ${bottom} ${leftRight} `;
     }
     // if four values are given, all edges are different
     case 4: {
-      const [top, right, bottom, left] = cleanedValues.map((item) => getThemeOrValueForCSS(item, themeSetting));
+      const [top, right, bottom, left] = cleanedValues.map((item) => getThemeOrValueAsCSS(item, themeSetting));
       return ` ${top} ${right} ${bottom} ${left} `;
     }
   }
