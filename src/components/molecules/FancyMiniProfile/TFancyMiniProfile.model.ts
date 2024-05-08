e60d8f535cd88874cc91e78bbb6accd42ce9c41d
MrTRyy <Tobiastry@aol.de>
2024-05-08 21:37:27 +0200

    refactor: Update FancyMiniProfile component to use Image prop instead of src

--- src/components/molecules/FancyMiniProfile/TFancyMiniProfile.model.ts
+++ src/components/molecules/FancyMiniProfile/TFancyMiniProfile.model.ts
@@ -1,11 +1,12 @@
 import { TComponentSizes } from '@/types/TComponentSizes';
 import { TLayer } from '@/types/TLayer';
 import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
+import React from 'react';
 
 export type TFancyMiniprofile = {
   title?: string;
   subTitle?: string;
-  src?: string;
+  Image: React.ReactElement;
   sizeC?: TComponentSizes;
   themeType?: TUiColorsNotTransparent;
   layer?: TLayer;
