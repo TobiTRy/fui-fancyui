38b12e03d13fd8617de0602108c068ea53a725c8
MrTRyy <Tobiastry@aol.de>
2024-05-19 22:47:19 +0200

    refactor: Remove unused clampNumbers utility function

--- src/utils/functions/clampNumbers/clampNumber.ts
+++
@@ -1,4 +0,0 @@
-// Clamp a number between a minimum and maximum value
-export const clampNumbers = (value: number, min: number, max: number): number => {
-  return Math.min(Math.max(value, min), max);
-};
--- src/utils/functions/clampNumbers/index.ts
+++
@@ -1,1 +0,0 @@
-export { clampNumbers } from './clampNumber';
