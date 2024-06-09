1dc1c85add0869410f5c7f90d82b7f07957c756d
MrTRyy <Tobiastry@aol.de>
2024-06-09 21:22:57 +0200

    refactor: Add useFancyToastMessageStore hook for managing toast message state

--- src/components/organisms/FancyToastMessage/index.ts
+++ src/components/organisms/FancyToastMessage/index.ts
@@ -1,3 +1,4 @@
 export { default as FancyToastMessage } from './FancyToastMessage';
+export { useFancyToastMessageStore } from './FancyToastMessage.state';
 
 export type { TFancyToastMessageStore, TFancyToastMessage } from './TFancyToastMessage.model';
