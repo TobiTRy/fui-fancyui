aa149f66a0c29d83574ff02e91b60ca7320a7224
MrTRyy <tobiastry@aol.de>
2024-06-10 17:12:49 +0200

    refactor: Update FancySkeletonGrid component to use useActiveBreakpoint hook

--- src/components/templates/FancySkeletonGrid/FancySkeletonGrid.tsx
+++ src/components/templates/FancySkeletonGrid/FancySkeletonGrid.tsx
@@ -1,16 +1,24 @@
-import { useWichBreakPointIsActive } from '@/utils/hooks/useWichBreakPointIsActive/useWichBreakPointIsActive';
+import { useActiveBreakpoint } from '@/utils/hooks/useActiveBreakpoint';
 
 import { SkeletonBox } from '@/components/atoms/SkeletonBox';
 import { FancyGridTemplate } from '@/components/templates/FancyGridTemplate';
 import { TFancySkeletonGrid } from '@/components/templates/FancySkeletonGrid/TFancySkeletonGrid.model';
 import { generateGridSet } from '@/components/templates/FancySkeletonGrid/utils/generateGidSet';
+import { themeStore } from '@/design/theme/themeStore';
 
 // --------------------------------------------------------------------------- //
 // -------- The Component generates Skeletonboxes based on the GridArea ------ //
 // --------------------------------------------------------------------------- //
 export default function FancySkeletonGrid(props: TFancySkeletonGrid) {
   const { gridAreas, gridAreasBreakPoints, layer, themeType, borderRadius, ...gridProps } = props;
-  const activeBreakPoint = useWichBreakPointIsActive();
+  const themeBreakPoints = themeStore((state) => state.theme.breakpoints);
+
+  const breakPointsArray = Object.keys(themeBreakPoints).map((key) => ({
+    id: key,
+    query: themeBreakPoints[key as keyof typeof themeBreakPoints],
+  }));
+
+  const activeBreakPoint = useActiveBreakpoint(breakPointsArray);
 
   // Generate a set of grid areas based on the gridAreas and gridAreasBreakPoints
   const set = generateGridSet({ gridAreas, gridAreasBreakPoints, activeBreakPoint });
