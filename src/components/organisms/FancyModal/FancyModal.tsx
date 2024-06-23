57c3b05d1e93db063f3bd98a7b04557e791f059a
MrTRyy <Tobiastry@aol.de>
2024-06-23 13:28:51 +0200

    refactor: Remove commented out code in FancyModal component

--- src/components/organisms/FancyModal/FancyModal.tsx
+++ src/components/organisms/FancyModal/FancyModal.tsx
@@ -7,18 +7,6 @@ import { TFancyModal } from './TFancyModal.modal';
 // ----------------- The modalModule to build up a Moadal  ------------------- //
 // --------------------------------------------------------------------------- //
 export default function FancyModal({ appendToDomID, modals, closeModal }: TFancyModal) {
-  // const closeModalHandler = (id: string) => {
-  //   // closing the modal is a two step process
-  //   // first we set the status to "closing" with the closeModal function
-  //   // this will trigger the animation in the modal component
-  //   closeModal(id);
-
-  //   //wait for the animation and remove the modal from the store
-  //   setTimeout(() => {
-  //     removeModal(id);
-  //   }, 300);
-  // };
-
   return (
     <>
       {/* ----- The FancModal Ports the Modal out of the root div in the spearte "modal" div ----- */}
