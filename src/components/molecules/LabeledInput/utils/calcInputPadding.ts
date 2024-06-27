5b4a35471294746296589f45e0d7e52a5ee9ef9b
MrTRyy <Tobiastry@aol.de>
2024-06-27 21:39:19 +0200

    refactor: Update LabeledInput padding and margin styles

--- src/components/molecules/LabeledInput/utils/calcInputPadding.ts
+++ src/components/molecules/LabeledInput/utils/calcInputPadding.ts
@@ -11,21 +11,25 @@ export function calcInputPadding(props: TCalcInputPadding) {
   if ($isLabelProvided) {
     if ($isActive) {
       return css`
-        padding: 16px 0 4px 0px; //if the input is active and the type is transparent
+        margin-top: 16px;
+        padding: 0 0 4px 0px; //if the input is active and the type is transparent
       `;
     } else {
       return css`
-        padding: 12px 0 8px 0px; //if the input is not active and the type is transparent
+        margin-top: 12px;
+        padding: 0 0 8px 0px; //if the input is not active and the type is transparent
       `;
     }
   } else {
     if ($isActive) {
       return css`
-        padding: 2px 0 2px 0px; //if the input is active and the type is transparent
+        margin-top: 2px;
+        padding: 0 0 2px 0px; //if the input is active and the type is transparent
       `;
     } else {
       return css`
-        padding: 2px 0 2px 0px; //if the input is not active and the type is transparent
+        margin-top: 2px;
+        padding: 0 0 2px 0px; //if the input is not active and the type is transparent
       `;
     }
   }
