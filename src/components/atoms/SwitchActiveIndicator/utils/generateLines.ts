f66a0fa091dc913078dbb4e501a0e4e67b3336b1
MrTRyy <Tobiastry@aol.de>
2024-06-30 14:08:58 +0200

    refactor: Update generateLines.ts to improve readability and add vertical line generation

--- src/components/atoms/SwitchActiveIndicator/utils/generateLines.ts
+++ src/components/atoms/SwitchActiveIndicator/utils/generateLines.ts
@@ -6,21 +6,54 @@ import { TTheme } from '@/types/TTheme';
 import { TStyledPrefixAndPicker } from '@/types/TStyledPrefixAndPicker';
 import { TActiveSwitchIndicator } from '@/components/atoms/SwitchActiveIndicator/TSwitchActiveindicator.model';
 
-const generateLine = ({ theme, $themeType = 'accent', $layer }: IGenerateLine) => {
+const generateHorzLine = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
   const backgroundColor = getBackgroundColor({ theme, $themeType, $layer });
 
   return css`
-    height: 1.5px;
+    width: ${$indicatorWidth ?? '100%'};
+    height: ${$indicatorHeight ?? '1.5px'};
+    background-color: ${backgroundColor};
+    border-radius: 10px;
+  `;
+};
+
+const generateVertLine = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
+  const backgroundColor = getBackgroundColor({ theme, $themeType, $layer });
+
+  return css`
+    height: ${$indicatorHeight ?? '100%'};
+    width: ${$indicatorWidth ?? '1.5px'};
     background-color: ${backgroundColor};
     border-radius: 10px;
   `;
 };
 
 // Define the function to generate an underline for the active indicator
-type IGenerateLine = TStyledPrefixAndPicker<TActiveSwitchIndicator, 'themeType' | 'layer'> & { theme: TTheme };
-export const generateUnderline = ({ theme, $themeType = 'accent', $layer }: IGenerateLine) => {
+type IGenerateLine = TStyledPrefixAndPicker<
+  TActiveSwitchIndicator,
+  'themeType' | 'layer' | 'indicatorHeight' | 'indicatorWidth'
+> & { theme: TTheme };
+export const generateUnderline = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
   // Get the background color for the active indicator
-  const line = generateLine({ theme, $themeType, $layer });
+  const line = generateHorzLine({ theme, $themeType, $layer, $indicatorHeight, $indicatorWidth });
   // Return the styled-component CSS for the active indicator
   return css`
     ${line}
@@ -29,13 +62,53 @@ export const generateUnderline = ({ theme, $themeType = 'accent', $layer }: IGen
   `;
 };
 
-export const generateTopline = ({ theme, $themeType = 'accent', $layer }: IGenerateLine) => {
-  const line = generateLine({ theme, $themeType, $layer });
+export const generateTopline = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
+  const line = generateHorzLine({ theme, $themeType, $layer, $indicatorHeight, $indicatorWidth });
 
   // Return the styled-component CSS for the active indicator
   return css`
     ${line}
     top: 0;
+    box-sizing: border-box;
+  `;
+};
+
+export const generateLeftLine = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
+  const line = generateVertLine({ theme, $themeType, $layer, $indicatorHeight, $indicatorWidth });
+
+  // Return the styled-component CSS for the active indicator
+  return css`
+    ${line}
+    left: 0;
+    box-sizing: border-box;
+  `;
+};
+
+export const generateRightLine = ({
+  theme,
+  $themeType = 'accent',
+  $layer,
+  $indicatorHeight,
+  $indicatorWidth,
+}: IGenerateLine) => {
+  const line = generateVertLine({ theme, $themeType, $layer, $indicatorHeight, $indicatorWidth });
+
+  // Return the styled-component CSS for the active indicator
+  return css`
+    ${line}
+    right: 0;
     box-sizing: border-box;
   `;
 };
