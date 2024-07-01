0ee4ec115faca54dbf6f898ba66429054aedcb21
MrTRyy <Tobiastry@aol.de>
2024-07-01 21:17:16 +0200

    refactor: Update SideBarItem styling and structure

--- src/components/molecules/SideBarItem/SideBarItem.style.tsx
+++ src/components/molecules/SideBarItem/SideBarItem.style.tsx
@@ -9,13 +9,8 @@ export type TStyledSideBarItem = {
 
 export const StyledSideBarItem = styled(FancyBox)`
   position: relative;
-  padding: var(--spacing-xxs);
   width: 100%;
   box-sizing: border-box;
   transition: background-color 0.2s ease-in-out;
-  border-radius: var(--borderRadius-xs);
-
-  &:hover {
-    background-color: var(--color-primary-3);
-  }
+  cursor: pointer;
 `;
--- src/components/molecules/SideBarItem/SideBarItem.tsx
+++ src/components/molecules/SideBarItem/SideBarItem.tsx
@@ -1,20 +1,29 @@
-import { FancyBox } from '@/components/atoms/FancyBox';
+import { TFancyBoxWithHTMLAttrs } from '@/components/atoms/FancyBox/FancyBox.model';
 import { FancySVGAtom } from '@/components/atoms/FancySVGAtom';
 import { Typography } from '@/components/atoms/Typography';
+import { StyledSideBarItem } from './SideBarItem.style';
 import { FancyFlexBox } from '@/components/templates/FancyFlexBox';
-import React from 'react';
 
-export default function SideBarItem() {
+import { ReactNode } from 'react';
+
+type TSideBarItem = {
+  icon: ReactNode;
+  label: string;
+} & TFancyBoxWithHTMLAttrs;
+
+export default function SideBarItem(props: TSideBarItem) {
+  const { icon, label, ...FancyBoxProps } = props;
+
   return (
-    <FancyBox layer={3} hoverLayer={6} className={'style.wrapper'}>
+    <StyledSideBarItem padding={'xxs'} borderRadius={'xs'} {...FancyBoxProps}>
       <FancyFlexBox align="center" gap={'sm'}>
         <FancySVGAtom sizeC="sm" externalStyle={{ flexShrink: 0 }}>
-          {'icon'}
+          {icon}
         </FancySVGAtom>
         <Typography variant="bodytextMd" externalStyle={{ overflow: 'hidden', flex: '1', whiteSpace: 'nowrap' }}>
-          {'label'}
+          {label}
         </Typography>
       </FancyFlexBox>
-    </FancyBox>
+    </StyledSideBarItem>
   );
 }
