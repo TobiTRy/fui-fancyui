094c73a26d48e1fc28de12363be4d4a1cbbbbbb7
MrTRyy <tobiastry@aol.de>
2024-07-11 18:18:23 +0200

    refactor: Update FancySwipeUpModal to use createFancySwipeUpModalStore for state management

--- src/components/organisms/FancySwipeUpModal/index.ts
+++ src/components/organisms/FancySwipeUpModal/index.ts
@@ -1,3 +1,5 @@
 export { default as FancySwipeUpModal } from './FancySwipeUpModal';
 
 export type { TFancySwipeUpModal, TFancySwipeUpModalStore, TModalSettings } from './TFancySwipeUpModal.model';
+
+export { createFancySwipeUpModalStore } from './createFancySwipeUpModal.state';
