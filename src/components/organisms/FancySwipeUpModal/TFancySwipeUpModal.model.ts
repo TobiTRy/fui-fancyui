67d638af82cc0cf81053c41c468fa16a9bfd225c
MrTRyy <tobiastry@aol.de>
2024-07-11 18:15:11 +0200

    refactor: Update FancySwipeUpModal to use createFancySwipeUpModalStore for state management

--- src/components/organisms/FancySwipeUpModal/TFancySwipeUpModal.model.ts
+++ src/components/organisms/FancySwipeUpModal/TFancySwipeUpModal.model.ts
@@ -13,6 +13,5 @@ export type TFancySwipeUpModal = {
 export type TFancySwipeUpModalStore = {
   modals: TFancySwipeUpModal[];
   openSwipeUpModal: (id: string, children: React.ReactNode, config?: TModalSettings) => void;
-  removeSwipeUpModal: (id: string) => void;
   closeSwipeUpModal: (id: string) => void;
 };
