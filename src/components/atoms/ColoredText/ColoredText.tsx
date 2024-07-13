d85b396e19055df2c492848241055c121010226d
MrTRyy <Tobiastry@aol.de>
2024-07-13 17:58:55 +0200

    refactor: Add hoverLayer prop to ColoredText component

--- src/components/atoms/ColoredText/ColoredText.tsx
+++ src/components/atoms/ColoredText/ColoredText.tsx
@@ -4,10 +4,10 @@ import { TColoredText } from './TColoredText.model';
 import { ElementType } from 'react';
 
 export default function ColoredText<T extends ElementType>(props: TColoredText & TDynamicElement<T>) {
-  const { as, children, themeType, layer, externalStyle } = props;
+  const { as, children, themeType, layer, externalStyle, hoverLayer } = props;
 
   return (
-    <StyledItem as={as} $themeType={themeType} $layer={layer} $externalStyle={externalStyle}>
+    <StyledItem as={as} $themeType={themeType} $layer={layer} $hoverLayer={hoverLayer} $externalStyle={externalStyle}>
       {children}
     </StyledItem>
   );
