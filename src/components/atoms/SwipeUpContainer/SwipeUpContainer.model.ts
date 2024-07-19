23fd0d0652f4a9dc1e1cf0f25bd5543e7fad94d9
MrTRyy <Tobiastry@aol.de>
2024-07-19 23:37:31 +0200

    refactor: Remove unused style prop in SwipeUpContainer.model.ts

--- src/components/atoms/SwipeUpContainer/SwipeUpContainer.model.ts
+++ src/components/atoms/SwipeUpContainer/SwipeUpContainer.model.ts
@@ -3,7 +3,6 @@ import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
 
 export type TSwipeUpContainer = {
   children: React.ReactNode;
-  style?: { height: string };
   isScalable?: boolean;
   themeType?: TUiColorsNotTransparent;
   layer?: TLayer;
