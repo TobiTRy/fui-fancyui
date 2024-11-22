1ed2130f36dbb4f6673f6e8e5fe0a1ea5c492a81
MrTRyy <tobiastry@aol.de>
2024-11-22 15:56:12 +0100

    refactor: Add useBodyOverflow hook to Modal component

--- src/components/molecules/Modal/Modal.tsx
+++ src/components/molecules/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { BackDrop } from '@/components/atoms/BackDrop';
 import { SimpleDialog } from '@/components/atoms/SimpleDialog';
 import { TModalWithHTMLAttributes } from './TModal.model';
+import { useBodyOverflow } from '@/utils/hooks/useBodyOverflow';
 
 // ----------------------------- ---------------------------------------------- //
 // ------  The main Modal Component to comstomize the Head/Bottomline  ------- //
@@ -22,27 +23,19 @@ export default function Modal(props: TModalWithHTMLAttributes) {
     ...htmlProps
   } = props;
   const [modalVisible, setModalVisible] = useState(false);
+  useBodyOverflow('hidden');
 
   // close the modal when the user clicks on the backdrop
   const closeModalHanlder = () => {
-    if (isCloseable === false) return;
+    if (!isCloseable) return;
     // enable the scroll on the body when the modal is closed
-    document.body.style.overflow = 'auto';
     //if a components needs controle from outside
     if (onClose) onClose();
     setModalVisible(false);
   };
 
   useEffect(() => {
-    if (isOpen) {
-      // disable the scroll on the body when the modal is open
-      document.body.style.overflowY = 'hidden';
-      setModalVisible(true);
-    } else {
-      // enable the scroll on the body when the modal is closed
-      document.body.style.overflowY = 'auto';
-      setModalVisible(false);
-    }
+    setModalVisible(!!isOpen);
   }, [isOpen]);
 
   return (
