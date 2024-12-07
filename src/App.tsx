d9a56cb20d18ebf6d8d1c53dc3c0f86107436aa0
MrTRyy <Tobiastry@aol.de>
2024-12-07 16:10:57 +0100

    feat: Add MaterialColorGeneratorRoute for dynamic material color palette generation

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ import StepperRoute from '@/Routes/StepperRoute/StepperRoute';
 import SwitchActiveIndicatorRoute from '@/Routes/SwitchActiveIndicatorRoute/SwitchActiveIndicatorRoute';
 import TextGradientRoute from '@/Routes/TextGradientRoute/TextGradientRoute';
 import { useFancyModalStore } from './Routes/ModalRoute/ModalRoute';
+import MaterialColorGeneratorRoute from '@/Routes/MaterialColorGeneratorRoute/MaterialColorGeneratorRoute';
 // const Icon = (
 //   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 //     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm11.666 1.89c.682 0 1.139.47 1.187 1.107H14v-.11c-.053-1.187-1.024-2-2.342-2-1.604 0-2.518 1.05-2.518 2.751v.747c0 1.7.905 2.73 2.518 2.73 1.314 0 2.285-.792 2.342-1.939v-.114h-1.147c-.048.615-.497 1.05-1.187 1.05-.839 0-1.318-.62-1.318-1.727v-.742c0-1.112.488-1.754 1.318-1.754zm-6.188.926h.044L6.542 11h1.006L9 5.001H7.818l-.82 4.355h-.056L5.97 5.001h-.94l-.972 4.355h-.053l-.827-4.355H2L3.452 11h1.005l1.02-4.184z" />
@@ -168,6 +169,9 @@ function App() {
                   <Link to="/colorGenerator">ColorGenerator</Link>
                 </li>
                 <li>
+                  <Link to="/material-color-generator">Meterial Color Generator</Link>
+                </li>
+                <li>
                   <Link to="/content">FancyContent</Link>
                 </li>
                 <li>
@@ -238,6 +242,7 @@ function App() {
               <Route path="/chips" element={<ChipsRoute />} />
               <Route path="/checkbox" element={<CheckboxRoute />} />
               <Route path="/colorGenerator" element={<ColorGeneratorRoute />} />
+              <Route path="/material-color-generator" element={<MaterialColorGeneratorRoute />} />
               <Route path="/content" element={<FancyContentRoute />} />
               <Route path="/switches" element={<SwitchesRoute />} />
               <Route path="/menu" element={<MenueRoute />} />
