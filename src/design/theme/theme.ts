32617963f0b224fff5990693f2e8d112e79967f0
TobiTRy <tobiastry@aol.de>
2025-01-04 18:17:44 +0100

    fix: Add xxl breakpoint to theme for improved responsive design

--- src/design/theme/theme.ts
+++ src/design/theme/theme.ts
@@ -203,6 +203,7 @@ export const theme = {
     md: '(min-width: 768px)',
     lg: '(min-width: 992px)',
     xl: '(min-width: 1200px)',
+    xxl: '(min-width: 1600px)',
   },
   globalElementSizes: {
     xxs: '16px',
