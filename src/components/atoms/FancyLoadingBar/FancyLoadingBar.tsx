dea9e6578f91817cbf39f2f7574e90b30b9a312e
TobiTRy <tobiastry@aol.de>
2025-05-30 20:22:39 +0200

    refactor: Enhance FancyLoadingBar and FancyLoadingSpinner components to accept additional HTML props for improved flexibility

--- src/components/atoms/FancyLoadingBar/FancyLoadingBar.tsx
+++ src/components/atoms/FancyLoadingBar/FancyLoadingBar.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { LoadingBar, LoadingContainer } from '@/components/atoms/FancyLoadingBar/FancyLoadingBar.style';
-import { TFancyLoadingBar } from '@/components/atoms/FancyLoadingBar/TFancyLoadingBar.model';
+import { TFancyLoadingBarWithHTMLProps } from '@/components/atoms/FancyLoadingBar/TFancyLoadingBar.model';
 
 // ----------------------------------------------------- //
 // -------- A Loadingbar that indicates loading -------- //
 // ----------------------------------------------------- //
-export default function FancyLoadingBar(props: TFancyLoadingBar) {
-  const { themeType, layer, externalStyle } = props;
+export default function FancyLoadingBar(props: TFancyLoadingBarWithHTMLProps) {
+  const { themeType, layer, externalStyle, ...rest } = props;
 
   return (
-    <LoadingContainer $externalStyle={externalStyle}>
+    <LoadingContainer $externalStyle={externalStyle} {...rest}>
       <LoadingBar $themeType={themeType} $layer={layer} />
     </LoadingContainer>
   );
--- src/components/atoms/FancyLoadingSpinner/IFancyLoadingSpinner.model.ts
+++ src/components/atoms/FancyLoadingSpinner/IFancyLoadingSpinner.model.ts
@@ -1,9 +1,14 @@
 import { TLayer } from '@/types/TLayer';
 import { sizes } from './sizeSettings';
 import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
+import { HTMLAttributes } from 'react';
 
 export type TFancyLoadingSpinner = {
   sizeC?: keyof typeof sizes;
   themeType?: TUiColorsNotTransparent;
   layer?: TLayer;
 };
+
+export type TFancyLoadingSpinnerHTMLProps = HTMLAttributes<HTMLDivElement>;
+
+export type TFancyLoadingSpinnerWithHTMLProps = TFancyLoadingSpinner & TFancyLoadingSpinnerHTMLProps;
