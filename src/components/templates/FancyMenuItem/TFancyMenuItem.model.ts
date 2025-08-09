9286af6600800489a88036b288a8b476f9c983c9
TobiTRy <tobiastry@aol.de>
2025-08-09 12:09:28 +0200

    feat: Extend FancyMenuItem to accept themeTypeText and layerText props for enhanced styling options

--- src/components/templates/FancyMenuItem/TFancyMenuItem.model.ts
+++ src/components/templates/FancyMenuItem/TFancyMenuItem.model.ts
@@ -1,12 +1,16 @@
 import { TMenueItemWithHTMLAttrs } from '@/components/atoms/MenuItem/TMenueItem.model';
-import { TComponentSizes } from '@/types/TComponentSizes';
-
 import { TFancyContent } from '@/components/molecules/FancyContent';
+
+import { TUiColorsNotTransparent } from '@/types/TUiColorsNotTransparent';
+import { TComponentSizes } from '@/types/TComponentSizes';
+import { TLayer } from '@/types/TLayer';
 
 export type TFancyMenuItem = {
   label?: string;
   icon?: React.ReactNode;
   sizeC?: TComponentSizes;
+  themeTypeText?: TUiColorsNotTransparent;
+  layerText?: TLayer;
 };
 
 type TFancyContentAttrs = Pick<TFancyContent, 'gapBetweenIcon'>;
