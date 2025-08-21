3a858c5579722a1e78b0babfb89e9f46ce17cf45
TobiTRy <tobiastry@aol.de>
2025-08-21 22:43:56 +0200

    style: Update padding and max-height in SimpleDialog and FancyModalHeadLine for improved layout consistency

--- src/components/molecules/FancyModalHeadLine/FancyModalHeadLine.style.tsx
+++ src/components/molecules/FancyModalHeadLine/FancyModalHeadLine.style.tsx
@@ -24,7 +24,7 @@ export const StyledHeadLineTitles = styled.span<TStyledHeadLineTitles & { theme:
   align-items: ${({ $alignCenter }) => ($alignCenter ? 'center' : 'flex-start')};
   text-align: ${({ $alignCenter }) => ($alignCenter ? 'center' : 'start')};
   gap: ${({ theme, $gapBetweenText }) => $gapBetweenText && theme.spacing[$gapBetweenText || 'xxs']};
-  padding: ${({ $alignCenter, theme }) => ($alignCenter ? `0 ${theme.spacing.xl}` : `0 ${theme.spacing.xl} 0 0 `)};
+  padding: ${({ $alignCenter, theme }) => ($alignCenter ? `0 ${theme.spacing.lg}` : `0 ${theme.spacing.lg} 0 0 `)};
   width: 100%;
   box-sizing: border-box;
 `;
