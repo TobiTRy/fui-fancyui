b0fa47a45c4cacec4b7516311e1a176bc071730e
TobiTRy <tobiastry@aol.de>
2025-09-02 21:12:29 +0200

    build: Update build script in package.json for production mode and refactor vite.config.ts for improved configuration structure

--- vite.config.ts
+++ vite.config.ts
@@ -6,47 +6,49 @@ import path from 'node:path';
 import preserveDirectives from 'rollup-preserve-directives';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
-  },
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    preserveDirectives() as Plugin, // This plugin is used to preserve the directives in the output files (e.g. "use client")
-    visualizer(),
-  ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: path.resolve(__dirname, './lib/index.ts'),
-      formats: ['es'],
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
     },
-    rollupOptions: {
-      treeshake: true,
-      external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'react/jsx-dev-runtime',
-        'styled-components',
-        '@react-spring/web',
-        'color',
-      ],
-      output: {
-        preserveModulesRoot: '.',
-        preserveModules: true,
-        entryFileNames: ({ name: fileName }) => {
-          return `${fileName}.js`;
-        },
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'styled-components': 'styled',
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+      }),
+      preserveDirectives() as Plugin, // This plugin is used to preserve the directives in the output files (e.g. "use client")
+      visualizer(),
+    ],
+    build: {
+      copyPublicDir: false,
+      lib: {
+        entry: path.resolve(__dirname, './lib/index.ts'),
+        formats: ['es'],
+      },
+      rollupOptions: {
+        treeshake: true,
+        external: [
+          'react',
+          'react-dom',
+          'react/jsx-runtime',
+          'react/jsx-dev-runtime',
+          'styled-components',
+          '@react-spring/web',
+          'color',
+        ],
+        output: {
+          preserveModulesRoot: '.',
+          preserveModules: true,
+          entryFileNames: ({ name: fileName }) => {
+            return `${fileName}.js`;
+          },
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            'styled-components': 'styled',
+          },
         },
       },
     },
-  },
+  };
 });
